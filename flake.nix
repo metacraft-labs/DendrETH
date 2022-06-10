7051ffd6586f315495a2d5baf73721da4c4ec947
Petar Kirov <petar.p.kirov@gmail.com>
2022-06-10 17:59:04 +0300

    style(nix): Format files with `alejandra`

--- flake.nix
+++ flake.nix
@@ -6,7 +6,11 @@
     flake-utils.url = github:numtide/flake-utils;
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
     flake-utils.lib.simpleFlake {
       inherit self nixpkgs;
       name = "DendrETH";
