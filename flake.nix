81ee8417c6fc546ec16cd2b42c5d3949a52a82a4
Petar Kirov <petar.p.kirov@gmail.com>
2022-06-10 17:26:00 +0300

    config(nixpkgs): Switch nixpkgs branch from `nixpkgs-unstable` `nixos-22.05`
    The `nixos-22.05` branch provides a combination of stability, binary
    cache availability and recent-enough versions of most packages.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "DendrETH";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = github:numtide/flake-utils;
   };
 
