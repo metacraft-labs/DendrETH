c9791031e142c46e45c978988e2c736ffbfcc2f6
Petar Kirov <petar.p.kirov@gmail.com>
2022-06-10 17:59:04 +0300

    style(nix): Format files with `alejandra`

--- shell.nix
+++ shell.nix
@@ -1,26 +1,26 @@
-{ pkgs }: with pkgs;
-let
+{pkgs}:
+with pkgs; let
   nodejs = nodejs-16_x;
 in
-mkShell {
-  buildInputs = [
-    # For priting the direnv banner
-    figlet
+  mkShell {
+    buildInputs = [
+      # For priting the direnv banner
+      figlet
 
-    # For formatting Nix files
-    alejandra
+      # For formatting Nix files
+      alejandra
 
-    # For an easy way to launch all required blockchain simulations
-    # and tailed log files
-    tmux
-    tmuxinator
+      # For an easy way to launch all required blockchain simulations
+      # and tailed log files
+      tmux
+      tmuxinator
 
-    # Node.js dev environment for unit tests
-    nodejs
-    (yarn.override {inherit nodejs;})
-  ];
+      # Node.js dev environment for unit tests
+      nodejs
+      (yarn.override {inherit nodejs;})
+    ];
 
-  shellHook = ''
-    figlet "DendrETH"
-  '';
-}
+    shellHook = ''
+      figlet "DendrETH"
+    '';
+  }
