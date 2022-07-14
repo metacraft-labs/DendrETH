1088457beee1d7898f23ac7d1f428d0c30523269
Zahary Karadjov <zahary@gmail.com>
2022-07-14 19:41:39 +0300

    Working example for manipulating sequences
    I've switched to `--gc:destructors` because this enables the
    necessary portions of the Nim system module for working with
    sequences.
    
    I've also swiched to LLVM 13, because LLVM 14 is marked as broken
    on macOS/ARM.

--- nix/nim-wasm/include/stdint.h
+++ nix/nim-wasm/include/stdint.h
@@ -7,3 +7,6 @@ typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long long uint64_t;
+
+typedef unsigned int size_t;
+typedef int ptrdiff_t;
