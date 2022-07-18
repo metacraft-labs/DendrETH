25dd2e959f9d9d50ccf870841a1e57fc4378b67a
Petar Kirov <petar.p.kirov@gmail.com>
2022-07-18 19:01:25 +0300

    build(nix/nim-wasm): Make `nim-wasm` direct replacement of `nim`
    Previously, the `nim-wasm` command didn't allow specifying any options to the
    nim compiler (outside of the input file).
    
    We solve this by moving the Nim compiler options (that were previously passed
    on the command-line) to a `nim.cfg` file generated by the Nix package.

--- nix/nim-wasm/default.nix
+++ nix/nim-wasm/default.nix
@@ -1,44 +1,51 @@
 {
+  writeTextFile,
   writeShellApplication,
   llvm,
   nim,
-}:
-writeShellApplication {
-  name = "nim-wasm";
-  runtimeInputs = [nim llvm.lld llvm.clang-unwrapped];
-  text = ''
-    input_file="''${!#}"
-    output_file="''${input_file%.nim}.wasm"
-    set -x
-    nim c \
-      --skipCfg:on \
-      --app:lib \
-      --os:standalone \
-      --cpu:wasm32 \
-      --cc:clang \
-      --noMain \
-      --opt:size \
-      --listCmd \
-      --d:nimNoLibc \
-      --d:wasm \
-      --d:noSignalHandler \
-      --d:nimPreviewFloatRoundtrip \
-      --exceptions:goto \
-      --gc:destructors \
-      --threads:off \
-      --stackTrace:off \
-      --lineTrace:off \
-      --lib:./vendor/nim/lib \
-      --passC:"-w" \
-      --passC:"-ferror-limit=3" \
-      --passC:"-I${./include}" \
-      --passC:"-fuse-ld=wasm-ld" \
-      --passC:"--target=wasm32-unknown-unknown-wasm" \
-      --passC:"-nostdinc -fno-builtin -fno-exceptions -fno-threadsafe-statics" \
-      --passC:"-fvisibility=hidden -flto" \
-      --passC:"-std=c99" \
-      --passL:"--target=wasm32-unknown-unknown-wasm -nostdlib -Wl,--no-entry,--allow-undefined,--export-dynamic,--gc-sections,--strip-all" \
-      "-o:$output_file" \
-      "$@"
-  '';
-}
+}: let
+  nimcfg = writeTextFile {
+    name = "nim-cfg";
+    text = ''
+      --skipCfg:on
+      --app:lib
+      --os:standalone
+      --cpu:wasm32
+      --cc:clang
+      --noMain
+      --opt:size
+      --listCmd
+      --d:nimNoLibc
+      --d:wasm
+      --d:noSignalHandler
+      --d:nimPreviewFloatRoundtrip
+      --exceptions:goto
+      --gc:destructors
+      --threads:off
+      --stackTrace:off
+      --lineTrace:off
+      --lib:"./vendor/nim/lib"
+      --clang.options.linker:""
+      --clang.cpp.options.linker:""
+      --passC:"-w"
+      --passC:"-ferror-limit=3"
+      --passC:"-I${./include}"
+      --passC:"-fuse-ld=wasm-ld"
+      --passC:"--target=wasm32-unknown-unknown-wasm"
+      --passC:"-nostdinc -fno-builtin -fno-exceptions -fno-threadsafe-statics"
+      --passC:"-fvisibility=hidden -flto"
+      --passC:"-std=c99"
+      --passL:"--target=wasm32-unknown-unknown-wasm -nostdlib -Wl,--no-entry,--allow-undefined,--export-dynamic,--gc-sections,--strip-all"
+    '';
+    destination = "/nim/nim.cfg";
+  };
+in
+  writeShellApplication {
+    name = "nim-wasm";
+    runtimeInputs = [nim llvm.lld llvm.clang-unwrapped];
+    text = ''
+      export XDG_CONFIG_HOME="${nimcfg}"
+      set -x
+      nim "$@"
+    '';
+  }
