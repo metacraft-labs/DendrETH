3105c2295044186d10d681a3b662a122a8c9de1b
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-07-19 21:03:54 +0300

    update compile script to work with more complex file structure

--- src/tezos/scripts/compile.js
+++ src/tezos/scripts/compile.js
@@ -9,23 +9,40 @@ const BUILD_PATH = path.join(__dirname, '..', 'build', 'compilation');
 const SRC_PATH = path.join(__dirname, '..', 'src');
 
 // Change the following regex to filter the templates being tested
-const FILE_FILTER = /.*[.]contract[.]ts/;
+const FILE_FILTER = /.*[.]ts/;
 
 if (fs.existsSync(BUILD_PATH)) {
     fs.rmSync(BUILD_PATH, { recursive: true });
 }
 fs.mkdirSync(BUILD_PATH, { recursive: true });
 
-const files = fs.readdirSync(SRC_PATH, { encoding: 'utf-8' });
+function getFilesInDir(_path) {
+    let files = [];
+    const fileAndFolders = fs.readdirSync(_path, { encoding: 'utf-8' ,withFileTypes: true});
+    for (let ff of fileAndFolders) {
+        if (ff.isDirectory()) {
+            files = [...files, ...getFilesInDir(path.join(_path, ff.name))]
+        } else {
+            files.push({
+                name: ff.name,
+                path: _path
+            })
+        }
+    }
+    return files;
+}
+
+const files = getFilesInDir(SRC_PATH, { encoding: 'utf-8' ,withFileTypes: true});
 
 files
-    .filter((f) => f.match(FILE_FILTER))
-    .map(async (fileName) => {
+    .filter((f) => f.name.match(FILE_FILTER))
+    .map(async (file) => {
         try {
-            const dir = path.join(BUILD_PATH, fileName.replace('.ts', ''));
+            const dir = path.join(BUILD_PATH, file.name.replace('.ts', ''));
             fs.mkdirSync(dir, { recursive: true });
-            execSync(`sh ${SCRIPT_PATH} compile ${SRC_PATH}/${fileName} ${dir}`);
+            console.log(`sh ${SCRIPT_PATH} compile ${file.path}/${file.name} ${dir}`)
+            execSync(`sh ${SCRIPT_PATH} compile ${file.path}/${file.name} ${dir}`);
         } catch (e) {
-            console.error(e.output?.toString() || e);
+            // console.error(e.message);
         }
     });
