f41672b3da581b3405e7f87451e5cf3e230b9250
EmilIvanichkovv <emil.ivanichkov@gmail.com>
2022-07-23 15:46:46 +0300

    config(jest): Update `testMatch` pattern to only look in `__test__` folders

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import type { InitialOptionsTsJest } from 'ts-jest'
 
 const config: InitialOptionsTsJest = {
   preset: 'ts-jest/presets/default-esm', // or other ESM presets
+  testMatch: [ "**/__tests__/**/*.ts" ],
   globals: {
     'ts-jest': {
       useESM: true,
