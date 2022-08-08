3ac10d8e83dc0205f8aef61052845c2db195762c
Dimo99 <dimodimov1999@gmail.com>
2022-08-08 17:09:14 +0300

    local changes

--- src/circom/scripts/compile-HelloWorld.sh
+++ src/circom/scripts/compile-HelloWorld.sh
@@ -16,15 +16,15 @@ echo "Compiling HelloWorld.circom..."
 # compile circuit
 
 circom HelloWorld.circom --r1cs --wasm --sym -o HelloWorld
-snarkjs r1cs info HelloWorld/HelloWorld.r1cs
+npx snarkjs r1cs info HelloWorld/HelloWorld.r1cs
 
 # Start a new zkey and make a contribution
 
-snarkjs groth16 setup HelloWorld/HelloWorld.r1cs powersOfTau28_hez_final_10.ptau HelloWorld/circuit_0000.zkey
-snarkjs zkey contribute HelloWorld/circuit_0000.zkey HelloWorld/circuit_final.zkey --name="1st Contributor Name" -v -e="random text"
-snarkjs zkey export verificationkey HelloWorld/circuit_final.zkey HelloWorld/verification_key.json
+npx snarkjs groth16 setup HelloWorld/HelloWorld.r1cs powersOfTau28_hez_final_10.ptau HelloWorld/circuit_0000.zkey
+npx snarkjs zkey contribute HelloWorld/circuit_0000.zkey HelloWorld/circuit_final.zkey --name="1st Contributor Name" -v -e="random text"
+npx snarkjs zkey export verificationkey HelloWorld/circuit_final.zkey HelloWorld/verification_key.json
 
 # generate solidity contract
-snarkjs zkey export solidityverifier HelloWorld/circuit_final.zkey ../HelloWorldVerifier.sol
+npx snarkjs zkey export solidityverifier HelloWorld/circuit_final.zkey ../HelloWorldVerifier.sol
 
 cd ../..
