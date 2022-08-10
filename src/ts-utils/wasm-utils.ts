07de90a4b28049cdcad004422efc957e2037ff4a
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2022-08-10 16:16:43 +0300

    feat(wasm-utils): Rename file and add helper functions
    Add interfaces for working with `wasm memory`
    Add `marshalSzzObjectToWasm` function

--- src/ts-utils/wasm-utils.ts
+++ src/ts-utils/wasm-utils.ts
@@ -1,4 +1,6 @@
 import { readFile } from 'fs/promises';
+import '@chainsafe/ssz';
+import { Type } from '@chainsafe/ssz';
 
 export async function loadWasm<Exports extends WebAssembly.Exports>({
   from,
@@ -22,4 +24,28 @@ export async function loadWasm<Exports extends WebAssembly.Exports>({
     throw new Error('Invalid argument: `from`');
   }
   return res.instance.exports as Exports;
+}
+
+interface WasmModuleMemoryInterface extends WebAssembly.Exports {
+  memory: WebAssembly.Memory;
+  allocMemory: (a: number) => any;
+}
+
+interface MemorySlice {
+  startOffset: number;
+  length: number;
+}
+
+export function marshalSzzObjectToWasm<T>(
+  exports: WasmModuleMemoryInterface,
+  sszObject: T,
+  sszType: Type<T>): MemorySlice {
+    const serialized = sszType.serialize(sszObject);
+    const startOffset = exports.allocMemory(serialized.length);
+    const slice = new Uint8Array(exports.memory.buffer);
+    slice.set(serialized, startOffset);
+    return {
+      startOffset,
+      length: serialized.length
+    }
 }
