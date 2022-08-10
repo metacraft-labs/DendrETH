6db2bc931236ee034be20a3276a0cfa4ab89bbb3
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2022-08-10 16:38:43 +0300

    refactor(tests): Formatting, add `src/nim-light-client` to the path

--- __tests__/nimToWasm/eth2Digest.nim
+++ __tests__/nimToWasm/eth2Digest.nim
@@ -1,11 +1,6 @@
-import ../../src/nim-light-client/light_client_utils
+import light_client_utils
 import nimcrypto/hash
 
-
-func eth2DigestCompare*(
-    blockRoot: Eth2Digest
-  ): bool {.cdecl, exportc, dynlib} =
+func eth2DigestCompare*(blockRoot: Eth2Digest): bool {.cdecl, exportc, dynlib} =
   var expected = MDigest[256].fromHex("ca6ddab42853a7aef751e6c2bf38b4ddb79a06a1f971201dcf28b0f2db2c0d61")
-  if expected == blockRoot:
-    return true
-  return false
+  expected == blockRoot
