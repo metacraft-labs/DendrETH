5fb3abe1df73a0bcc45178f256eabeff189fc758
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2022-08-17 17:02:50 +0300

    feat(hex-utils): Implement `arrayToString` function

--- src/ts-utils/hex-utils.ts
+++ src/ts-utils/hex-utils.ts
@@ -1,8 +1,12 @@
 export function arrayToHex(arr: Uint8Array): string {
-    return '0x' + Buffer.from(arr).toString('hex');
-  }
+  return '0x' + Buffer.from(arr).toString('hex');
+}
 
-  export function hexToArray(hex: string): Buffer {
-    hex = hex.startsWith('0x') ? hex.slice(2) : hex;
-    return Buffer.from(hex, 'hex');
-  }
+export function hexToArray(hex: string): Buffer {
+  hex = hex.startsWith('0x') ? hex.slice(2) : hex;
+  return Buffer.from(hex, 'hex');
+}
+
+export function arrayToString(arr: Uint8Array) {
+  return String.fromCharCode(...arr);
+}
