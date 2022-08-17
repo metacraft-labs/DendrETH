e0d071f8829c5314fd9da37b57f19a87f87c4c12
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2022-08-17 13:37:59 +0300

    refactor(LCW): Remove unnecessary code.
    Clean unnecessary types and functions in `light_client_utils`
    Add explanation comment for `--passC:"-mbulk-memory"` in
    `nix/nim-wasm/default.nix`

--- src/nim-light-client/light_client_utils.nim
+++ src/nim-light-client/light_client_utils.nim
@@ -1,5 +1,5 @@
 import
-  std/[typetraits, sequtils, options, tables],
+  std/[typetraits, options, tables],
   ssz_serialization/[merkleization, types, proofs],
   nimcrypto/hash,
   blscurve
@@ -103,16 +103,6 @@ const
   #Phase0
   SLOTS_PER_EPOCH* {.intdefine.}: uint64 = 32
   SYNC_COMMITTEE_SIZE* = 512
-    # 2**4 (= 16)
-  MAX_PROPOSER_SLASHINGS*: uint64 = 16
-  # 2**1 (= 2)
-  MAX_ATTESTER_SLASHINGS*: uint64 = 2
-  # 2**7 (= 128)
-  MAX_ATTESTATIONS*: uint64 = 128
-  # 2**4 (= 16)
-  MAX_DEPOSITS*: uint64 = 16
-  # 2**4 (= 16)
-  MAX_VOLUNTARY_EXITS*: uint64 = 16
   # Altair
   ALTAIR_FORK_VERSION = Version [byte 0x01, 0x00, 0x00, 0x00]
   ALTAIR_FORK_EPOCH = Epoch(74240)
@@ -123,20 +113,8 @@ const
   # Bellatrix
   BELLATRIX_FORK_VERSION = Version [byte 0x02, 0x00, 0x00, 0x00]
   BELLATRIX_FORK_EPOCH = Epoch(uint64.high)
-    # 2**30 (= 1,073,741,824)
-  MAX_BYTES_PER_TRANSACTION* = 1073741824
-  # 2**20 (= 1,048,576)
-  MAX_TRANSACTIONS_PER_PAYLOAD* = 1048576
-  # 2**8 (= 256)
-  BYTES_PER_LOGS_BLOOM* = 256
-  # 2**5 (= 32)
-  MAX_EXTRA_DATA_BYTES* = 32
+
   # Sharding
-  SHARDING_FORK_VERSION = Version [byte 0x03, 0x00, 0x00, 0x00]
-  SHARDING_FORK_EPOCH = Epoch(uint64.high)
-
-  MAX_VALIDATORS_PER_COMMITTEE*: uint64 = 2048
-
   SLOTS_PER_SYNC_COMMITTEE_PERIOD* =
    SLOTS_PER_EPOCH * EPOCHS_PER_SYNC_COMMITTEE_PERIOD
 
@@ -147,27 +125,14 @@ const
   FAR_FUTURE_SLOT* = Slot(not 0'u64)
   FAR_FUTURE_EPOCH* = (not 0'u64).Epoch # 2^64 - 1 in spec
 
-  # FAR_FUTURE_EPOCH* = Epoch(not 0'u64) # in presets
   FAR_FUTURE_PERIOD* = SyncCommitteePeriod(not 0'u64)
+
 type
 # Types from base.nim
   DomainType* = distinct array[4, byte]
   Eth2Domain* = array[32, byte]
 
-  GraffitiBytes* = distinct array[MAX_GRAFFITI_SIZE, byte]
-  Gwei* = uint64
   ForkDigest* = distinct array[4, byte]
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#signedbeaconblockheader
-  SignedBeaconBlockHeader* = object
-    message*: BeaconBlockHeader
-    signature*: ValidatorSig
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#eth1data
-  Eth1Data* = object
-    deposit_root*: Eth2Digest
-    deposit_count*: uint64
-    block_hash*: Eth2Digest
 
   # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#beaconblockheader
   BeaconBlockHeader* = object
@@ -182,83 +147,6 @@ type
     object_root*: Eth2Digest
     domain*: Eth2Domain
 
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#proposerslashing
-  ProposerSlashing* = object
-    signed_header_1*: SignedBeaconBlockHeader
-    signed_header_2*: SignedBeaconBlockHeader
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#attesterslashing
-  AttesterSlashing* = object
-    attestation_1*: IndexedAttestation
-    attestation_2*: IndexedAttestation
-
-  CommitteeValidatorsBits* = BitList[Limit MAX_VALIDATORS_PER_COMMITTEE]
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#indexedattestation
-  IndexedAttestation* = object
-    attesting_indices*: List[uint64, Limit MAX_VALIDATORS_PER_COMMITTEE]
-    data*: AttestationData
-    signature*: ValidatorSig
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#attestation
-  Attestation* = object
-    aggregation_bits*: CommitteeValidatorsBits
-    data*: AttestationData
-    signature*: ValidatorSig
-
-  TrustedAttestation* = object
-    # The Trusted version, at the moment, implies that the cryptographic signature was checked.
-    # It DOES NOT imply that the state transition was verified.
-    # Currently the code MUST verify the state transition as soon as the signature is verified
-    aggregation_bits*: CommitteeValidatorsBits
-    data*: AttestationData
-    signature*: TrustedSig
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#checkpoint
-  Checkpoint* = object
-    epoch*: Epoch
-    root*: Eth2Digest
-
-  FinalityCheckpoints* = object
-    justified*: Checkpoint
-    finalized*: Checkpoint
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#AttestationData
-  AttestationData* = object
-    slot*: Slot
-    index*: uint64 ## `CommitteeIndex` after validation
-    # LMD GHOST vote
-    beacon_block_root*: Eth2Digest
-    # FFG vote
-    source*: Checkpoint
-    target*: Checkpoint
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#deposit
-  Deposit* = object
-    proof*: array[DEPOSIT_CONTRACT_TREE_DEPTH + 1, Eth2Digest]
-      ## Merkle path to deposit root
-    data*: DepositData
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#depositdata
-  DepositData* = object
-    pubkey*: ValidatorPubKey
-    withdrawal_credentials*: Eth2Digest
-    amount*: Gwei
-    # Cannot use TrustedSig here as invalid signatures are possible and determine
-    # if the deposit should be added or not during processing
-    signature*: ValidatorSig  # Signing over DepositMessage
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#voluntaryexit
-  VoluntaryExit* = object
-    epoch*: Epoch
-      ## Earliest epoch when voluntary exit can be processed
-    validator_index*: uint64 # `ValidatorIndex` after validation
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#signedvoluntaryexit
-  SignedVoluntaryExit* = object
-    message*: VoluntaryExit
-    signature*: ValidatorSig
-
   # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#forkdata
   ForkData* = object
     current_version*: Version
@@ -272,14 +160,6 @@ type
     ## cases, like the database state)
     blob*: array[RawPubKeySize, byte]
 
-  TrustedSig* = object
-    data*: array[RawSigSize, byte]
-
-  UncompressedPubKey* = object
-    ## Uncompressed variation of ValidatorPubKey - this type is faster to
-    ## deserialize but doubles the storage footprint
-    blob*: array[UncompressedPubKeySize, byte]
-
   CookedPubKey* = distinct blscurve.PublicKey ## Valid deserialized key
   CookedSig* = distinct blscurve.Signature  ## \
   ## Cooked signatures are those that have been loaded successfully from a
@@ -287,48 +167,12 @@ type
   ## checking
   ValidatorSig* = object
     blob*: array[RawSigSize, byte]
-# Types from forks.nim
-  BeaconBlockFork* {.pure.} = enum
-    Phase0,
-    Altair,
-    Bellatrix
 
+# Types from forks.nim
   BeaconStateFork* {.pure.} = enum
     Phase0,
     Altair,
     Bellatrix
-
-  ForkedBeaconBlock* = object
-    case kind*: BeaconBlockFork
-    of BeaconBlockFork.Phase0:    phase0Data*:    Phase0BeaconBlock
-    of BeaconBlockFork.Altair:    altairData*:    AltairBeaconBlock
-    of BeaconBlockFork.Bellatrix: bellatrixData*: BellatrixBeaconBlock
-
-# Types from phase0.nim
-# https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#beaconblockbody
-  Phase0BeaconBlockBody* = object
-    randao_reveal*: ValidatorSig
-    eth1_data*: Eth1Data
-    graffiti*: GraffitiBytes
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#beaconblock
-  Phase0BeaconBlock* = object
-    ## For each slot, a proposer is chosen from the validator pool to propose
-    ## a new block. Once the block as been proposed, it is transmitted to
-    ## validators that will have a chance to vote on it through attestations.
-    ## Each block collects attestations, or votes, on past blocks, thus a chain
-    ## is formed.
-
-    slot*: Slot
-    proposer_index*: uint64 # `ValidatorIndex` after validation
-
-    parent_root*: Eth2Digest
-      ## Root hash of the previous block
-
-    state_root*: Eth2Digest
-      # The state root, _after_ this block has been processed
-
-    body*: Phase0BeaconBlockBody
 
 # Types from altair.nim
   FinalityBranch* =
@@ -340,11 +184,7 @@ type
   NextSyncCommitteeBranch* =
     array[log2trunc(NEXT_SYNC_COMMITTEE_INDEX), Eth2Digest]
 
-  TrustedSyncAggregate* = object
-    sync_committee_bits*: BitArray[SYNC_COMMITTEE_SIZE]
-    sync_committee_signature*: TrustedSig
-
-  # https://github.com/ethereum/consensus-specs/blob/vFuture/specs/altair/sync-protocol.md#lightclientbootstrap
+  # https://github.com/ethereum/consensus-specs/blob/dev/specs/altair/light-client/sync-protocol.md#lightclientbootstrap
   LightClientBootstrap* = object
     header*: BeaconBlockHeader
       ## The requested beacon block header
@@ -353,7 +193,7 @@ type
       ## Current sync committee corresponding to `header`
     current_sync_committee_branch*: CurrentSyncCommitteeBranch
 
-  # https://github.com/ethereum/consensus-specs/blob/vFuture/specs/altair/sync-protocol.md#lightclientupdate
+  # https://github.com/ethereum/consensus-specs/blob/dev/specs/altair/light-client/sync-protocol.md#lightclientupdate  LightClientUpdate* = object
   LightClientUpdate* = object
     attested_header*: BeaconBlockHeader
       ## The beacon block header that is attested to by the sync committee
@@ -371,7 +211,7 @@ type
     signature_slot*: Slot
       ## Slot at which the aggregate signature was created (untrusted)
 
-  # https://github.com/ethereum/consensus-specs/blob/vFuture/specs/altair/sync-protocol.md#lightclientfinalityupdate
+  # https://github.com/ethereum/consensus-specs/blob/dev/specs/altair/light-client/sync-protocol.md#lightclientfinalityupdate
   LightClientFinalityUpdate* = object
     # The beacon block header that is attested to by the sync committee
     attested_header*: BeaconBlockHeader
@@ -385,7 +225,7 @@ type
     # Slot at which the aggregate signature was created (untrusted)
     signature_slot*: Slot
 
-  # https://github.com/ethereum/consensus-specs/blob/vFuture/specs/altair/sync-protocol.md#lightclientoptimisticupdate
+  # https://github.com/ethereum/consensus-specs/blob/dev/specs/altair/light-client/sync-protocol.md#lightclientoptimisticupdate
   LightClientOptimisticUpdate* = object
     # The beacon block header that is attested to by the sync committee
     attested_header*: BeaconBlockHeader
@@ -411,62 +251,7 @@ type
     LightClientBootstrap |
     SomeLightClientUpdate
 
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/altair/beacon-chain.md#synccommittee
-  SyncCommittee* = object
-    pubkeys*: HashArray[Limit SYNC_COMMITTEE_SIZE, ValidatorPubKey]
-    aggregate_pubkey*: ValidatorPubKey
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#beaconblock
-  AltairBeaconBlock* = object
-    ## For each slot, a proposer is chosen from the validator pool to propose
-    ## a new block. Once the block as been proposed, it is transmitted to
-    ## validators that will have a chance to vote on it through attestations.
-    ## Each block collects attestations, or votes, on past blocks, thus a chain
-    ## is formed.
-
-    slot*: Slot
-    proposer_index*: uint64 # `ValidatorIndex` after validation
-
-    parent_root*: Eth2Digest
-      ## Root hash of the previous block
-
-    state_root*: Eth2Digest
-      ## The state root, _after_ this block has been processed
-
-    body*: AltairBeaconBlockBody
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/altair/beacon-chain.md#beaconblockbody
-  AltairBeaconBlockBody* = object
-    randao_reveal*: ValidatorSig
-    eth1_data*: Eth1Data
-      ## Eth1 data vote
-
-    graffiti*: GraffitiBytes
-      ## Arbitrary data
-
-    # Operations
-    proposer_slashings*: List[ProposerSlashing, Limit MAX_PROPOSER_SLASHINGS]
-    attester_slashings*: List[AttesterSlashing, Limit MAX_ATTESTER_SLASHINGS]
-    attestations*: List[Attestation, Limit MAX_ATTESTATIONS]
-    deposits*: List[Deposit, Limit MAX_DEPOSITS]
-    voluntary_exits*: List[SignedVoluntaryExit, Limit MAX_VOLUNTARY_EXITS]
-
-    # [New in Altair]
-    sync_aggregate*: SyncAggregate
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/altair/beacon-chain.md#syncaggregate
-  SyncAggregate* = object
-    sync_committee_bits*: BitArray[SYNC_COMMITTEE_SIZE]
-    sync_committee_signature*: ValidatorSig
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#signedbeaconblock
-  AltairSignedBeaconBlock* = object
-    message*: AltairBeaconBlock
-    signature*: ValidatorSig
-
-    root*: Eth2Digest # cached root of signed beacon block
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/altair/sync-protocol.md#lightclientstore
+  # https://github.com/ethereum/consensus-specs/blob/dev/specs/altair/light-client/sync-protocol.md#lightclientstore
   LightClientStore* = object
     finalized_header*: BeaconBlockHeader
       ## Beacon block header that is finalized
@@ -486,82 +271,15 @@ type
       ## safety threshold)
     current_max_active_participants*: uint64
 
-# Types from bellatrix.nim
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/bellatrix/beacon-chain.md#custom-types
-  Transaction* = List[byte, Limit MAX_BYTES_PER_TRANSACTION]
+  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/altair/beacon-chain.md#synccommittee
+  SyncCommittee* = object
+    pubkeys*: HashArray[Limit SYNC_COMMITTEE_SIZE, ValidatorPubKey]
+    aggregate_pubkey*: ValidatorPubKey
 
-  ExecutionAddress* = object
-    data*: array[20, byte]  # TODO there's a network_metadata type, but the import hierarchy's inconvenient
-
-  BloomLogs* = object
-    data*: array[BYTES_PER_LOGS_BLOOM, byte]
-
-  PayloadID* = array[8, byte]
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#beaconblock
-  BellatrixBeaconBlock* = object
-    ## For each slot, a proposer is chosen from the validator pool to propose
-    ## a new block. Once the block as been proposed, it is transmitted to
-    ## validators that will have a chance to vote on it through attestations.
-    ## Each block collects attestations, or votes, on past blocks, thus a chain
-    ## is formed.
-
-    slot*: Slot
-    proposer_index*: uint64 # `ValidatorIndex` after validation
-
-    parent_root*: Eth2Digest
-      ## Root hash of the previous block
-
-    state_root*: Eth2Digest
-      ## The state root, _after_ this block has been processed
-
-    body*: BellatrixBeaconBlockBody
-
-# https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/bellatrix/beacon-chain.md#beaconblockbody
-  BellatrixBeaconBlockBody* = object
-    randao_reveal*: ValidatorSig
-    eth1_data*: Eth1Data
-      ## Eth1 data vote
-
-    graffiti*: GraffitiBytes
-      ## Arbitrary data
-
-    # Operations
-    proposer_slashings*: List[ProposerSlashing, Limit MAX_PROPOSER_SLASHINGS]
-    attester_slashings*: List[AttesterSlashing, Limit MAX_ATTESTER_SLASHINGS]
-    attestations*: List[Attestation, Limit MAX_ATTESTATIONS]
-    deposits*: List[Deposit, Limit MAX_DEPOSITS]
-    voluntary_exits*: List[SignedVoluntaryExit, Limit MAX_VOLUNTARY_EXITS]
-    sync_aggregate*: SyncAggregate
-
-    # Execution
-    execution_payload*: ExecutionPayload  # [New in Bellatrix]
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/bellatrix/beacon-chain.md#executionpayload
-  ExecutionPayload* = object
-    parent_hash*: Eth2Digest
-    fee_recipient*: ExecutionAddress  # 'beneficiary' in the yellow paper
-    state_root*: Eth2Digest
-    receipts_root*: Eth2Digest # 'receipts root' in the yellow paper
-    logs_bloom*: BloomLogs
-    prev_randao*: Eth2Digest  # 'difficulty' in the yellow paper
-    block_number*: uint64  # 'number' in the yellow paper
-    gas_limit*: uint64
-    gas_used*: uint64
-    timestamp*: uint64
-    extra_data*: List[byte, MAX_EXTRA_DATA_BYTES]
-    base_fee_per_gas*: UInt256
-
-    # Extra payload fields
-    block_hash*: Eth2Digest # Hash of execution block
-    transactions*: List[Transaction, MAX_TRANSACTIONS_PER_PAYLOAD]
-
-  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/phase0/beacon-chain.md#signedbeaconblock
-  BellatrixSignedBeaconBlock* = object
-    message*: BellatrixBeaconBlock
-    signature*: ValidatorSig
-
-    root*: Eth2Digest # cached root of signed beacon block
+  # https://github.com/ethereum/consensus-specs/blob/v1.2.0-rc.1/specs/altair/beacon-chain.md#syncaggregate
+  SyncAggregate* = object
+    sync_committee_bits*: BitArray[SYNC_COMMITTEE_SIZE]
+    sync_committee_signature*: ValidatorSig
 
 # Types from helpers.nim
   LightClientUpdateMetadata* = object
@@ -704,7 +422,7 @@ template data*(v: ForkDigest | Version | DomainType): array[4, byte] =
 
 # Callables from ssz_codec.nim
 template toSszType*(v: Slot|Epoch|SyncCommitteePeriod): auto = uint64(v)
-template toSszType*(v: ForkDigest|GraffitiBytes): auto = distinctBase(v)
+template toSszType*(v: ForkDigest): auto = distinctBase(v)
 template toSszType*(v: Version): auto = distinctBase(v)
 
 # Callables from forks.nim
@@ -856,7 +574,6 @@ func is_better_data*(new_meta, old_meta: LightClientUpdateMetadata): bool =
 
   # Tiebreaker 2: Prefer older data (fewer changes to best data)
   new_meta.attested_slot < old_meta.attested_slot
-
 
 template is_better_update*[A, B: SomeLightClientUpdate](
     new_update: A, old_update: B): bool =
