cc1acf52ea19e4204b9711bf3ebb2fc3ff38ccbc
Petar Kirov <petar.p.kirov@gmail.com>
2022-08-25 14:20:00 +0300

    config(flake.nix): Add `nixConfig` and `inputs` settings to enable cachix binary cache

--- flake.nix
+++ flake.nix
@@ -1,12 +1,25 @@
 {
   description = "DendrETH";
 
+  # Opt into `nix-blockchain-development`'s substituter (binary cache).
+  # `nixConfig` settings are not transitive so every user of a flake with a
+  # custom binary cache must manually include its `nixConfig` settings for
+  # substituters and trusted public keys:
+  nixConfig = {
+    extra-substituters = "https://nix-blockchain-development.cachix.org";
+    extra-trusted-public-keys = "nix-blockchain-development.cachix.org-1:Ekei3RuW3Se+P/UIo6Q/oAgor/fVhFuuuX5jR8K/cdg=";
+  };
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    flake-utils.url = github:numtide/flake-utils;
+    # To ensure all packages from mcl-blockchain will be fetched from its
+    # binary cache we need to ensure that we use exact same commit hash of the
+    # inputs below. If we didn't, we may either:
+    # * end up with multiple copies of the same package from nixpkgs
+    # * be unable to use the binary cache, since the packages there where
+    #   using different versions of their dependencies from nixpkgs
     mcl-blockchain.url = "github:metacraft-labs/nix-blockchain-development";
-    mcl-blockchain.inputs.nixpkgs.follows = "nixpkgs";
-    mcl-blockchain.inputs.flake-utils.follows = "flake-utils";
+    nixpkgs.follows = "mcl-blockchain/nixpkgs";
+    flake-utils.follows = "mcl-blockchain/flake-utils";
   };
 
   outputs = {
