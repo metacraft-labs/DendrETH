7821a0002393a9b8b4c256e9c41cfecd5329a9b8
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2022-09-02 11:56:54 +0300

    feat(wasm-utils): Add utils for `emscripten` compiled `wasm` files

--- src/ts-utils/wasm-utils.ts
+++ src/ts-utils/wasm-utils.ts
@@ -1,6 +1,55 @@
 import { readFile } from 'fs/promises';
 import '@chainsafe/ssz';
 import { Type } from '@chainsafe/ssz';
+import { assert } from 'node:console';
+
+var HEAP,
+/** @type {ArrayBuffer} */
+  buffer,
+/** @type {Int8Array} */
+  HEAP8,
+/** @type {Uint8Array} */
+  HEAPU8: Uint8Array,
+/** @type {Int16Array} */
+  HEAP16,
+/** @type {Uint16Array} */
+  HEAPU16,
+/** @type {Int32Array} */
+  HEAP32,
+/** @type {Uint32Array} */
+  HEAPU32,
+/** @type {Float32Array} */
+  HEAPF32,
+/** @type {Float64Array} */
+  HEAPF64;
+
+function alignUp(x:any, multiple:any) {
+  if (x % multiple > 0) {
+    x += multiple - (x % multiple);
+  }
+  return x;
+}
+var Module:any = typeof Module !== 'undefined' ? Module : {};
+
+
+function updateGlobalBufferAndViews(buf:any) {
+  buffer = buf;
+  Module['HEAPU8'] = HEAP8 = new Uint8Array(buf);
+}
+
+
+export function emscripten_realloc_buffer(size: any, memory: WebAssembly.Memory) {
+  try {
+    // round size grow request up to wasm page size (fixed 64KB per spec)
+    memory.grow((size - memory.buffer.byteLength + 65535) >>> 16); // .grow() takes a delta compared to the previous size
+    updateGlobalBufferAndViews(memory.buffer);
+    return 1 /*success*/;
+  } catch(e) {
+    console.log('emscripten_realloc_buffer: Attempted to grow heap from ' + memory.buffer.byteLength  + ' bytes to ' + size + ' bytes, but got error: ' + e);
+  }
+  // implicit 0 return to save code size (caller will cast "undefined" into 0
+  // anyhow)
+}
 
 export async function loadWasm<Exports extends WebAssembly.Exports>({
   from,
@@ -11,9 +60,10 @@ export async function loadWasm<Exports extends WebAssembly.Exports>({
 }) {
   let res: WebAssembly.WebAssemblyInstantiatedSource;
   let memory: WebAssembly.Memory;
+  let HEAPU8: Uint8Array;
   importObject = {
     env: {
-      print: (x: unknown) => console.log(x),
+      print: (x: undefined) => console.log(x),
       wasmQuit: (errOffset: number, errLength: number) => {
         throwWasmException({
           memory,
@@ -21,7 +71,82 @@ export async function loadWasm<Exports extends WebAssembly.Exports>({
           length: errLength,
         });
       },
+      emscripten_notify_memory_growth: (x:undefined) => console.log("emscripten_notify_memory_growth"),
+      emscripten_memcpy_big: (dest: any, src: any, num: any) => {
+        HEAPU8.copyWithin(dest, src, src + num);
+      },
+      emscripten_resize_heap: (requestedSize: any) => {
+        console.log("emscripten_resize_heap")
+        var oldSize = HEAPU8.length;
+        requestedSize = requestedSize >>> 0;
+        // With pthreads, races can happen (another thread might increase the size in between), so return a failure, and let the caller retry.
+        assert(requestedSize > oldSize);
+
+        // Memory resize rules:
+        // 1. Always increase heap size to at least the requested size, rounded up to next page multiple.
+        // 2a. If MEMORY_GROWTH_LINEAR_STEP == -1, excessively resize the heap geometrically: increase the heap size according to
+        //                                         MEMORY_GROWTH_GEOMETRIC_STEP factor (default +20%),
+        //                                         At most overreserve by MEMORY_GROWTH_GEOMETRIC_CAP bytes (default 96MB).
+        // 2b. If MEMORY_GROWTH_LINEAR_STEP != -1, excessively resize the heap linearly: increase the heap size by at least MEMORY_GROWTH_LINEAR_STEP bytes.
+        // 3. Max size for the heap is capped at 2048MB-WASM_PAGE_SIZE, or by MAXIMUM_MEMORY, or by ASAN limit, depending on which is smallest
+        // 4. If we were unable to allocate as much memory, it may be due to over-eager decision to excessively reserve due to (3) above.
+        //    Hence if an allocation fails, cut down on the amount of excess growth, in an attempt to succeed to perform a smaller allocation.
+
+        // A limit is set for how much we can grow. We should not exceed that
+        // (the wasm binary specifies it, so if we tried, we'd fail anyhow).
+        // In CAN_ADDRESS_2GB mode, stay one Wasm page short of 4GB: while e.g. Chrome is able to allocate full 4GB Wasm memories, the size will wrap
+        // back to 0 bytes in Wasm side for any code that deals with heap sizes, which would require special casing all heap size related code to treat
+        // 0 specially.
+        var maxHeapSize = 2147483648;
+        if (requestedSize > maxHeapSize) {
+          console.log(
+            'Cannot enlarge memory, asked to go up to ' +
+              requestedSize +
+              ' bytes, but the limit is ' +
+              maxHeapSize +
+              ' bytes!',
+          );
+          return false;
+        }
+
+        // Loop through potential heap size increases. If we attempt a too eager reservation that fails, cut down on the
+        // attempted size and reserve a smaller bump instead. (max 3 times, chosen somewhat arbitrarily)
+        for (var cutDown = 1; cutDown <= 4; cutDown *= 2) {
+          var overGrownHeapSize = oldSize * (1 + 0.2 / cutDown); // ensure geometric growth
+          // but limit overreserving (default to capping at +96MB overgrowth at most)
+          overGrownHeapSize = Math.min(
+            overGrownHeapSize,
+            requestedSize + 100663296,
+          );
+
+          var newSize = Math.min(
+            maxHeapSize,
+            alignUp(Math.max(requestedSize, overGrownHeapSize), 65536),
+          );
+
+          var replacement = emscripten_realloc_buffer(newSize, memory);
+          if (replacement) {
+            return true;
+          }
+        }
+        console.log(
+          'Failed to grow the heap from ' +
+            oldSize +
+            ' bytes to ' +
+            ' bytes, not enough memory!',
+        );
+        return false;
+      },
+
+      blscurve_blst_min_pubkey_sig_coreInit000: (x: any) => {"blscurve_blst_min_pubkey_sig_coreInit000"},
+      main: (x: any) => {"main"},
+
       ...importObject.env,
+    },
+    wasi_snapshot_preview1: {
+      args_sizes_get: (x: any) => {"args_sizes_get"},
+      args_get: (x: any) => {"args_get"},
+      proc_exit: (x: any) => {"proc_exit"},
     },
   };
 
@@ -39,6 +164,7 @@ export async function loadWasm<Exports extends WebAssembly.Exports>({
     throw new Error('Invalid argument: `from`');
   }
   memory = (res.instance.exports as any).memory;
+  HEAPU8 = new Uint8Array(memory.buffer)
   return res.instance.exports as Exports;
 }
 
