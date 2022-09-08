3eb89bfaae1dbda1119e4bbad437807879663286
Dimo99 <dimodimov1999@gmail.com>
2022-09-08 18:31:56 +0300

    (fix): Fix the invalid proof bug

--- src/circom/circuits/is_supermajority.circom
+++ src/circom/circuits/is_supermajority.circom
@@ -1,11 +1,22 @@
 pragma circom 2.0.3;
 
-include "curve.circom";
-include "../node_modules/circomlib/circuits/bitify.circom";
-include "../node_modules/circomlib/circuits/comparators.circom";
+include "../../../node_modules/circomlib/circuits/comparators.circom";
 
-template IsSuperMajority() {
-  var N = 512;
+
+// Potential problem could be if bitmask is not consisting of 0 and 1
+template IsSuperMajority(N) {
   signal input bitmask[N];
 
+  signal output out;
+
+  var sum = 0;
+  for(var i = 0; i < N; i++) {
+    sum += bitmask[i];
+  }
+
+  component greaterThan = GreaterEqThan(12);
+  greaterThan.in[0] <== sum * 3;
+  greaterThan.in[1] <== 1024;
+
+  out <== greaterThan.out;
 }
--- src/circom/scripts/proof_efficient/get-proof-input.ts
+++ src/circom/scripts/proof_efficient/get-proof-input.ts
@@ -5,6 +5,7 @@ import { writeFileSync } from "fs";
 import { BitVectorType } from "@chainsafe/ssz";
 import * as path from "path";
 import { getFilesInDir } from "../../../ts-utils/data";
+import { aggregateSignatures, init, PointFormat, PublicKey, SecretKey, Signature, verifyAggregate, aggregatePublicKeys, verify } from "@chainsafe/bls";
 
 const hashToField = utils.hashToField;
 
@@ -33,10 +34,10 @@ function getMessage(blockRoot: Uint8Array) {
 }
 
 async function getProof(update) {
-  let points: PointG1[] = update.next_sync_committee.pubkeys.map(x => PointG1.fromHex(x.slice(2))).slice(1, 3);
+  let points: PointG1[] = update.next_sync_committee.pubkeys.map(x => PointG1.fromHex(x.slice(2))).slice(0, 2);
   const SyncCommitteeBits = new BitVectorType(512);
   let bitmask = SyncCommitteeBits.fromJson(update.sync_aggregate.sync_committee_bits);
-  let signature: PointG2 = PointG2.fromSignature(formatHex(update.sync_aggregate.sync_committee_signature));
+  // let signature: PointG2 = PointG2.fromSignature(formatHex(update.sync_aggregate.sync_committee_signature));
   const BeaconBlockHeader = ssz.phase0.BeaconBlockHeader;
   let block_header = BeaconBlockHeader.defaultValue();
   block_header.slot = Number.parseInt(update.attested_header.slot);
@@ -48,18 +49,38 @@ async function getProof(update) {
   let message = getMessage(hash);
   let u = await hashToField(message, 2);
 
+
+  await init("herumi");
+
+  const secretKey1 = SecretKey.fromKeygen();
+  const secretKey2 = SecretKey.fromKeygen();
+
+  const pubkey1 = secretKey1.toPublicKey();
+  const pubkey2 = secretKey2.toPublicKey();
+
+  const signature1 = secretKey1.sign(message);
+  const signature2 = secretKey2.sign(message);
+
+  const aggSignature = aggregateSignatures([signature1.toBytes(), signature2.toBytes()]);
+
+  console.log(verifyAggregate([pubkey1.toBytes(), pubkey2.toBytes()], message, aggSignature));
+
+  const aggPubKey = aggregatePublicKeys([pubkey1.toBytes(), pubkey2.toBytes()]);
+
+  console.log(verify(aggPubKey, message, aggSignature));
+
   let input = {
-    points: points.map(x => [bigint_to_array(55, 7, x.toAffine()[0].value), bigint_to_array(55, 7, x.toAffine()[1].value)]),
-    aggregatedKey: BigInt(update.next_sync_committee.aggregate_pubkey).toString(2).split(''),
+    points: [PointG1.fromHex(secretKey1.toPublicKey().toBytes()), PointG1.fromHex(secretKey2.toPublicKey().toBytes())].map(x => [bigint_to_array(55, 7, x.toAffine()[0].value), bigint_to_array(55, 7, x.toAffine()[1].value)]),
+    // aggregatedKey: BigInt(update.next_sync_committee.aggregate_pubkey).toString(2).split(''),
     bitmask: bigint_to_array(253, 3, BigInt("0b" + bitmask.toBoolArray().map(x => x ? "1" : "0").join(''))).reverse(),
     signature: [
       [
-        bigint_to_array(55, 7, signature.toAffine()[0].c0.value),
-        bigint_to_array(55, 7, signature.toAffine()[0].c1.value)
+        bigint_to_array(55, 7, PointG2.fromSignature(aggSignature).toAffine()[0].c0.value),
+        bigint_to_array(55, 7, PointG2.fromSignature(aggSignature).toAffine()[0].c1.value)
       ],
       [
-        bigint_to_array(55, 7, signature.toAffine()[1].c0.value),
-        bigint_to_array(55, 7, signature.toAffine()[1].c1.value)
+        bigint_to_array(55, 7, PointG2.fromSignature(aggSignature).toAffine()[1].c0.value),
+        bigint_to_array(55, 7, PointG2.fromSignature(aggSignature).toAffine()[1].c1.value)
       ]
     ],
     hash: [
