e498ec5e60bbd953f5905d71ddd75d206a791d94
Zahary Karadjov <zahary@gmail.com>
2022-09-22 18:27:33 +0300

    Allow the circom tests to pass on M1 macs by using the WASM tester

--- beacon-light-client/circom/test/compress.test.ts
+++ beacon-light-client/circom/test/compress.test.ts
@@ -1,13 +1,13 @@
 import { PointG1 } from "@noble/bls12-381";
 import { bigint_to_array } from "../../../libs/typescript/ts-utils/bls";
-import { c } from "circom_tester";
+import { fastestTester } from "./circuit_tester";
 import { expect } from "chai";
 import * as update from "../../../vendor/eth2-light-client-updates/mainnet/updates/00290.json";
 
 describe("Add public keys test", () => {
   it("Test1", async () => {
     let point: PointG1 = update.next_sync_committee.pubkeys.map(x => PointG1.fromHex(x.slice(2)))[0];
-    const circuit = await c("./scripts/compress/compress.circom",);
+    const circuit = await fastestTester("./scripts/compress/compress.circom",);
 
     const result = [bigint_to_array(55, 7, point.toAffine()[0].value), bigint_to_array(55, 7, point.toAffine()[1].value)];
 
