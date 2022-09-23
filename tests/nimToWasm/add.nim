cb48f794ae5e935e1180dc78ab0ab09f930c1da9
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2022-09-23 15:06:42 +0300

    test(LCW): Corrections

--- tests/nimToWasm/add.nim
+++ tests/nimToWasm/add.nim
@@ -1,10 +1,13 @@
+when defined(emcc):
+  {.emit: "#include <emscripten.h>".}
+  {.pragma: wasmPragma, cdecl, exportc, dynlib, codegenDecl: "EMSCRIPTEN_KEEPALIVE $# $#$#".}
+else:
+  {.pragma: wasmPragma, cdecl, exportc, dynlib.}
+
 proc print(value: cdouble) {.importc, cdecl}
 
-proc sum*(a,b: float64): float64 {.cdecl, exportc, dynlib.} =
+proc sum*(a,b: float64): float64 {.wasmPragma.} =
   a + b
 
-proc printAdd*(a,b: float64) {.cdecl, exportc, dynlib} =
+proc printAdd*(a,b: float64) {.wasmPragma.} =
   print(sum(a, b))
-
-proc start*() {.exportc: "_start".} =
-  discard
