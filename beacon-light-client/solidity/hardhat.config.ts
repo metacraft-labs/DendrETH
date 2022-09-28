31e6adc4d80853ad6863c2e9c977be34874fa53f
Petar Kirov <petar.p.kirov@gmail.com>
2022-09-28 19:04:52 +0300

    config(hardhat): Refactor env config loading

--- beacon-light-client/solidity/hardhat.config.ts
+++ beacon-light-client/solidity/hardhat.config.ts
@@ -8,6 +8,27 @@ import '@nomiclabs/hardhat-ethers';
 
 import './tasks';
 
+const envConfig = {
+  USER_PRIVATE_KEY: process.env.USER_PRIVATE_KEY,
+  LOCAL_HARDHAT_PRIVATE_KEY: process.env.LOCAL_HARDHAT_PRIVATE_KEY,
+  INFURA_API_KEY: process.env.INFURA_API_KEY,
+  ETHERSCAN_API_KEY: process.env.ETHERSCAN_API_KEY,
+};
+
+for (const envVar of Object.keys(envConfig)) {
+  if (!envConfig[envVar]) {
+    console.warn(`$${envVar} environment variable is not set`);
+    process.exit(0);
+  }
+}
+
+if (!/^0x[0-9a-fA-F]{64}$/.test(envConfig.USER_PRIVATE_KEY ?? '')) {
+  console.warn(
+    'Setting $USER_PRIVATE_KEY to $LOCAL_HARDHAT_PRIVATE_KEY as fallback',
+  );
+  envConfig.USER_PRIVATE_KEY = envConfig.LOCAL_HARDHAT_PRIVATE_KEY;
+}
+
 export default {
   solidity: {
     version: '0.8.9',
@@ -22,25 +43,25 @@ export default {
   networks: {
     local: {
       url: 'http://127.0.0.1:8545/',
-      accounts: [process.env.LOCAL_HARDHAT_PRIVATE_KEY],
+      accounts: [envConfig.LOCAL_HARDHAT_PRIVATE_KEY],
     },
     hardhat: {
       blockGasLimit: 30000000,
       allowUnlimitedContractSize: true,
     },
     ropsten: {
-      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.USER_PRIVATE_KEY],
+      url: `https://ropsten.infura.io/v3/${envConfig.INFURA_API_KEY}`,
+      accounts: [envConfig.USER_PRIVATE_KEY],
     },
     mainnet: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.USER_PRIVATE_KEY],
+      url: `https://goerli.infura.io/v3/${envConfig.INFURA_API_KEY}`,
+      accounts: [envConfig.USER_PRIVATE_KEY],
     },
   },
   mocha: {
     timeout: 100000000,
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: envConfig.ETHERSCAN_API_KEY,
   },
 };
