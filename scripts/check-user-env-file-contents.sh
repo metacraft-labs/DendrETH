5068e091dc68c1ecfc99fe09add2bf37d4776ebc
Zahary Karadjov <zahary@gmail.com>
2022-10-03 17:31:24 +0300

    Properly enforce the user .env file requirements even when using nix develop

---
+++ scripts/check-user-env-file-contents.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+STATUS=0
+
+RED='\033[0;31m'
+BOLD='\033[1;37m'
+NC='\033[0m' # No Color
+
+check_required_var () {
+  if [ -z "${!1}" ]; then
+    printf "${RED}error:${NC} Please add the required ${BOLD}$1${NC} variable to your local .env file. "
+    echo $2
+    STATUS=1
+  fi
+}
+
+check_required_var INFURA_API_KEY \
+  "It's required for deploying and interacting with the Solidity contracts on public testnets. You can obtain such as key by creating an account at https://infura.io"
+
+check_required_var USER_PRIVATE_KEY \
+  "This is your personal testing private key that will be used both in the local network and on the public testnets. It should be hex-encoded."
+
+check_required_var ETHERSCAN_API_KEY \
+  "This is used for Etherscan contract verification. You can obtain such as key by creating an account at https://etherscan.io"
+
+exit $STATUS
--- shell.nix
+++ shell.nix
@@ -57,10 +57,21 @@ in
     ];
 
     shellHook = ''
+      set -e
+
       export NODE_OPTIONS="--experimental-vm-modules"
       export PATH="$PATH:$PWD/node_modules/.bin";
       export CC=clang
       export LOCAL_NIM_LIB="$PWD/vendor/nim/lib"
+      export LOCAL_HARDHAT_PRIVATE_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+
+      if [ -f .env ]; then
+        set -a
+        source .env
+        set +a
+      fi
+
+      scripts/check-user-env-file-contents.sh
 
       figlet "DendrETH"
     '';
