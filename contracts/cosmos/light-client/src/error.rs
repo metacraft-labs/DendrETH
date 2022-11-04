1432e6715d04ef73ff51f9fca7f85634aef6e89c
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2022-11-04 19:28:44 +0200

    feat(cosmos-contract): Implement Cosmos Light Client smart contract

--- contracts/cosmos/light-client/src/error.rs
+++ contracts/cosmos/light-client/src/error.rs
@@ -3,11 +3,14 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ContractError {
-    #[error("{0}")]
-    Std(#[from] StdError),
+  #[error("{0}")] Std(#[from] StdError),
 
-    #[error("Unauthorized")]
-    Unauthorized {},
-    // Add any other custom errors you like here.
-    // Look at https://docs.rs/thiserror/1.0.21/thiserror/ for details.
+  #[error("Unauthorized")] Unauthorized {},
+  // Add any other custom errors you like here.
+  // Look at https://docs.rs/thiserror/1.0.21/thiserror/ for details.
+}
+
+#[no_mangle]
+pub fn panic_on_error(text: *mut u8, len: usize) {
+  panic!("{}", unsafe { String::from_raw_parts(text, len, 0) })
 }
