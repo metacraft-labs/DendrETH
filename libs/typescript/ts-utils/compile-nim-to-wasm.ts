18c3331cd3736ce0a06058741e26b4945d1f3d18
EmilIvanichkovv <emil.ivanichkov@gmail.com>
2022-11-09 22:35:28 +0200

    refactor(compile-nim-to-wasm): Add way to set `optional flags`
    This is useful when we need to use `nim-wasm` with specific flags or defines

--- libs/typescript/ts-utils/compile-nim-to-wasm.ts
+++ libs/typescript/ts-utils/compile-nim-to-wasm.ts
@@ -5,13 +5,16 @@ import { exec as exec_ } from 'node:child_process';
 
 const exec = promisify(exec_);
 
-export async function compileNimFileToWasm(nimSourceFilepath: string) {
+export async function compileNimFileToWasm(
+  nimSourceFilepath: string,
+  optionalFlags?: string,
+) {
   const inputFileName = resolve(nimSourceFilepath);
   const outputFileName = inputFileName.replace(/\.nim$/, '.wasm');
   console.info(`➤ rm ${outputFileName}`);
   await rm(outputFileName, { force: true });
   const useEmcc = process.env.USE_EMCC === '1' ? '-d:emcc' : '';
-  const command = `nim-wasm c --lib:"./vendor/nim/lib" --d:lightClientWASM ${useEmcc} -o:${outputFileName} ${inputFileName}`;
+  const command = `nim-wasm c --lib:"./vendor/nim/lib" ${useEmcc} -o:${outputFileName} ${optionalFlags} ${inputFileName}`;
   console.info(`➤ ${command}`);
   await exec(command);
   return { outputFileName };
