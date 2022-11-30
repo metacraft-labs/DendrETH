bf2bd195cc5701bc9ddeacd7fd5a35954b8b62e4
Kristin Kirkov <krischanss99@gmail.com>
2022-11-30 14:56:28 +0200

    feat(cosmos-verifier): Implement command line tool for interacting with cosmos

---
+++ contracts/cosmos/verifier/lib/nim/config.nim
@@ -0,0 +1,50 @@
+import
+  confutils/defs
+
+type
+  StartUpCommand* = enum
+    noCommand
+    init
+    update
+    query
+
+type
+  CosmosVeryfierConf* = object
+
+    chainId* {.
+      defaultValue: "testing"
+      desc: "Chain ID" }: string
+
+    rpc* {.
+      defaultValue: "http://localhost:26657"
+      desc: "Address of the network" }: string
+
+    wallet* {.
+      defaultValue: "fred --keyring-backend test --keyring-dir $HOME/.wasmd_keys"
+      desc: "Wallet used for signing all transactions"}:string
+
+    case cmd* {.
+      command
+      defaultValue: noCommand }: StartUpCommand
+    of noCommand:
+      discard
+
+    of init:
+      vKeyPath* {.
+        desc: "Path to the verification key"}: string
+      currentHeaderPath* {.
+        desc: "Path to the new header"}: string
+      codeId*{.
+        desc: "Contract ID"}: string
+
+    of update:
+      proofPath* {.
+        desc: "Path to the proof"}: string
+      newHeaderPath* {.
+        desc: "Path to the new header"}: string
+      contract* {.
+        desc: "Contract Hash"}: string
+
+    of query:
+      contract2* {.
+        desc: "Contract Hash"}: string
