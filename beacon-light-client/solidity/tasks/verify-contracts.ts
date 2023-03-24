2f949d20b781499750da727442d1a8dabb008f18
Dimo99 <dimodimov1999@gmail.com>
2023-03-24 18:39:27 +0200

    refactor(solidity): Deploy should use beaconApiInterface

--- beacon-light-client/solidity/tasks/verify-contracts.ts
+++ beacon-light-client/solidity/tasks/verify-contracts.ts
@@ -1,12 +1,22 @@
 import { task } from 'hardhat/config';
+import { checkConfig } from '../../../libs/typescript/ts-utils/common-utils';
+import { BeaconApi } from '../../../relay/implementations/beacon-api';
 import { getConstructorArgs } from './utils';
 
 task('verify-contracts', 'Verify')
   .addParam('lightclient', 'The address of the BeaconLightClient contract')
   .addParam('slot', 'The slot ')
   .setAction(async (args, { run }) => {
+    const config = {
+      BEACON_REST_API: process.env.BEACON_REST_API,
+    };
+
+    checkConfig(config);
+
+    const beaconApi = new BeaconApi(config.BEACON_REST_API!);
+
     await run('verify:verify', {
       address: args.lightclient,
-      constructorArguments: await getConstructorArgs(args.slot),
+      constructorArguments: await getConstructorArgs(beaconApi, args.slot),
     });
   });
