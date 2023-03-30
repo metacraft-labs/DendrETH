1054c9de231344731a695de723b6177f3ed287ce
Petar Kirov <petar.p.kirov@gmail.com>
2023-03-30 17:40:44 +0300

    feat(nix): Replace `rapidsnark` git submodule with Nix package

--- shell.nix
+++ shell.nix
@@ -73,6 +73,8 @@ in
         # Developer tool to help you get up and running quickly with a new Rust
         # project by leveraging a pre-existing git repository as a template.
         cargo-generate
+
+        metacraft-labs.rapidsnark
       ]
       ++ lib.optionals (!stdenv.isDarwin) [
         metacraft-labs.solana
