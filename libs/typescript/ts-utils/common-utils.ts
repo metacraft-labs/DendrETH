c45346416f3a348cd490355445f5b6ba068f5f18
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2023-04-19 13:41:59 +0300

    feat(test): Append new test data to the gas usage files

--- libs/typescript/ts-utils/common-utils.ts
+++ libs/typescript/ts-utils/common-utils.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs';
+
 export function byteArrayToNumber(byteArray) {
   var value = 0;
   for (var i = byteArray.length - 1; i >= 0; i--) {
@@ -13,4 +15,16 @@ export function checkConfig(config: any) {
       process.exit(0);
     }
   }
+}
+
+// Open Json file and append data to it
+export function appendJsonFile(filePath: string, data: any) {
+  let fileData: any[] = [];
+  try {
+    fileData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  } catch (e) {
+    console.warn(`Unable to read file ${filePath}`);
+  }
+  fileData.push(data);
+  fs.writeFileSync(filePath, JSON.stringify(fileData, null, 2));
 }
