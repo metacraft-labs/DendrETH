63fe7fa2bb7f1a2d079774f0375be91932312c51
Petar Kirov <petar.p.kirov@gmail.com>
2023-04-27 17:36:11 +0300

    refactor(scripts): Create build-nix-shell script that will correctly build the devshell with submodules in the CI

---
+++ scripts/build-nix-shell.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+
+# shellcheck source=./get-host-system
+. "$SCRIPT_DIR/get-host-system"
+
+system="$(get_host_system)"
+
+set -x
+
+if [[ -n "${GITHUB_ENV:-}" ]]; then
+  git config --global url."git@github.com:".insteadOf https://github.com/
+  git config --global url."git://".insteadOf https://
+fi
+
+nix build --json --print-build-logs ".#devShells.$system.default"
