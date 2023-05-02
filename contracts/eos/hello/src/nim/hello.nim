ab6129a289dd8977c8451ae84c92ab25144a3610
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2023-05-02 19:03:21 +0300

    feat(eos/contracts): Introduce way to call `print` from nim

--- contracts/eos/hello/src/nim/hello.nim
+++ contracts/eos/hello/src/nim/hello.nim
@@ -3,5 +3,11 @@ when defined(emcc):
 else:
   {.pragma: wasmPragma, cdecl, exportc, dynlib.}
 
+# For debugging purposes. This way we can call print from the nim code in the EOS contract
+when defined(DEBUG):
+  const irr = "</nix/store/88nw85nvskb71hsch8m22bka1hckq3k5-cdt/cdt/include/eosiolib/core/eosio/print.hpp>"
+  proc print(value: cstring) {. header:irr, importc:"eosio::internal_use_do_not_use::prints", cdecl}
+
+
 proc helloFromNim(): cstring {.wasmPragma.} =
   return "Hello from Nim"
