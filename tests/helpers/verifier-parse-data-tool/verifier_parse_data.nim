be110c08db86031217bdbbc80d9697757cdbdf15
Kristin Kirkov <krischanss99@gmail.com>
2023-05-17 12:11:23 +0300

    feat(verifier): Add slot and domain to the EOS contract

--- tests/helpers/verifier-parse-data-tool/verifier_parse_data.nim
+++ tests/helpers/verifier-parse-data-tool/verifier_parse_data.nim
@@ -91,15 +91,18 @@ proc execCommand*(): string =
       let newOptimisticHeader = hexToByteArray[32](updateJson["attestedHeaderRoot"].str)
       let newFinalizedHeader = hexToByteArray[32](updateJson["finalizedHeaderRoot"].str)
       let newExecutionStateRoot = hexToByteArray[32](updateJson["finalizedExecutionStateRoot"].str)
+      let slot = updateJson["attestedHeaderSlot"]
 
-      let update= "'{\"proof\":" & $proof.toHex() & ",\"new_optimistic_header_root\": \"" & $newOptimisticHeader.toHex() & "\" ,\"new_finalized_header_root\": \"" & $newFinalizedHeader.toHex() & "\" ,\"new_execution_state_root\": \"" & $newExecutionStateRoot.toHex() & "\" } '"
+      let update= "'{\"proof\": \"" & $proof.toHex() & "\" ,\"new_optimistic_header_root\": \"" & $newOptimisticHeader.toHex() & "\" ,\"new_finalized_header_root\": \"" & $newFinalizedHeader.toHex() & "\" ,\"new_execution_state_root\": \"" & $newExecutionStateRoot.toHex()  & "\" ,\"new_slot\": \"" & $slot & "\" } '"
 
       echo update
     of StartUpCommand.initDataEOS:
 
       let vkey = createVerificationKey(conf.verificationKeyPathEOS)
       let hex = hexToByteArray[32](conf.initHeaderRootEOS)
-      let init = "\'{\"key\":\"dendreth\", \"verification_key\": \"" & $vkey.toHex() & "\" ,\"current_header_hash\": \"" & $hex.toHex() & "\" }\'"
+      let domain = hexToByteArray[32](conf.domainEOS)
+
+      let init = "\'{\"key\":\"dendreth\", \"verification_key\": \"" & $vkey.toHex() & "\" ,\"current_header_hash\": \"" & $hex.toHex() & "\" ,\"current_slot\": \"" & $5609069 & "\" ,\"domain\": \"" & $domain.toHex() &  "\" }\'"
       echo init
 
 let a = execCommand()
