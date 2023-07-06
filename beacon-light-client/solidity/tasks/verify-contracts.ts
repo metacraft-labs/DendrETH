86fdc03a48bd3c15226ee5b8ca0b1a09f2e564a3
Dimo99 <dimodimov1999@gmail.com>
2023-07-06 16:29:59 +0300

    feat(relay): Add config for beacon node values

--- beacon-light-client/solidity/tasks/verify-contracts.ts
+++ beacon-light-client/solidity/tasks/verify-contracts.ts
@@ -1,21 +1,19 @@
 import { task } from 'hardhat/config';
-import { checkConfig } from '../../../libs/typescript/ts-utils/common-utils';
 import { BeaconApi } from '../../../relay/implementations/beacon-api';
 import { getConstructorArgs } from './utils';
-import * as networkConfig from '../../../relay/constants/network_config.json';
-import { Config } from '../../../relay/constants/constants';
+import { getNetworkConfig } from '../../../relay/utils/get_current_network_config';
 
 task('verify-contracts', 'Verify')
   .addParam('lightclient', 'The address of the BeaconLightClient contract')
   .addParam('slot', 'The slot ')
   .addParam('follownetwork', 'The network to follow')
   .setAction(async (args, { run }) => {
-    if (!networkConfig[args.follownetwork]) {
+    if (args.follownetwork !== 'pratter' && args.follownetwork !== 'mainnet') {
       console.warn('This follownetwork is not specified in networkconfig');
       return;
     }
 
-    const currentConfig = networkConfig[args.follownetwork] as Config;
+    const currentConfig = getNetworkConfig(args.follownetwork);
 
     const beaconApi = new BeaconApi(currentConfig.BEACON_REST_API!);
 
---
+++ relay/utils/get_current_network_config.ts
@@ -0,0 +1,21 @@
+import {
+  checkConfig,
+} from '../../libs/typescript/ts-utils/common-utils';
+import { Config } from '../constants/constants';
+import * as network_config from '../constants/network_config.json';
+
+export function getNetworkConfig(network: 'pratter' | 'mainnet'): Config {
+  const config = {
+    BEACON_REST_API_PRATTER: process.env.BEACON_REST_API_PRATTER,
+    BEACON_REST_API_MAINNET: process.env.BEACON_REST_API_MAINNET,
+  };
+
+  checkConfig(config);
+
+  network_config[network]["BEACON_REST_API"] =
+    network === 'pratter'
+      ? config.BEACON_REST_API_PRATTER!.split(',')
+      : config.BEACON_REST_API_MAINNET!.split(',');
+
+  return network_config[network] as Config;
+}
