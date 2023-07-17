9457b87b6fce1b8c04883f7afdf96fd2c5641c64
Dimo99 <dimodimov1999@gmail.com>
2023-07-17 15:45:39 +0300

    fix(ci): Add linting check

--- relay/utils/smart_contract_utils.ts
+++ relay/utils/smart_contract_utils.ts
@@ -1,13 +1,14 @@
-import { IBeaconApi } from "../abstraction/beacon-api-interface";
-import { ISmartContract } from "../abstraction/smart-contract-abstraction";
+import { IBeaconApi } from '../abstraction/beacon-api-interface';
+import { ISmartContract } from '../abstraction/smart-contract-abstraction';
 
-export async function getSlotOnChain(smartContract: ISmartContract, beaconApi: IBeaconApi) {
+export async function getSlotOnChain(
+  smartContract: ISmartContract,
+  beaconApi: IBeaconApi,
+) {
   const header_root_on_chain = await smartContract.optimisticHeaderRoot();
 
   console.log('header on chain', header_root_on_chain);
 
-  const lastSlotOnChain = await beaconApi.getBlockSlot(
-    header_root_on_chain
-  );
+  const lastSlotOnChain = await beaconApi.getBlockSlot(header_root_on_chain);
   return lastSlotOnChain;
 }
