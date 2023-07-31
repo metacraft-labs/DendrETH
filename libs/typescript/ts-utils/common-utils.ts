50a876e1d8973e1295b328c40d2250f0f05f3116
Kristin Kirkov <krischanss99@gmail.com>
2023-07-31 17:35:44 +0300

    feat(verify-ffjs): Add a Verifier using ffjavascript

--- libs/typescript/ts-utils/common-utils.ts
+++ libs/typescript/ts-utils/common-utils.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import { promisify } from 'node:util';
 import { exec as exec_ } from 'node:child_process';
+import { sha256 } from 'ethers/lib/utils';
 
 const exec = promisify(exec_);
 
@@ -63,4 +64,117 @@ export function getEnvString(varName: string) {
     process.env[varName],
     `Env variable '${varName}' is missing.`,
   );
+}
+
+export function unstringifyBigInts(o) {
+  if (typeof o == 'string' && /^[0-9]+$/.test(o)) {
+    return BigInt(o);
+  } else if (typeof o == 'string' && /^0x[0-9a-fA-F]+$/.test(o)) {
+    return BigInt(o);
+  } else if (Array.isArray(o)) {
+    return o.map(unstringifyBigInts);
+  } else if (typeof o == 'object') {
+    if (o === null) return null;
+    const res = {};
+    const keys = Object.keys(o);
+    keys.forEach(k => {
+      res[k] = unstringifyBigInts(o[k]);
+    });
+    return res;
+  } else {
+    return o;
+  }
+}
+
+function hexToBitString(num) {
+  var bitmask: string = '';
+  switch (num) {
+    case '0': {
+      bitmask = '0000';
+      break;
+    }
+    case '1': {
+      bitmask = '0001';
+      break;
+    }
+    case '2': {
+      bitmask = '0010';
+      break;
+    }
+    case '3': {
+      bitmask = '0011';
+      break;
+    }
+    case '4': {
+      bitmask = '0100';
+      break;
+    }
+    case '5': {
+      bitmask = '0101';
+      break;
+    }
+    case '6': {
+      bitmask = '0110';
+      break;
+    }
+    case '7': {
+      bitmask = '0111';
+      break;
+    }
+    case '8': {
+      bitmask = '1000';
+      break;
+    }
+    case '9': {
+      bitmask = '1001';
+      break;
+    }
+    case 'a': {
+      bitmask = '1010';
+      break;
+    }
+    case 'b': {
+      bitmask = '1011';
+      break;
+    }
+    case 'c': {
+      bitmask = '1100';
+      break;
+    }
+    case 'd': {
+      bitmask = '1101';
+      break;
+    }
+    case 'e': {
+      bitmask = '1110';
+      break;
+    }
+    case 'f': {
+      bitmask = '1111';
+      break;
+    }
+  }
+  return bitmask;
+}
+
+export function bitTo2BigInts(hexNum) {
+  var firstNumInBits: string = '0b000';
+  var secondNumInBits: string = '0b';
+  for (var i = 0; i < 253; i += 1) {
+    secondNumInBits = secondNumInBits.concat('0');
+  }
+
+  for (var i = 2; i < 65; i += 1) {
+    firstNumInBits = firstNumInBits.concat(
+      hexToBitString(sha256(hexNum)[i]).toString(),
+    );
+  }
+  const lastBitArray = hexToBitString(sha256(hexNum)[65]);
+
+  firstNumInBits = firstNumInBits.concat(lastBitArray[0].toString());
+  secondNumInBits = secondNumInBits.concat(lastBitArray[1].toString());
+  secondNumInBits = secondNumInBits.concat(lastBitArray[2].toString());
+  secondNumInBits = secondNumInBits.concat(lastBitArray[3].toString());
+
+  return [BigInt(firstNumInBits), BigInt(secondNumInBits)];
 }
