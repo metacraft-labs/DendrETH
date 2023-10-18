58f8d95a3b2ebaee27c1af659d32eb1604024feb
Aneta Tsvetkova <anetastsvetkova@gmail.com>
2023-10-18 17:36:03 +0300

    Revert deleted files

--- casper-finality-proofs/src/test_engine/utils/parse_file.rs
+++ casper-finality-proofs/src/test_engine/utils/parse_file.rs
@@ -2,7 +2,6 @@ use serde::Deserialize;
 use std::fs::File;
 use std::io::Read;
 
-#[allow(dead_code)] // We allow dead_code since this is used in tests
 pub fn read_fixture<T: for<'a> Deserialize<'a>>(filename: &str) -> T {
     let mut file = File::open(filename).unwrap();
     let mut context = String::new();
