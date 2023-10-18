f0f22a8857fe64572b60530a3207011911d93bde
Nikolay Kostadinov <nikolaykostadinov21@gmail.com>
2023-10-18 17:07:25 +0300

    fix debug position_mod_256

--- casper-finality-proofs/src/utils/byte_variable.rs
+++ casper-finality-proofs/src/utils/byte_variable.rs
@@ -3,7 +3,7 @@ use array_macro::array;
 
 pub fn constant<L: PlonkParameters<D>, const D: usize>(
     builder: &mut CircuitBuilder<L, D>,
-    value: ByteVariable<L::Field>,
+    value: u8,
 ) -> ByteVariable {
     ByteVariable(array![i => BoolVariable::constant(builder, (value >> (7 - i)) & 1 == 1); 8])
 }
