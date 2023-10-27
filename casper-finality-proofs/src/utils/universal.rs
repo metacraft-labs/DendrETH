f47a57e9f6edd5ed7f884d8ca4516dea47561b2c
Nikolay Kostadinov <nikolaykostadinov21@gmail.com>
2023-10-27 13:38:39 +0300

    Move conversion of bytes32 to variable and bits to variable in separate functions. Implement test case for 9999 input data for mainnet. Remove div_rem as builder.rem already exists

--- casper-finality-proofs/src/utils/universal.rs
+++ casper-finality-proofs/src/utils/universal.rs
@@ -1,5 +1,6 @@
 use plonky2::iop::target::BoolTarget;
-use plonky2x::prelude::{BoolVariable, CircuitBuilder, PlonkParameters, Variable, CircuitVariable, U64Variable};
+use plonky2::field::types::Field;
+use plonky2x::prelude::{BoolVariable, CircuitBuilder, PlonkParameters, Variable, CircuitVariable, Bytes32Variable};
 use itertools::Itertools;
 
 /// Fails if i1 != true.
@@ -25,28 +26,7 @@ pub fn le_sum<L: PlonkParameters<D>, const D: usize>(
     Variable(builder.api.le_sum(bits.into_iter()))
 }
 
-pub fn div_rem_u64<L: PlonkParameters<D>, const D: usize>(
-    builder: &mut CircuitBuilder<L, D>,
-    lhs: U64Variable,
-    rhs: U64Variable,
-) -> U64Variable {
-    let quotient = builder.div(lhs, rhs);
-    let quotient_times_rhs = builder.mul(quotient, rhs);
-
-    builder.sub(lhs, quotient_times_rhs)
-}
-
-pub fn div_rem<L: PlonkParameters<D>, const D: usize>(
-    builder: &mut CircuitBuilder<L, D>,
-    lhs: Variable,
-    rhs: Variable,
-) -> Variable {
-    let quotient = builder.div(lhs, rhs);
-    let quotient_times_rhs = builder.mul(quotient, rhs);
-
-    builder.sub(lhs, quotient_times_rhs)
-}
-
+/// Exponentiate `base` to the power of `exponent`, given by its little-endian bits.
 pub fn exp_from_bits<L: PlonkParameters<D>, const D: usize>(
     builder: &mut CircuitBuilder<L, D>,
     base: Variable,
@@ -54,4 +34,31 @@ pub fn exp_from_bits<L: PlonkParameters<D>, const D: usize>(
 ) -> Variable {
     Variable(builder.api.exp_from_bits(base.0, exponent_bits.into_iter()
     .map(|x| BoolTarget::new_unsafe(x.0 .0))))
+}
+
+/// Converts Bytes32Variable's bits to little-endian bit representation and returns the accumulation of each bit by power of 2.
+pub fn bytes32_to_variable<L: PlonkParameters<D>, const D: usize>(
+    builder: &mut CircuitBuilder<L, D>,
+    bytes: Bytes32Variable,
+    start_idx: usize,
+    end_idx: usize,
+) -> Variable {
+    let const_2: Variable = builder.constant(L::Field::from_canonical_usize(2));
+    let mut power_of_2 = builder.constant(L::Field::from_canonical_usize(1));
+    let mut result = builder.constant(L::Field::from_canonical_usize(0));
+    let mut bits: Vec<BoolVariable> = Vec::new();
+
+    for i in start_idx..end_idx {
+        for j in 0..8 {
+            bits.push(bytes.0 .0[7 - i].0[j]);
+        }
+    }
+
+    for i in 0..32 {
+        let addend = builder.mul(bits[31 - i].0, power_of_2);
+        result = builder.add(addend, result);
+        power_of_2 = builder.mul(const_2, power_of_2);
+    }
+
+    result
 }
--- casper-finality-proofs/src/utils/variable.rs
+++ casper-finality-proofs/src/utils/variable.rs
@@ -1,3 +1,4 @@
+use plonky2::field::types::Field;
 use plonky2x::prelude::{BoolVariable, ByteVariable, CircuitBuilder, PlonkParameters, Variable};
 use itertools::Itertools;
 
@@ -22,4 +23,21 @@ pub fn to_bits<const N: usize, L: PlonkParameters<D>, const D: usize>(
         .collect_vec()
         .try_into()
         .unwrap()
+}
+
+pub fn bits_to_variable<L: PlonkParameters<D>, const D: usize>(
+    builder: &mut CircuitBuilder<L, D>,
+    bits: Vec<BoolVariable>,
+    bits_len: usize
+) -> Variable {
+    let const_2: Variable = builder.constant(L::Field::from_canonical_usize(2));
+    let mut power_of_2 = builder.constant(L::Field::from_canonical_usize(1));
+    let mut result = builder.constant(L::Field::from_canonical_usize(0));
+    for i in 0..bits_len {
+        let addend = builder.mul(bits[i].0, power_of_2);
+        result = builder.add(addend, result);
+        power_of_2 = builder.mul(const_2, power_of_2);
+    }
+
+    result
 }
