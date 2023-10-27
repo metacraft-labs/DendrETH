5c0d37365554878aeaa7bcdff2cae56c9a2adea8
Hristo Staykov <izo8611@gmail.com>
2023-10-27 18:10:17 +0300

    Code foramtting added.

--- vendor/zkllvm-metacraft-circuits/src/tests/compute_shuffled_index_test/compute_shuffled_index_test.cpp
+++ vendor/zkllvm-metacraft-circuits/src/tests/compute_shuffled_index_test/compute_shuffled_index_test.cpp
@@ -6,7 +6,7 @@
 #include <array>
 
 #include <llvm/ObjectYAML/YAML.h>
-#include <iostream> 
+#include <iostream>
 #include <fstream>
 #include <streambuf>
 
@@ -14,37 +14,37 @@
 #include "utils/byte_utils.h"
 #include "utils/file_utils.h"
 
-using llvm::yaml::Output;
 using llvm::yaml::Input;
-using llvm::yaml::MappingTraits;
 using llvm::yaml::IO;
+using llvm::yaml::MappingTraits;
+using llvm::yaml::Output;
 
 using namespace circuit_byte_utils;
 using namespace file_utils;
 
 using std::cout;
 
-uint64_t compute_shuffled_index(
-        uint64_t index,
-        uint64_t index_count,
-        std::array<Byte, 32> seed,
-        int SHUFFLE_ROUND_COUNT = 90) {
+uint64_t compute_shuffled_index(uint64_t index,
+                                uint64_t index_count,
+                                std::array<Byte, 32>
+                                    seed,
+                                int SHUFFLE_ROUND_COUNT = 90) {
     assert_true(index < index_count);
 
-    std::array<Byte, 32+1+4> source_buffer{};
+    std::array<Byte, 32 + 1 + 4> source_buffer {};
 
     //!!! sha256_to_bytes_array(seed, source_buffer);
     std::copy(seed.begin(), seed.end(), source_buffer.begin());
 
     // Swap or not (https://link.springer.com/content/pdf/10.1007%2F978-3-642-32009-5_1.pdf)
     // See the 'generalized domain' algorithm on page 3
-    for(Byte current_round = 0; current_round < SHUFFLE_ROUND_COUNT; current_round++) {
+    for (Byte current_round = 0; current_round < SHUFFLE_ROUND_COUNT; current_round++) {
         source_buffer[32] = current_round;
 
         //!!! auto eth2digest = hash<hashes::sha2<256>>(source_buffer.begin(), source_buffer.begin() + 33);
         std::array<Byte, 32> eth2digest_bytes;
-        picosha2::hash256(source_buffer.begin(), source_buffer.begin() + 33, 
-                          eth2digest_bytes.begin(), eth2digest_bytes.end());
+        picosha2::hash256(source_buffer.begin(), source_buffer.begin() + 33, eth2digest_bytes.begin(),
+                          eth2digest_bytes.end());
         ///!!! sha256_to_bytes_array(eth2digest, eth2digest_bytes);
         auto first8bytes = take_n_elements<Byte, eth2digest_bytes.size(), 8>(eth2digest_bytes);
         // PrintContainer(first8bytes);
@@ -59,19 +59,17 @@ uint64_t compute_shuffled_index(
         }
         ///!!! auto source = hash<hashes::sha2<256>>(source_buffer.begin(), source_buffer.end());
         std::array<Byte, 32> source_to_bytes;
-        picosha2::hash256(source_buffer.begin(), source_buffer.end(), 
-                          source_to_bytes.begin(), source_to_bytes.end());
+        picosha2::hash256(source_buffer.begin(), source_buffer.end(), source_to_bytes.begin(), source_to_bytes.end());
         ///!!! sha256_to_bytes_array(source, source_to_bytes);
         auto byte_value = source_to_bytes[(position % 256) >> 3];
         auto bit = (byte_value >> (position % 8)) % 2;
 
-        if(bit != 0) {
+        if (bit != 0) {
             index = flip;
         }
     }
 
     return index;
-
 }
 
 struct TestInput {
@@ -82,16 +80,16 @@ struct TestInput {
 
 namespace llvm {
     namespace yaml {
-        template <>
+        template<>
         struct MappingTraits<TestInput> {
-          static void mapping(IO &io, TestInput &info) {
-            io.mapRequired("seed",         info.seed);
-            io.mapOptional("count",        info.count);
-            io.mapOptional("mapping",      info.mapping);
-          }
+            static void mapping(IO& io, TestInput& info) {
+                io.mapRequired("seed", info.seed);
+                io.mapOptional("count", info.count);
+                io.mapOptional("mapping", info.mapping);
+            }
         };
-    }
-}
+    }    // namespace yaml
+}    // namespace llvm
 
 int main(int argc, char* argv[]) {
 
@@ -99,22 +97,21 @@ int main(int argc, char* argv[]) {
 
     std::array<Byte, 32> source_buffer;
 
-    //sha = hash<hashes::sha2<256>>(source_buffer.begin(), source_buffer.end());
+    // sha = hash<hashes::sha2<256>>(source_buffer.begin(), source_buffer.end());
 
-    auto process_test_input = [] (const std::vector<path>& cases, int SHUFFLE_ROUND_COUNT) {
-        for(const auto& v : cases) {
+    auto process_test_input = [](const std::vector<path>& cases, int SHUFFLE_ROUND_COUNT) {
+        for (const auto& v : cases) {
             std::cout << v.string() << ":\n";
 
             std::ifstream t(v.string());
-            std::string yaml_content((std::istreambuf_iterator<char>(t)),
-                                      std::istreambuf_iterator<char>());
+            std::string yaml_content((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
 
             TestInput doc;
 
             Input yin(yaml_content);
             yin >> doc;
 
-            if ( yin.error() ) {
+            if (yin.error()) {
                 std::cerr << "Failes to process " << v.string() << "\n";
                 return false;
             }
@@ -132,35 +129,29 @@ int main(int argc, char* argv[]) {
             // }
             // std::cout << "\n";
             std::vector<uint64_t> mapping_result;
-            for(size_t i = 0; i < doc.mapping.size(); i++) {
+            for (size_t i = 0; i < doc.mapping.size(); i++) {
                 auto result = compute_shuffled_index(i, doc.mapping.size(), seed_bytes, SHUFFLE_ROUND_COUNT);
                 mapping_result.push_back(result);
             }
-            for(size_t i = 0; i < mapping_result.size(); i++) {
+            for (size_t i = 0; i < mapping_result.size(); i++) {
                 assert_true(mapping_result[i] == doc.mapping[i]);
             }
-
         }
         return true;
     };
 
     std::vector<path> result;
     path my_path("./consensus-spec-tests");
-    find_matching_files(my_path, 
-        std::vector<std::string>{
-            "minimal",
-            "mapping.yaml"}, result);
-    
-    if(!process_test_input(result, 10)) {
+    find_matching_files(my_path, std::vector<std::string> {"minimal", "mapping.yaml"}, result);
+
+    if (!process_test_input(result, 10)) {
         return 1;
     }
 
     result.clear();
-    find_matching_files(my_path, std::vector<std::string>{
-        "mainnet",
-        "mapping.yaml"}, result);
+    find_matching_files(my_path, std::vector<std::string> {"mainnet", "mapping.yaml"}, result);
 
-    if(!process_test_input(result, 90)) {
+    if (!process_test_input(result, 90)) {
         return 1;
     }
 
--- vendor/zkllvm-metacraft-circuits/src/utils/file_utils.h
+++ vendor/zkllvm-metacraft-circuits/src/utils/file_utils.h
@@ -2,34 +2,27 @@
 using namespace boost::filesystem;
 
 namespace file_utils {
-    void find_matching_files(
-        const path& dir_path,                     // in this directory,
-        const std::vector<std::string>& patterns, // search for this name,
-        std::vector<path>& path_found)            // placing path here if found
+    void find_matching_files(const path& dir_path,                        // in this directory,
+                             const std::vector<std::string>& patterns,    // search for this name,
+                             std::vector<path>& path_found)               // placing path here if found
     {
-        auto check_matching = [](const std::string& file_path,
-                                 const std::vector<std::string> & patterns) {
-            for(const auto& v : patterns) {
-                if(file_path.find(v) == std::string::npos) {
+        auto check_matching = [](const std::string& file_path, const std::vector<std::string>& patterns) {
+            for (const auto& v : patterns) {
+                if (file_path.find(v) == std::string::npos) {
                     return false;
                 }
             }
             return true;
         };
-        if ( !exists( dir_path ) ) return;
-        directory_iterator end_itr; // default construction yields past-the-end
-        for (directory_iterator itr( dir_path );
-             itr != end_itr;
-             ++itr )
-        {
-            if (is_directory(itr->status()))
-            {
+        if (!exists(dir_path))
+            return;
+        directory_iterator end_itr;    // default construction yields past-the-end
+        for (directory_iterator itr(dir_path); itr != end_itr; ++itr) {
+            if (is_directory(itr->status())) {
                 find_matching_files(itr->path(), patterns, path_found);
-            }
-            else if (check_matching(itr->path().string(), patterns))
-            {
+            } else if (check_matching(itr->path().string(), patterns)) {
                 path_found.push_back(itr->path());
             }
         }
     }
-}
+}    // namespace file_utils
