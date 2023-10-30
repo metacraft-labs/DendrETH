a594956d7096a4f7f021e1e7e8eaaf2094ae810a
Hristo Staykov <izo8611@gmail.com>
2023-10-30 16:22:49 +0200

    feat(zkllvm-circuits/compute_shuffled_index_circuit): Compiled compute_shuffled_index.cpp as circuit.
    By modifying picosha2.h to use only std::array instead of std::vector
    we are now able to compile compute_shuffled_index's code as circuit.

--- vendor/zkllvm-metacraft-circuits/src/circuit_utils/base_types.h
+++ vendor/zkllvm-metacraft-circuits/src/circuit_utils/base_types.h
@@ -1,4 +1,14 @@
 #include <stdint.h>
 
+#pragma once
+
 using Byte = unsigned char;
 using Bytes32 = std::array<Byte, 32>;
+
+#ifdef __ZKLLVM__
+#define assert_true(c) \
+    { __builtin_assigner_exit_check(c); }
+#else
+#define assert_true(c) \
+    { assert(c); }
+#endif
