c9bf878f424b2963ea796fbd780b60b1443b6516
Aneta Tsvetkova <anetastsvetkova@gmail.com>
2023-11-09 13:56:29 +0200

    Added env vars for max shuffle count

--- casper-finality-proofs/fuzz/fuzz_targets/compute_shuffled_index_mainnet.rs
+++ casper-finality-proofs/fuzz/fuzz_targets/compute_shuffled_index_mainnet.rs
@@ -10,18 +10,19 @@ use casper_finality_proofs::test_engine::wrappers::compute_shuffled_index::wrapp
 use libfuzzer_sys::arbitrary::Unstructured;
 use libfuzzer_sys::fuzz_target;
 use once_cell::sync::Lazy;
+use std::env::var;
 use utils::arbitrary_types::ArbitraryH256;
 
 #[derive(Debug, arbitrary::Arbitrary)]
 struct TestData {
     pub seed: ArbitraryH256,
-    #[arbitrary(with = |u: &mut Unstructured| u.int_in_range(1..=15))]
+    #[arbitrary(with = |u: &mut Unstructured| u.int_in_range(1..=var("MAX_COUNT_COMPUTE_SHUFFLED_INDEX_MAINNET").unwrap().parse::<u8>().unwrap_or(15)))]
     pub count: u8,
 }
 
 fuzz_target!(|data: TestData| {
     Lazy::force(&MAINNET_CIRCUIT);
-
+    println!("input: {:?}", data);
     let mut indices = Vec::<u64>::new();
 
     let count = data.count as u64;
