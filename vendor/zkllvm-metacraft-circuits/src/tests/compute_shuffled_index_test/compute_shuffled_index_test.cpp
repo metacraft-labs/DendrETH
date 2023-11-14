e09ce55265e81c8e7aea5e5b750ef69e25cee86b
Hristo Staykov <izo8611@gmail.com>
2023-11-14 19:43:14 +0200

    feat(zkllvm-circuits/compute_shuffled_index_circuit): Move content of implementation in imp folder.

--- vendor/zkllvm-metacraft-circuits/src/tests/compute_shuffled_index_test/compute_shuffled_index_test.cpp
+++ vendor/zkllvm-metacraft-circuits/src/tests/compute_shuffled_index_test/compute_shuffled_index_test.cpp
@@ -1,6 +1,7 @@
 #include <nil/crypto3/hash/algorithm/hash.hpp>
 #include <nil/crypto3/hash/sha2.hpp>
 #include "circuit_utils/circuit_byte_utils.h"
+#include "circuits_imp/compute_shuffled_index_imp.h"
 
 #include <algorithm>
 #include <array>
@@ -14,6 +15,8 @@
 #include "utils/byte_utils.h"
 #include "utils/file_utils.h"
 
+#include "llvm/Support/JSON.h"
+
 using llvm::yaml::Input;
 using llvm::yaml::IO;
 using llvm::yaml::MappingTraits;
@@ -23,54 +26,6 @@ using namespace circuit_byte_utils;
 using namespace file_utils;
 
 using std::cout;
-
-uint64_t compute_shuffled_index(uint64_t index,
-                                uint64_t index_count,
-                                std::array<Byte, 32>
-                                    seed,
-                                int SHUFFLE_ROUND_COUNT = 90) {
-    assert_true(index < index_count);
-
-    std::array<Byte, 32 + 1 + 4> source_buffer {};
-
-    //!!! sha256_to_bytes_array(seed, source_buffer);
-    std::copy(seed.begin(), seed.end(), source_buffer.begin());
-
-    // Swap or not (https://link.springer.com/content/pdf/10.1007%2F978-3-642-32009-5_1.pdf)
-    // See the 'generalized domain' algorithm on page 3
-    for (Byte current_round = 0; current_round < SHUFFLE_ROUND_COUNT; current_round++) {
-        source_buffer[32] = current_round;
-
-        //!!! auto eth2digest = hash<hashes::sha2<256>>(source_buffer.begin(), source_buffer.begin() + 33);
-        std::array<Byte, 32> eth2digest_bytes;
-        picosha2::hash256(source_buffer.begin(), source_buffer.begin() + 33, eth2digest_bytes.begin(),
-                          eth2digest_bytes.end());
-        ///!!! sha256_to_bytes_array(eth2digest, eth2digest_bytes);
-        auto first8bytes = take_n_elements<Byte, eth2digest_bytes.size(), 8>(eth2digest_bytes);
-        // PrintContainer(first8bytes);
-        auto first8bytes_int = bytes_to_int<uint64_t>(first8bytes);
-        auto pivot = first8bytes_int % index_count;
-        uint64_t flip = (pivot + index_count - index) % index_count;
-        auto position = std::max(index, flip);
-
-        auto source_buffer_additional_bytes = int_to_bytes(uint32_t(position >> 8));
-        for (auto i = 0; i < 4; i++) {
-            source_buffer[33 + i] = source_buffer_additional_bytes[i];
-        }
-        ///!!! auto source = hash<hashes::sha2<256>>(source_buffer.begin(), source_buffer.end());
-        std::array<Byte, 32> source_to_bytes;
-        picosha2::hash256(source_buffer.begin(), source_buffer.end(), source_to_bytes.begin(), source_to_bytes.end());
-        ///!!! sha256_to_bytes_array(source, source_to_bytes);
-        auto byte_value = source_to_bytes[(position % 256) >> 3];
-        auto bit = (byte_value >> (position % 8)) % 2;
-
-        if (bit != 0) {
-            index = flip;
-        }
-    }
-
-    return index;
-}
 
 struct TestInput {
     std::string seed;
@@ -112,7 +67,7 @@ int main(int argc, char* argv[]) {
             yin >> doc;
 
             if (yin.error()) {
-                std::cerr << "Failes to process " << v.string() << "\n";
+                std::cerr << "Failed to process " << v.string() << "\n";
                 return false;
             }
 
@@ -130,7 +85,7 @@ int main(int argc, char* argv[]) {
             // std::cout << "\n";
             std::vector<uint64_t> mapping_result;
             for (size_t i = 0; i < doc.mapping.size(); i++) {
-                auto result = compute_shuffled_index(i, doc.mapping.size(), seed_bytes, SHUFFLE_ROUND_COUNT);
+                auto result = compute_shuffled_index_imp(i, doc.mapping.size(), seed_bytes, SHUFFLE_ROUND_COUNT);
                 mapping_result.push_back(result);
             }
             for (size_t i = 0; i < mapping_result.size(); i++) {
