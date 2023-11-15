d806337610f4bd8ee4ed826240489e9cf49861d6
Stefan Nikolov <consultsen.stefan@gmail.com>
2023-11-15 17:01:01 +0200

    split_bitmask WIP

--- casper-finality-proofs/src/verify_attestation_data/verify_subcommittee_votes.rs
+++ casper-finality-proofs/src/verify_attestation_data/verify_subcommittee_votes.rs
@@ -1,9 +1,10 @@
+use plonky2::iop::target::BoolTarget;
 use plonky2x::backend::circuit::{Circuit, PlonkParameters};
 use plonky2x::backend::function::Plonky2xFunction;
 use plonky2x::frontend::eth::beacon::generators::BeaconBalanceBatchWitnessHint;
 use plonky2x::frontend::eth::beacon::vars::BeaconValidatorVariable;
 use plonky2x::frontend::eth::vars::BLSPubkeyVariable;
-use plonky2x::prelude::{CircuitBuilder, Variable, U64Variable, Bytes32Variable, ArrayVariable, CircuitVariable, U256Variable, BoolVariable};
+use plonky2x::prelude::{CircuitBuilder, Variable, U64Variable, Bytes32Variable, ArrayVariable, CircuitVariable, U256Variable, BoolVariable, Field};
 use plonky2x::utils::eth::beacon::{BeaconHeaderContainer, BeaconValidator, BeaconHeader};
 
 use crate::checkpoint::Checkpoint;
@@ -16,7 +17,8 @@ const EPOCHS_PER_ETH1_VOTING_PERIOD: i32 = 64;
 const SUBCOMMITTEE_SIZE: i32 = 128;
 const MAX_ATTESTATIONS: i32 = 128;
 const MAX_VALIDATORS_PER_COMMITTEE: i32 = 2048;
-
+const VALIDATOR_HASH_TREE_DEPTH: i32 = 1234;
+const VALIDATOR_SIZE_UPPER_BOUND: i32 = 16_000_000;
 #[derive(Debug, Clone)]
 struct VerifySubcommitteeVotes;
 
@@ -95,6 +97,14 @@ struct AttestationData {
 
 }
 
+struct ValidatorHashData {
+    validator_state_root: Bytes32Variable,
+    validator_leaf: Bytes32Variable,
+    validator_branch: ArrayVariable<Bytes32Variable,VALIDATOR_HASH_TREE_DEPTH>,
+    validator_gindex: U256Variable,
+    validator_index: U256Variable,
+}
+
 impl Circuit for VerifySubcommitteeVotes {
     fn define<L: PlonkParameters<D>, const D: usize>(builder: &mut CircuitBuilder<L, D>) {
         
@@ -103,67 +113,93 @@ impl Circuit for VerifySubcommitteeVotes {
         let validators_proof = builder.read::<ArrayVariable<Bytes32Variable, 5>>();
         let validators_gindex = U64Variable::constant(builder, 43); // Is this correct?
 
-        let validator_list_length = builder.read::<Variable>();
         let validator_list = 
-            builder.read::<ArrayVariable<BeaconValidatorVariable>,validator_list_length>();
+            builder.read::<ArrayVariable<BeaconValidatorVariable>,MAX_VALIDATORS_PER_COMMITTEE>();
+        
+        verify_state_root(
+            builder,
+            state_root,
+            validators_root,
+            validators_proof,
+            validators_gindex
+        );
 
-        verify_subcommittee_votes(builder)
-    } 
+        // Define builder read
+        let validator_hash_list = builder.read::<ArrayVariable<ValidatorHashData>>();
 
-    // TODO: We have to use a poseidon commitment mapper to build the validator hashtree root 
-    fn verify_validator<L: PlonkParameters<D>, const D: usize>(
-            builder: &mut CircuitBuilder<L, D>,
-            validator: BeaconValidatorVariable
-        ) { 
+        let bitmask: ArrayVariable<BoolVariable,VALIDATOR_SIZE_UPPER_BOUND> = ArrayVariable::new(BoolVariable::ZERO);
 
-            let align_epoch1 = builder.lte(validator.activation_eligibility_epoch, validator.activation_epoch);
-            let align_epoch2 = builder.lte(validator.activation_epoch, validator.withdrawable_epoch);
-            let align_epoch3 = builder.lte(validator.withdrawable_epoch, validator.exit_epoch);
-
-            let aligned_count = builder.add_many(&[align_epoch1,align_epoch2, align_epoch3]);
-            // implement add_many for BoolVariable or better yet and_many
-
-            builder.assert_is_equal(aligned_count, Variable::constant(builder, 3));
-
-            let accumulated_key: BLSPubkeyVariable = pubkey_accumulator(builder, validator);
-
-            accumulated_key
-
+        for i in MAX_VALIDATORS_PER_COMMITTEE {
+            verify_validator(builder, validator_list[i], validator_hash_list[i]);
+            bitmask[validator_hash_list[i].validator_index] = 1;
         }
 
-    fn pubkey_accumulator<L: PlonkParameters<D>, const D: usize>(
-        builder: &mut CircuitBuilder<L, D>,
-        element: BLSPubkeyVariable
-    ) -> BLSPubkeyVariable{
-
-        todo!();
-    }
-
-    fn attestation_accumulator<L: PlonkParameters<D>, const D: usize>( // Do I need this?
-        builder: &mut CircuitBuilder<L, D>,
-        attestation: AttestationData // Definition may change
-    ) {
-
-        // Apply BLS signature and return
-
-        todo!();
-    }
-
-    fn verify_subcommittee_votes<L: PlonkParameters<D>, const D: usize>(
-        builder: &mut CircuitBuilder<L, D>,
-        state_root: Bytes32Variable,
-        validators_root: Bytes32Variable,
-        validators_proof: ArrayVariable<Bytes32Variable, 5>,
-        validators_gindex: U64Variable
-    ) {
-
-        // Prove validator set is part of the state root
-        builder.ssz_verify_proof(state_root, validators_root, validators_proof, validators_gindex);
-
-        // build poseidon commitment mapper (for validator in validator set )
-        // prove poseidon hash tree root maps to sha256 validator hash tree root
-
+        builder.write::<ArrayVariable<BoolTarget,VALIDATOR_SIZE_UPPER_BOUND>>(bitmask);
     }
 
 }
 
+fn verify_validator<L: PlonkParameters<D>, const D: usize>(
+    builder: &mut CircuitBuilder<L, D>,
+    validator: BeaconValidatorVariable,
+    validator_hash: ValidatorHashData
+
+) { 
+    // Ordering check
+    let align_epoch1 = builder.lte(validator.activation_eligibility_epoch, validator.activation_epoch);
+    let align_epoch2 = builder.lte(validator.activation_epoch, validator.withdrawable_epoch);
+    let align_epoch3 = builder.lte(validator.withdrawable_epoch, validator.exit_epoch);
+
+    let aligned_count = builder.add_many(&[align_epoch1,align_epoch2, align_epoch3]);
+    // implement add_many for BoolVariable or better yet and_many
+
+    builder.assert_is_equal(aligned_count, Variable::constant(builder, 3));
+
+    builder.ssz_verify_proof(
+        validator_hash.validator_state_root,
+        validator_hash.validator_leaf,
+        validator_hash.branch,
+        validator_hash.gindex
+    );
+
+    // I need access to validator.slot to prove slot is part of beacon state
+
+    let accumulated_key: BLSPubkeyVariable = pubkey_accumulator(builder, validator);
+
+    accumulated_key
+
+}
+
+fn pubkey_accumulator<L: PlonkParameters<D>, const D: usize>(
+builder: &mut CircuitBuilder<L, D>,
+element: BLSPubkeyVariable
+) -> BLSPubkeyVariable{
+
+todo!();
+}
+
+fn attestation_accumulator<L: PlonkParameters<D>, const D: usize>( // Do I need this?
+    builder: &mut CircuitBuilder<L, D>,
+    attestation: AttestationData // Definition may change
+) {
+
+// Apply BLS signature and return
+
+todo!();
+}
+
+fn verify_state_root<L: PlonkParameters<D>, const D: usize>(
+    builder: &mut CircuitBuilder<L, D>,
+    state_root: Bytes32Variable,
+    validators_root: Bytes32Variable,
+    validators_proof: ArrayVariable<Bytes32Variable, 5>,
+    validators_gindex: U64Variable
+) {
+
+// Prove validator set is part of the state root
+builder.ssz_verify_proof(state_root, validators_root, validators_proof, validators_gindex);
+
+// build poseidon commitment mapper (for validator in validator set )
+// prove poseidon hash tree root maps to sha256 validator hash tree root
+
+}
