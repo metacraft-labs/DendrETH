d76578ec954f4c7327d8ea9193cee8ec678811df
Dimo99 <dimodimov1999@gmail.com>
2023-12-15 16:13:06 +0200

    move beaconState.bin

--- beacon-light-client/plonky2/get_balances_input/get_balances_input.ts
+++ beacon-light-client/plonky2/get_balances_input/get_balances_input.ts
@@ -91,7 +91,7 @@ let TAKE;
 
   const beaconApi = new BeaconApi([options['beacon-node']]);
 
-  const { beaconState } = MOCK ? {beaconState: ssz.capella.BeaconState.deserialize(fs.readFileSync('mock_data/beaconState.bin'))} : await beaconApi.getBeaconState(6953401);
+  const { beaconState } = MOCK ? {beaconState: ssz.capella.BeaconState.deserialize(fs.readFileSync('../mock_data/beaconState.bin'))} : await beaconApi.getBeaconState(6953401);
 
   const validators = beaconState.validators.slice(0, TAKE);
   TAKE = validators.length;
