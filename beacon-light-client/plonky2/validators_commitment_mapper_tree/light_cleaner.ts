7aa849c25439fe6ecb6372a8ed522840409a5fda
Martin Ivanov <xeartybg@gmail.com>
2024-01-30 14:51:33 +0200

    feat: make light clean duration configurable

--- beacon-light-client/plonky2/validators_commitment_mapper_tree/light_cleaner.ts
+++ beacon-light-client/plonky2/validators_commitment_mapper_tree/light_cleaner.ts
@@ -5,66 +5,10 @@ const {
 import Redis from 'ioredis';
 import { sleep } from '../../../libs/typescript/ts-utils/common-utils';
 import validator_commitment_constants from '../constants/validator_commitment_constants.json';
-import yargs from 'yargs';
-
-async function lightCleanMod(this: any, db: Redis, prefix: any) {
-  const processingKey = prefix.of(":processing");
-  const mainQueueKey = prefix.of(":queue");
-  const cleaningKey = prefix.of(":cleaning");
-
-
-  const processing: Array<string> = await db.lrange(
-    processingKey,
-    0,
-    -1
-  );
-  for (const itemId of processing) {
-    if (!(await this.leaseExists(db, itemId))) {
-      await db.lpush(cleaningKey, itemId);
-      const removed = await db.lrem(processingKey, 0, itemId);
-      if (removed > 0) {
-        await db.rpush(mainQueueKey, itemId);
-      }
-      await db.lrem(cleaningKey, 0, itemId);
-    }
-  }
-
-  const forgot: Array<string> = await db.lrange(cleaningKey, 0, -1);
-  for (const itemId of forgot) {
-    const leaseExists: boolean = await this.leaseExists(db, itemId);
-    if (
-      !leaseExists &&
-      (await db.lpos(mainQueueKey, itemId)) == null &&
-      (await db.lpos(processingKey, itemId)) == null
-    ) {
-      /**
-       * FIXME: this introduces a race
-       * maybe not anymore
-       * no, it still does, what if the job has been completed?
-       */
-      await db.rpush(mainQueueKey, itemId);
-    }
-    await db.lrem(cleaningKey, 0, itemId);
-  }
-}
+import { getOptions, lightClean } from '../light_cleaner_common';
 
 (async () => {
-  const options = yargs
-    .usage('Usage: -redis-host <Redis host> -redis-port <Redis port>')
-    .option('redis-host ', {
-      alias: 'redis-host',
-      describe: 'The Redis host',
-      type: 'string',
-      default: '127.0.0.1',
-      description: 'Sets a custom redis connection',
-    })
-    .option('redis-port', {
-      alias: 'redis-port',
-      describe: 'The Redis port',
-      type: 'number',
-      default: 6379,
-      description: 'Sets a custom redis connection',
-    }).argv;
+  const options = getOptions().argv;
 
   const prefix = new KeyPrefix(`${validator_commitment_constants.validatorProofsQueue}`);
   const first_level_proofs = new WorkQueue(prefix);
@@ -75,8 +19,8 @@ async function lightCleanMod(this: any, db: Redis, prefix: any) {
 
   while (true) {
     console.log('Performing light clean');
-    await lightCleanMod.call(first_level_proofs, redis, prefix);
-    console.log('Waiting 5 seconds');
-    await sleep(5000);
+    await lightClean.call(first_level_proofs, redis, prefix);
+    console.log(`Waiting ${options['clean-duration'] / 1000} seconds`);
+    await sleep(options['clean-duration']);
   }
 })();
