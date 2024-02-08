d60154ab6ea6a15b00c7ff1bd7b0f2f55065f6aa
Hristo Staykov <izo8611@gmail.com>
2024-02-08 19:07:59 +0200

    Clone test input data as part of run script.

--- vendor/zkllvm-metacraft-circuits/scripts/compile_and_run_tests.sh
+++ vendor/zkllvm-metacraft-circuits/scripts/compile_and_run_tests.sh
@@ -9,6 +9,11 @@ echo "CURRENT_DIR = " $CURRENT_DIR
 
 cd $SCRIPT_DIR/../docker && docker build -t zcli:latest -f Dockerfile_zcli . && docker run -v $SCRIPT_DIR/../../:/DendrETH zcli:latest
 
+if [ ! -d $SCRIPT_DIR/../src/tests/verify_attestation_data_test/finalizer-data ]
+then 
+    git clone git@github.com:metacraft-labs/finalizer-data.git $SCRIPT_DIR/../src/tests/verify_attestation_data_test/finalizer-data
+fi
+
 cd $CURRENT_DIR
 
 bash ${SCRIPT_DIR}/run.sh --docker compile
