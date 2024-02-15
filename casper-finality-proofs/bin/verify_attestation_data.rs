80e9531bbf48c8e5de3898d9f6e608202ef2d09e
Stefan Nikolov <consultsen.stefan@gmail.com>
2024-02-15 14:36:42 +0200

    Update VAD Binary

--- casper-finality-proofs/bin/verify_attestation_data.rs
+++ casper-finality-proofs/bin/verify_attestation_data.rs
@@ -4,18 +4,15 @@ use serde_json::Value;
 use plonky2x::{
     backend::circuit::{Circuit, DefaultParameters}, frontend::vars::Bytes32Variable, prelude::{bytes32, CircuitBuilder}
 };
-use casper_finality_proofs::{utils::eth_objects::{AttestationInput, ValidatorDataInput}, verify_attestation_data::verify_attestation_data::VerifyAttestationData};
+use casper_finality_proofs::{constants::TEST_VALIDATORS_IN_COMMITMENT_SIZE, utils::{eth_objects::{AttestationInput, ValidatorDataInput}, json::read_json_from_file}, verify_attestation_data::verify_attestation_data::VerifyAttestationData};
 fn main() {
     plonky2x::utils::setup_logger();
 
-    let file_path = "./data/merged_234400.json";
-    let file = File::open(file_path);
-    let mut contents = String::new();
+    let file_path 
+        = "/home/stefan/code/repos/metacraft-labs/DendrETH/casper-finality-proofs/data/merged_234400.json";
+    let attestations_json = read_json_from_file(file_path).unwrap();
 
-    file.unwrap().read_to_string(&mut contents).unwrap();
-    let json_value: Value = serde_json::from_str(&contents).unwrap();
-
-    let attestation = json_value.get("attestations")
+    let attestation = attestations_json.get("attestations")
         .and_then(Value::as_array)
         .and_then(|array| array.get(0))
         .unwrap();
@@ -26,26 +23,25 @@ fn main() {
     const D: usize = 2;
 
     let mut builder = CircuitBuilder::<L, D>::new();
-        
+
     VerifyAttestationData::define(&mut builder);
+
     let circuit = builder.build();
     let mut input = circuit.input();
 
     //TODO: prev_block_root should be part of attestation_input and not hardcoded
-    let prev_block_root: String = "d5c0418465ffab221522a6991c2d4c0041f1b8e91d01b1ea3f6b882369f689b7".to_string();
-    input.write::<Bytes32Variable>(bytes32!(prev_block_root));
-
+    // let prev_block_root: String = "d5c0418465ffab221522a6991c2d4c0041f1b8e91d01b1ea3f6b882369f689b7".to_string();
+    // input.write::<Bytes32Variable>(bytes32!(prev_block_root));
 
     attestation_input.write(&mut input);
 
-    const N: usize = 10;
-    let validator_samples: Vec<ValidatorDataInput> = attestation.get("validators").clone()
+    let validators: Vec<ValidatorDataInput>= attestation.get("validators").clone()
         .and_then(Value::as_array)
         .unwrap()
         .iter()
-        .take(N)
+        .take(TEST_VALIDATORS_IN_COMMITMENT_SIZE)
         .map(|validator|serde_json::from_value(validator.clone()).unwrap())
-        .collect_vec();
+        .collect();
 
 
     println!("\nAttestation Read!\n");
@@ -58,7 +54,7 @@ fn main() {
 
     // let samples = serde_json::from_value(validator_samples.clone()).unwrap();
 
-    for validator in validator_samples {
+    for validator in validators {
         validator.write(&mut input);
     }
 
