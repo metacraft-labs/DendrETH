d42146f6533b2fa6a7d3f5f7e5114a4a9230e875
Kristin Kirkov <krischanss99@gmail.com>
2024-02-22 11:26:19 +0200

    build(nix): Add `prometheus` pkg

--- libs/nix/common-shell-pkgs.nix
+++ libs/nix/common-shell-pkgs.nix
@@ -83,6 +83,8 @@ in
     #  to manage non-containerized applications.
     process-compose
 
+    # Monitoring system & time series database
+    prometheus
   ]
   ++ lib.optionals (stdenv.isx86_64) [
     metacraft-labs.rapidsnark
