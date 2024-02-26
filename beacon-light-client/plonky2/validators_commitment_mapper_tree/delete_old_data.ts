01585d640a1afe3724b414c749573746a6d5067e
Martin Ivanov <xeartybg@gmail.com>
2024-02-26 15:57:06 +0200

    feat(proof_storage): implement proof storage in typescript fix data commitment mapper old epoch deletion
    Co-authored-by: Aneta Tsvetkova <anetastsvetkova@gmail.com>

--- beacon-light-client/plonky2/validators_commitment_mapper_tree/delete_old_data.ts
+++ beacon-light-client/plonky2/validators_commitment_mapper_tree/delete_old_data.ts
@@ -2,6 +2,7 @@ import { Redis as RedisLocal } from '../../../relay/implementations/redis';
 
 import validator_commitment_constants from '../constants/validator_commitment_constants.json';
 import yargs from 'yargs';
+import { createProofStorage } from '../proof_storage/proof_storage';
 
 (async () => {
   const options = yargs
@@ -30,14 +31,31 @@ import yargs from 'yargs';
     }).argv;
 
   const redis = new RedisLocal(options['redis-host'], options['redis-port']);
+  const proofStorage = createProofStorage(options)
 
-  const validatorKeys = await redis.getAllKeys(`${validator_commitment_constants.validatorKey}:*:${validator_commitment_constants.epochLookupKey}`);
-  const validatorProofKeys = await redis.getAllKeys(`${validator_commitment_constants.validatorProofKey}:*:${validator_commitment_constants.epochLookupKey}`);
-  const keys = [...validatorKeys, ...validatorProofKeys].map((key) => key.substring(0, key.lastIndexOf(':')));
+  let validatorKeys = await redis.getAllKeys(`${validator_commitment_constants.validatorKey}:*:${validator_commitment_constants.epochLookupKey}`);
+  let validatorProofKeys = await redis.getAllKeys(`${validator_commitment_constants.validatorProofKey}:*:${validator_commitment_constants.epochLookupKey}`);
 
-  const deleted = await Promise.all(keys.map(async (key) => redis.pruneOldEpochs(key, options['oldest-epoch'])));
+  validatorKeys = validatorKeys.map((key) => key.substring(0, key.lastIndexOf(':')));
+  validatorProofKeys = validatorProofKeys.map((key) => key.substring(0, key.lastIndexOf(':')));
+
+  // delete proofs
+  const proofKeys = validatorProofKeys.map(key => validator_commitment_constants.validatorProofStorage + key.slice(key.indexOf(':')))
+  await Promise.all(proofKeys.map(async (proofKey, index) => {
+    const outdatedEpochs = await redis.collectOutdatedEpochs(validatorProofKeys[index], options['oldest-epoch'])
+    const keysToDelete = outdatedEpochs.map(epoch => `${proofKey}:${epoch}`);
+    return Promise.all(keysToDelete.map(key => proofStorage.delProof(key)));
+  }));
+
+  // delete redis data
+  const redisKeys = [...validatorKeys, ...validatorProofKeys];
+  const deleted = await Promise.all(redisKeys.map(async (key) => {
+    return redis.pruneOldEpochs(key, options['oldest-epoch']);
+  }));
+
   const deletedCount = deleted.reduce((sum, value) => sum + value);
   console.log(`Deleted ${deletedCount} database entries`);
 
+  await proofStorage.quit();
   await redis.quit();
 })();
