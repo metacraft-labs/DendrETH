1f286426eda353a49e70baca01d5f0c1fadea0c3
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2024-02-28 15:00:15 +0200

    feat(experimets): Support writing to file

--- tests/experiments/utils/constants.ts
+++ tests/experiments/utils/constants.ts
@@ -1,3 +1,5 @@
+export const resultsFile = 'tests/experiments/output.json';
+
 export const exampleLeafData = {
   pubkey:
     '0x93247f2209abcacf57b75a51dafae777f9dd38bc7053d1af526f220a7489a6d3a2753e5f3e8b1cfe39b56f43611df74a',
