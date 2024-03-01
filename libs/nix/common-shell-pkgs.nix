6d7b01191e48ecd1c85e04bef6ae98b63b0bdead
Petar Kirov <petar.p.kirov@gmail.com>
2024-03-01 09:32:28 +0200

    build(nix): Upgrade Node.js to v21

--- libs/nix/common-shell-pkgs.nix
+++ libs/nix/common-shell-pkgs.nix
@@ -3,7 +3,6 @@
   rust-stable,
 }:
 with pkgs; let
-  nodejs = nodejs-18_x;
   llvm = llvmPackages_14;
   emscripten = metacraft-labs.emscripten;
   nim-wasm = callPackage ./nim-wasm {inherit llvm emscripten;};
@@ -26,8 +25,8 @@ in
     tmux
     tmuxinator
     # Node.js dev environment for unit tests
-    nodejs
-    metacraft-labs.corepack-shims
+    nodejs_21
+    corepack_21
 
     # For WebAssembly unit-testing
     wasm3 # wasmer is currently broken on macOS ARM
