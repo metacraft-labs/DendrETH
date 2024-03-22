94e443058ff085738993f9f939167319719ed24f
Petar Kirov <petar.p.kirov@gmail.com>
2024-03-22 20:46:46 +0200

    build(nix/circuits_executables): Add necessary build inputs and refactor crane artifacts

--- libs/nix/circuits_executables/default.nix
+++ libs/nix/circuits_executables/default.nix
@@ -2,32 +2,30 @@
   lib,
   craneLib,
   fetchFromGitHub,
+  pkg-config,
+  openssl,
   ...
 }: let
   src-path = ../../../beacon-light-client/plonky2;
-  cargoArtifacts = craneLib.buildDepsOnly {
-    pname = "plonky2";
-    version = "0.1.0";
 
-    src = fetchFromGitHub {
-      owner = "metacraft-labs";
-      repo = "plonky2";
-      rev = "12402078a460c41cd11013d065367c8e25bb8478";
-      hash = "sha256-uPfN65vlWh92Se8muhrO071WNaAGI+PUOY4x1syvspU=";
-    };
-  };
-in
-  craneLib.buildPackage rec {
-    pname = "circuits_executables";
+  sharedAttrs = {
     version = "0.1.0";
-
-    inherit cargoArtifacts;
+    nativeBuildInputs = [pkg-config];
+    buildInputs = [openssl];
     src = src-path;
     cargoLock = "${src-path}/circuits_executables/Cargo.lock";
     cargoToml = "${src-path}/circuits_executables/Cargo.toml";
-
     postUnpack = ''
       cd $sourceRoot/circuits_executables
       sourceRoot="."
     '';
-  }
+  };
+
+  cargoArtifacts = craneLib.buildDepsOnly sharedAttrs;
+in
+  craneLib.buildPackage (
+    sharedAttrs
+    // {
+      inherit cargoArtifacts;
+    }
+  )
