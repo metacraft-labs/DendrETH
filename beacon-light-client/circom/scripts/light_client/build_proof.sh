442b81caadda33ca8c5d0509beea55a9d16586c0
Kristin Kirkov <krischanss99@gmail.com>
2024-04-19 17:29:00 +0300

    feat(circuit-scripts): Update scripts for circom circuits

--- beacon-light-client/circom/scripts/light_client/build_proof.sh
+++ beacon-light-client/circom/scripts/light_client/build_proof.sh
@@ -1,68 +1,27 @@
 #!/usr/bin/env bash
+source "${BASH_SOURCE%/*}/../../../../scripts/utils/paths.sh"
+source "${BASH_SOURCE%/*}/../common.sh"
 
-PHASE1=../../build/pot28_final.ptau
+CIRCOM_DIR="${ROOT}/beacon-light-client/circom"
+SNARKJS_DIR="${ROOT}"/vendor/snarkjs
+SNARKJS="$SNARKJS_DIR"/cli.js
+PHASE1="${BUILD_DIR}"/pot28_final.ptau
 CIRCUIT_NAME=light_client
-BUILD_DIR=../../build/"$CIRCUIT_NAME"
+BUILD_DIR="${CIRCOM_DIR}/build/$CIRCUIT_NAME"
+CIRCUIT_DIR="${CIRCOM_DIR}/scripts/${CIRCUIT_NAME}"
 
-if [ -f "$PHASE1" ]; then
-    echo "Found Phase 1 ptau file"
-else
-    echo "No Phase 1 ptau file found. Exiting..."
-    exit 1
-fi
+git submodule update --init --recursive
 
-if [ ! -d "$BUILD_DIR" ]; then
-    echo "No build directory found. Creating build directory..."
-    mkdir -p "$BUILD_DIR"
-fi
+look_for_ptau_file
 
-echo $PWD
+create_build_folder
 
-echo "****COMPILING CIRCUIT****"
-start=`date +%s`
-#circom "$CIRCUIT_NAME".circom --O0 --c --output "$BUILD_DIR"
-circom "$CIRCUIT_NAME".circom --O2 --r1cs --sym --c --output "$BUILD_DIR"
-end=`date +%s`
-echo "DONE ($((end-start))s)"
+compile_the_circuit
 
-echo "****COMPILING C++ WITNESS GENERATION CODE****"
-start=`date +%s`
-cd "$BUILD_DIR"/"$CIRCUIT_NAME"_cpp
-make
-end=`date +%s`
-echo "DONE ($((end-start))s)"
+compile_cpp_witness
 
-# echo "****VERIFYING WITNESS****"
-# start=`date +%s`
-# ./"$CIRCUIT_NAME" ../../../scripts/"$CIRCUIT_NAME"/input_"$CIRCUIT_NAME".json ../witness.wtns
-# end=`date +%s`
-# echo "DONE ($((end-start))s)"
+install_snarkjs_packages
 
-# cd ..
-# snarkjs wej witness.wtns witness.json
+generate_zkey
 
-cd ..
-
-echo "****GENERATING ZKEY 0****"
-start=`date +%s`
-node --trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc ../../../../node_modules/snarkjs/cli.js zkey new "$CIRCUIT_NAME".r1cs "$PHASE1" "$CIRCUIT_NAME"_0.zkey -v > zkey0.out
-end=`date +%s`
-echo "DONE ($((end-start))s)"
-
-# echo "****CONTRIBUTE TO PHASE 2 CEREMONY****"
-# start=`date +%s`
-# yarn snarkjs zkey contribute -verbose "$CIRCUIT_NAME"_0.zkey "$CIRCUIT_NAME".zkey -n="First phase2 contribution" -e="some random text 5555" > contribute.out
-# end=`date +%s`
-# echo "DONE ($((end-start))s)"
-
-# echo "****VERIFYING FINAL ZKEY****"
-# start=`date +%s`
-# node --trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc ../../node_modules/snarkjs/cli.js zkey verify -verbose "$CIRCUIT_NAME".r1cs "$PHASE1" "$CIRCUIT_NAME".zkey > verify.out
-# end=`date +%s`
-# echo "DONE ($((end-start))s)"
-
-echo "****EXPORTING VKEY****"
-start=`date +%s`
-yarn snarkjs zkey export verificationkey "$CIRCUIT_NAME"_0.zkey vkey.json -v
-end=`date +%s`
-echo "DONE ($((end-start))s)"
+export_vkey
