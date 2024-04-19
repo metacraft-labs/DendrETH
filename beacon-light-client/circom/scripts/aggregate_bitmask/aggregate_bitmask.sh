c803e5a8c0bd297d0f2c5be134cc4df6839b975c
Kristin Kirkov <krischanss99@gmail.com>
2024-04-19 17:29:00 +0300

    feat(circuit-scripts): Update scripts for circom circuits

--- beacon-light-client/circom/scripts/aggregate_bitmask/aggregate_bitmask.sh
+++ beacon-light-client/circom/scripts/aggregate_bitmask/aggregate_bitmask.sh
@@ -1,78 +1,44 @@
 #!/usr/bin/env bash
+source "${BASH_SOURCE%/*}/../../../../scripts/utils/paths.sh"
+source "${BASH_SOURCE%/*}/../common.sh"
 
-PHASE1=../../circuits/pot25_final.ptau
+CIRCOM_DIR="${ROOT}/beacon-light-client/circom"
+SNARKJS_DIR="${ROOT}"/vendor/snarkjs
+SNARKJS="${SNARKJS_DIR}"/cli.js
+PHASE1=~/Downloads/powersOfTau28_hez_final_24.ptau
 CIRCUIT_NAME=aggregate_bitmask
-BUILD_DIR=../../build/"$CIRCUIT_NAME"
+BUILD_DIR="${CIRCOM_DIR}/build/${CIRCUIT_NAME}"
+CIRCUIT_DIR="${CIRCOM_DIR}/scripts/${CIRCUIT_NAME}"
 
-if [ -f "$PHASE1" ]; then
-    echo "Found Phase 1 ptau file"
-else
-    echo "No Phase 1 ptau file found. Exiting..."
-    exit 1
-fi
+git submodule update --init --recursive
 
-if [ ! -d "$BUILD_DIR" ]; then
-    echo "No build directory found. Creating build directory..."
-    mkdir -p "$BUILD_DIR"
-fi
+look_for_ptau_file
 
-echo $PWD
+create_build_folder
 
-echo "****COMPILING CIRCUIT****"
-start=`date +%s`
-#circom "$CIRCUIT_NAME".circom --O0 --c --output "$BUILD_DIR"
-circom "$CIRCUIT_NAME".circom --O1 --r1cs --sym --c --output "$BUILD_DIR"
-end=`date +%s`
-echo "DONE ($((end-start))s)"
+compile_the_circuit
 
-echo "****COMPILING C++ WITNESS GENERATION CODE****"
-start=`date +%s`
-cd "$BUILD_DIR"/"$CIRCUIT_NAME"_cpp
-make
-end=`date +%s`
-echo "DONE ($((end-start))s)"
+compile_cpp_witness
 
 echo "****VERIFYING WITNESS****"
-start=`date +%s`
-./"$CIRCUIT_NAME" ../../../scripts/"$CIRCUIT_NAME"/input.json ../witness.wtns
-end=`date +%s`
-echo "DONE ($((end-start))s)"
+start=$(date +%s)
+cd "$BUILD_DIR"/"$CIRCUIT_NAME"_cpp
+./"$CIRCUIT_NAME" "$CIRCOM_DIR"/test/"$CIRCUIT_NAME"_N1/data/notzero/input.json ../witness.wtns
+end=$(date +%s)
+echo "DONE ($((end - start))s)"
 
-cd ..
-yarn snarkjs wej $PWD/witness.wtns witness.json
+node "${SNARKJS}" wej "${BUILD_DIR}"/witness.wtns "${BUILD_DIR}"/witness.json
 
-echo "****GENERATING ZKEY 0****"
-start=`date +%s`
-node --trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc ../../../../node_modules/snarkjs/cli.js zkey new "$CIRCUIT_NAME".r1cs "$PHASE1" "$CIRCUIT_NAME"_0.zkey -v > zkey0.out
-end=`date +%s`
-echo "DONE ($((end-start))s)"
+install_snarkjs_packages
 
-echo "****CONTRIBUTE TO PHASE 2 CEREMONY****"
-start=`date +%s`
-node ../../../../node_modules/snarkjs/cli.js zkey contribute -verbose "$CIRCUIT_NAME"_0.zkey "$CIRCUIT_NAME".zkey -n="First phase2 contribution" -e="some random text 5555" > contribute.out
-end=`date +%s`
-echo "DONE ($((end-start))s)"
+generate_zkey
 
-echo "****VERIFYING FINAL ZKEY****"
-start=`date +%s`
-node --trace-gc --trace-gc-ignore-scavenger --max-old-space-size=2048000 --initial-old-space-size=2048000 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=2048000 --expose-gc ../../../../node_modules/snarkjs/cli.js zkey verify -verbose "$CIRCUIT_NAME".r1cs "$PHASE1" "$CIRCUIT_NAME".zkey > verify.out
-end=`date +%s`
-echo "DONE ($((end-start))s)"
+contribute_to_phase_2_ceremony
 
-echo "****EXPORTING VKEY****"
-start=`date +%s`
-node ../../../../node_modules/snarkjs/cli.js zkey export verificationkey "$CIRCUIT_NAME".zkey vkey.json -v
-end=`date +%s`
-echo "DONE ($((end-start))s)"
+verify_final_key
 
-echo "****GENERATING PROOF FOR SAMPLE INPUT****"
-start=`date +%s`
-../../../../vendor/rapidsnark/build/prover "$CIRCUIT_NAME".zkey witness.wtns proof.json public.json > proof.out
-end=`date +%s`
-echo "DONE ($((end-start))s)"
+export_vkey
 
-echo "****VERIFYING PROOF FOR SAMPLE INPUT****"
-start=`date +%s`
-node ../../../../node_modules/snarkjs/cli.js groth16 verify vkey.json public.json proof.json -v
-end=`date +%s`
-echo "DONE ($((end-start))s)"
+generate_proof_for_sample_input
+
+verify_proof_for_sample_input
