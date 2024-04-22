d6ed601a44296f701320356d1f2f73fcfb33a0d7
Kristin Kirkov <krischanss99@gmail.com>
2024-04-22 18:01:14 +0300

    feat(circom-scripts): Renaming scripts

--- beacon-light-client/circom/scripts/light_client_recursive/light_client_recursive.sh
+++ beacon-light-client/circom/scripts/light_client_recursive/light_client_recursive.sh
@@ -4,10 +4,10 @@ source "${BASH_SOURCE%/*}/../common.sh"
 
 CIRCOM_DIR="${ROOT}/beacon-light-client/circom"
 SNARKJS_DIR="${ROOT}"/vendor/snarkjs
-SNARKJS="$SNARKJS_DIR"/cli.js
+SNARKJS="${SNARKJS_DIR}"/cli.js
 PHASE1="${BUILD_DIR}"/pot28_final.ptau
-CIRCUIT_NAME=validator_balances
-BUILD_DIR="${CIRCOM_DIR}/build/$CIRCUIT_NAME"
+CIRCUIT_NAME=light_client_recursive
+BUILD_DIR="${CIRCOM_DIR}/build/${CIRCUIT_NAME}"
 CIRCUIT_DIR="${CIRCOM_DIR}/scripts/${CIRCUIT_NAME}"
 
 git submodule update --init --recursive
@@ -20,25 +20,8 @@ compile_the_circuit
 
 compile_cpp_witness
 
-echo "****VERIFYING WITNESS****"
-start=$(date +%s)
-./"$CIRCUIT_NAME" ../../../scripts/"$CIRCUIT_NAME"/"$CIRCUIT_NAME"-input.json ../witness.wtns
-end=$(date +%s)
-echo "DONE ($((end - start))s)"
-
-cd ..
-yarn snarkjs wej witness.wtns witness.json
-
 install_snarkjs_packages
 
 generate_zkey
 
-contribute_to_phase_2_ceremony
-
-verify_final_key
-
 export_vkey
-
-generate_proof_for_sample_input
-
-verify_proof_for_sample_input
--- beacon-light-client/circom/scripts/validator_balances/validator_balances.sh
+++ beacon-light-client/circom/scripts/validator_balances/validator_balances.sh
@@ -4,10 +4,10 @@ source "${BASH_SOURCE%/*}/../common.sh"
 
 CIRCOM_DIR="${ROOT}/beacon-light-client/circom"
 SNARKJS_DIR="${ROOT}"/vendor/snarkjs
-SNARKJS="$SNARKJS_DIR"/cli.js
+SNARKJS="${SNARKJS_DIR}"/cli.js
 PHASE1="${BUILD_DIR}"/pot28_final.ptau
-CIRCUIT_NAME=hash_tree_root
-BUILD_DIR="${CIRCOM_DIR}/build/$CIRCUIT_NAME"
+CIRCUIT_NAME=validator_balances
+BUILD_DIR="${CIRCOM_DIR}/build/${CIRCUIT_NAME}"
 CIRCUIT_DIR="${CIRCOM_DIR}/scripts/${CIRCUIT_NAME}"
 
 git submodule update --init --recursive
@@ -21,14 +21,13 @@ compile_the_circuit
 compile_cpp_witness
 
 echo "****VERIFYING WITNESS****"
-
 start=$(date +%s)
-cd "$BUILD_DIR"/"$CIRCUIT_NAME"_cpp
-./"$CIRCUIT_NAME" "$CIRCOM_DIR"/test/"$CIRCUIT_NAME"/data/case01/input.json ../witness.wtns
+./"$CIRCUIT_NAME" ../../../scripts/"$CIRCUIT_NAME"/"$CIRCUIT_NAME"-input.json ../witness.wtns
 end=$(date +%s)
 echo "DONE ($((end - start))s)"
 
-node "${SNARKJS}" wej "${BUILD_DIR}"/witness.wtns "${BUILD_DIR}"/witness.json
+cd ..
+yarn snarkjs wej witness.wtns witness.json
 
 install_snarkjs_packages
 
