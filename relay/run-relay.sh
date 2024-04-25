aa774ee94e43767ae37901b6fba8785057b1a2ca
Kristin Kirkov <krischanss99@gmail.com>
2024-04-25 14:58:26 +0300

    refactor(relay): Format 'run-relay.sh'

--- relay/run-relay.sh
+++ relay/run-relay.sh
@@ -9,17 +9,17 @@ process-compose -t=false &
 
 calculate_checksum() {
   local FILE_PATH=$1
-  b3sum "$FILE_PATH" | cut -d ' ' -f 1
+  b3sum "${FILE_PATH}" | cut -d ' ' -f 1
 }
 
 download_zkey_file() {
   echo "Downloading zkey file from http://dendreth.metacraft-labs.com/capella_94.zkey ..."
 
-  curl http://dendreth.metacraft-labs.com/capella_94.zkey > "data/light_client.zkey"
+  curl http://dendreth.metacraft-labs.com/capella_94.zkey >"data/light_client.zkey"
 
   CALCULATED_ZKEY_SUM=$(calculate_checksum data/light_client.zkey)
 
-  if [ "$ZKEY_B3SUM_SUM" = "$CALCULATED_ZKEY_SUM" ]; then
+  if [ "${ZKEY_B3SUM_SUM}" = "${CALCULATED_ZKEY_SUM}" ]; then
     echo "Zkey file downloaded successfully to data/light_client.zkey"
   else
     echo "Failed to download zkey file from http://dendreth.metacraft-labs.com/capella_94.zkey"
@@ -30,11 +30,11 @@ download_zkey_file() {
 download_dat_file() {
   echo "Downloading .dat file from https://media.githubusercontent.com/media/metacraft-labs/DendrETH-build-artifacts/master/light_client_cpp/light_client.dat ..."
 
-  curl -k https://media.githubusercontent.com/media/metacraft-labs/DendrETH-build-artifacts/master/light_client_cpp/light_client.dat > "data/light_client.dat"
+  curl -k https://media.githubusercontent.com/media/metacraft-labs/DendrETH-build-artifacts/master/light_client_cpp/light_client.dat >"data/light_client.dat"
 
   CALCULATED_DAT_SUM=$(calculate_checksum data/light_client.dat)
 
-  if [ "$DAT_B3SUM_SUM" = "$CALCULATED_DAT_SUM" ]; then
+  if [ "${DAT_B3SUM_SUM}" = "${CALCULATED_DAT_SUM}" ]; then
     echo ".dat file downloaded successfully to data/light_client.dat"
   else
     echo "Failed to download .dat file from https://media.githubusercontent.com/media/metacraft-labs/DendrETH-build-artifacts/master/light_client_cpp/light_client.dat"
@@ -46,8 +46,8 @@ if [ ! -f "data/light_client.zkey" ]; then
   download_zkey_file
 else
   CALCULATED_ZKEY_SUM=$(calculate_checksum data/light_client.zkey)
-  echo $CALCULATED_ZKEY_SUM
-  if [ "$ZKEY_B3SUM_SUM" = "$CALCULATED_ZKEY_SUM" ]; then
+  echo "${CALCULATED_ZKEY_SUM}"
+  if [ "${ZKEY_B3SUM_SUM}" = "${CALCULATED_ZKEY_SUM}" ]; then
     echo "Using cached zkey file at data/light_client.zkey"
   else
     echo "Wrong version of light_client.zkey cached downloading again..."
@@ -59,8 +59,8 @@ if [ ! -f "data/light_client.dat" ]; then
   download_dat_file
 else
   CALCULATED_DAT_SUM=$(calculate_checksum data/light_client.dat)
-  echo $CALCULATED_DAT_SUM
-  if [ "$DAT_B3SUM_SUM" = "$CALCULATED_DAT_SUM" ]; then
+  echo ${CALCULATED_DAT_SUM}
+  if [ "${DAT_B3SUM_SUM}" = "${CALCULATED_DAT_SUM}" ]; then
     echo "Using cached .dat file at data/light_client.dat"
   else
     echo "Wrong version of light_client.dat cached downloading again..."
@@ -73,7 +73,7 @@ mkdir -p build
 cp relay/light_client build/light_client
 cp data/light_client.dat light_client.dat
 
-if [[ -z "$REDIS_HOST" ]] && [[ -z "$REDIS_PORT" ]]; then
+if [[ -z "${REDIS_HOST}" ]] && [[ -z "${REDIS_PORT}" ]]; then
   echo "REDIS_HOST and REDIS_PORT environment variables are not set. Using default values."
   REDIS_HOST="localhost"
   REDIS_PORT="6379"
@@ -81,8 +81,7 @@ else
   echo "Using Redis settings from environment variables"
 fi
 
-
-if [[ -z "$PROVER_SERVER_HOST" ]] && [[ -z "$PROVER_SERVER_PORT" ]]; then
+if [[ -z "${PROVER_SERVER_HOST}" ]] && [[ -z "${PROVER_SERVER_PORT}" ]]; then
   echo "PROVER_SERVER_HOST and PROVER_SERVER_PORT environment variables are not set. Using default values."
   PROVER_SERVER_HOST="http://127.0.0.1"
   PROVER_SERVER_PORT="5000"
@@ -90,9 +89,7 @@ else
   echo "Using prover server settings from environment variables"
 fi
 
-
-
-if [[ "$PROVER_SERVER_HOST" == "http://127.0.0.1" ]]; then
+if [[ "${PROVER_SERVER_HOST}" == "http://127.0.0.1" ]]; then
   echo "Starting local prover server..."
   process-compose process start proverserver
   echo "Prover server started with command"
@@ -102,33 +99,33 @@ if [[ "$PROVER_SERVER_HOST" == "http://127.0.0.1" ]]; then
 
   echo "Waiting for server to start..."
 
-  for ((i=1; i<=$max_attempts; i++)); do
-    response=$(curl -s -o /dev/null -w "%{http_code}" "$PROVER_SERVER_HOST":"$PROVER_SERVER_PORT"/status)
+  for ((i = 1; i <= max_attempts; i++)); do
+    response=$(curl -s -o /dev/null -w "%{http_code}" "${PROVER_SERVER_HOST}":"${PROVER_SERVER_PORT}"/status)
 
-    if [ $response -eq 200 ]; then
+    if [ "${response}" -eq 200 ]; then
       echo "Server is up and running."
       server_started=true
       break
     fi
 
-    echo "Attempt $i: Server is not responding. Waiting for 2 seconds..."
+    echo "Attempt ${i}: Server is not responding. Waiting for 2 seconds..."
     sleep 2
   done
 
-  if [ $server_started == false ]; then
+  if [ ${server_started} == false ]; then
     echo "Server failed to start after 5 minutes. Exiting."
     exit 1
   fi
 else
-  echo "Using remote prover server at $PROVER_SERVER_HOST:$PROVER_SERVER_PORT"
+  echo "Using remote prover server at ${PROVER_SERVER_HOST}:${PROVER_SERVER_PORT}"
 fi
 
-if [[ "$REDIS_HOST" == "localhost" ]] && [[ "$REDIS_PORT" == "6379" ]]; then
+if [[ "${REDIS_HOST}" == "localhost" ]] && [[ "${REDIS_PORT}" == "6379" ]]; then
   echo "Starting local Redis server..."
   process-compose process start redis
   echo "Local Redis server started"
 else
-  echo "Using remote Redis server at $REDIS_HOST:$REDIS_PORT"
+  echo "Using remote Redis server at ${REDIS_HOST}:${REDIS_PORT}"
 fi
 
 echo "Starting Prometheus server on 9090"
@@ -145,12 +142,12 @@ echo "Starting the proof generation task"
 process-compose process start proofGenerationWorker
 echo "Proof generation task started"
 
-if [ -z "$SLOTS_JUMP" ]; then
+if [ -z "${SLOTS_JUMP}" ]; then
   echo "Error: SLOTS_JUMP environment variable is not set. Exiting..."
   exit 1
 fi
 
-if [[ "$PRATTER" != "TRUE" && "$MAINNET" != "TRUE" ]]; then
+if [[ "${PRATTER}" != "TRUE" && "${MAINNET}" != "TRUE" ]]; then
   echo "Neither PRATTER nor MAINNET is set or true."
   exit 1
 fi
@@ -158,121 +155,121 @@ fi
 run_network_tasks() {
 
   # Run hardhat tasks for different networks
-  if [ -n "$LC_GOERLI" ]; then
+  if [ -n "${LC_GOERLI}" ]; then
     echo "Starting light client for Goerli network"
     process-compose process start goerli
   else
     echo "Skipping Goerli network"
   fi
 
-  if [ -n "$LC_OPTIMISTIC_GOERLI" ]; then
+  if [ -n "${LC_OPTIMISTIC_GOERLI}" ]; then
     echo "Starting light client for Optimistic Goerli network"
     process-compose process start goeoptimisticGoerlirli
   else
     echo "Skipping Optimistic Goerli network"
   fi
 
-  if [ -n "$LC_BASE_GOERLI" ]; then
+  if [ -n "${LC_BASE_GOERLI}" ]; then
     echo "Starting light client for Base Goerli network"
     process-compose process start baseGoerli
   else
     echo "Skipping Base Goerli network"
   fi
 
-  if [ -n "$LC_ARBITRUM_GOERLI" ]; then
+  if [ -n "${LC_ARBITRUM_GOERLI}" ]; then
     echo "Starting light client for Arbitrum Goerli network"
     process-compose process start arbitrumGoerli
   else
     echo "Skipping Arbitrum Goerli network"
   fi
 
-  if [ -n "$LC_SEPOLIA" ]; then
+  if [ -n "${LC_SEPOLIA}" ]; then
     echo "Starting light client for Sepolia network"
     process-compose process start sepolia
   else
     echo "Skipping Sepolia network"
   fi
 
-  if [ -n "$LC_MUMBAI" ]; then
+  if [ -n "${LC_MUMBAI}" ]; then
     echo "Starting light client for Mumbai network"
     process-compose process start mumbai
   else
     echo "Skipping Mumbai network"
   fi
 
-  if [ -n "$LC_FUJI" ]; then
+  if [ -n "${LC_FUJI}" ]; then
     echo "Starting light client for Fuji network"
     process-compose process start fuji
   else
     echo "Skipping Fuji network"
   fi
 
-  if [ -n "$LC_FANTOM" ]; then
+  if [ -n "${LC_FANTOM}" ]; then
     echo "Starting light client for Fantom network"
     process-compose process start fantom
   else
     echo "Skipping Fantom network"
   fi
 
-  if [ -n "$LC_ALFAJORES" ]; then
+  if [ -n "${LC_ALFAJORES}" ]; then
     echo "Starting light client for Alfajores network"
     process-compose process start alfajores
   else
     echo "Skipping Alfajores network"
   fi
 
-  if [ -n "$LC_BSC" ]; then
+  if [ -n "${LC_BSC}" ]; then
     echo "Starting light client for BSC network"
     process-compose process start bsc
   else
     echo "Skipping BSC network"
   fi
 
-  if [ -n "$LC_AURORA" ]; then
+  if [ -n "${LC_AURORA}" ]; then
     echo "Starting light client for Aurora network"
     process-compose process start aurora
   else
     echo "Skipping Aurora network"
   fi
 
-  if [ -n "$LC_GNOSIS" ]; then
+  if [ -n "${LC_GNOSIS}" ]; then
     echo "Starting light client for Gnosis network"
     process-compose process start gnosis
   else
     echo "Skipping Gnosis network"
   fi
 
-  if [ -n "$LC_CHIADO" ]; then
+  if [ -n "${LC_CHIADO}" ]; then
     echo "Starting light client for Chiado network"
     process-compose process start chiado
   else
     echo "Skipping Chiado network"
   fi
 
-  if [ -n "$LC_EVMOS" ]; then
+  if [ -n "${LC_EVMOS}" ]; then
     echo "Starting light client for EVMOS network"
     process-compose process start evmos
   else
     echo "Skipping EVMOS network"
   fi
 
-  if [ -n "$LC_MALAGA" ]; then
+  if [ -n "${LC_MALAGA}" ]; then
     echo "Starting light client for Malaga network"
     process-compose process start malaga
   else
     echo "Skipping Malaga network"
   fi
 
-  echo "Everything started for $FOLLOW_NETWORK"
+  echo "Everything started for ${FOLLOW_NETWORK}"
 }
 
 # Call the function based on PRATTER or MAINNET
-if [[ "$PRATTER" == "TRUE" ]]; then
+if [[ "${PRATTER}" == "TRUE" ]]; then
   export FOLLOW_NETWORK="pratter"
   run_network_tasks
 fi
 
-if [[ "$MAINNET" == "TRUE" ]]; then
+if [[ "${MAINNET}" == "TRUE" ]]; then
   export FOLLOW_NETWORK="mainnet"
   run_network_tasks
 fi
