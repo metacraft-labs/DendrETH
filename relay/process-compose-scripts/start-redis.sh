f4882dd9c72bc5be6a5e07726cdb45153e98e5d8
Kristin Kirkov <krischanss99@gmail.com>
2024-04-29 17:17:15 +0300

    feat(relay): Making process-compose handle all of 'run-relay.sh'

---
+++ relay/process-compose-scripts/start-redis.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+source "${BASH_SOURCE%/*}/../../scripts/utils/paths.sh"
+source "${ROOT}/.env"
+
+# Check if Redis is running
+if pgrep redis-server >/dev/null; then
+  echo "Redis is already running."
+else
+  echo "Redis is not running. Starting a new one"
+
+  # Check for redis environment variables
+  if [[ -z "${REDIS_HOST}" ]] && [[ -z "${REDIS_PORT}" ]]; then
+    echo "REDIS_HOST and REDIS_PORT environment variables are not set. Using default values."
+    REDIS_HOST="localhost"
+    REDIS_PORT="6379"
+  else
+    echo "Using Redis settings from environment variables"
+  fi
+
+  if [[ "${REDIS_HOST}" == "localhost" ]] && [[ "${REDIS_PORT}" == "6379" ]]; then
+    echo "Starting local Redis server..."
+    redis-server --appendonly yes
+    echo "Local Redis server started"
+  else
+    echo "Using remote Redis server at ${REDIS_HOST}:${REDIS_PORT}"
+  fi
+fi
