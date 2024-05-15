944fc8ddffb3e404e42083773772f84cd3693f9b
Xearty <xeartybg@gmail.com>
2024-05-15 03:43:34 +0300

    fixed shit that havent been fixed in last commit

--- beacon-light-client/plonky2/crates/circuit_derive/src/lib.rs
+++ beacon-light-client/plonky2/crates/circuit_derive/src/lib.rs
@@ -3,6 +3,7 @@ use derive_circuit_target::impl_derive_circuit_target;
 use derive_public_inputs_readable::impl_derive_public_inputs_readable;
 use derive_set_witness::impl_derive_set_witness;
 use derive_target_primitive::impl_derive_target_primitive;
+use proc_macro::TokenStream;
 use syn::parse_macro_input;
 use syn::DeriveInput;
 use utils::create_struct_with_fields;
@@ -17,31 +18,31 @@ mod derive_target_primitive;
 mod utils;
 
 #[proc_macro_derive(TargetPrimitive)]
-pub fn derive_target_primitive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input_ast: DeriveInput = parse_macro_input!(input as DeriveInput);
+pub fn derive_target_primitive(input: TokenStream) -> TokenStream {
+    let input_ast = parse_macro_input!(input as DeriveInput);
     impl_derive_target_primitive(input_ast).into()
 }
 
 #[proc_macro_derive(AddVirtualTarget)]
-pub fn derive_add_virtual_target(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input_ast: DeriveInput = parse_macro_input!(input as DeriveInput);
+pub fn derive_add_virtual_target(input: TokenStream) -> TokenStream {
+    let input_ast = parse_macro_input!(input as DeriveInput);
     impl_derive_add_virtual_target(input_ast).into()
 }
 
 #[proc_macro_derive(PublicInputsReadable, attributes(serde))]
-pub fn derive_public_inputs_readable(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input_ast: DeriveInput = parse_macro_input!(input as DeriveInput);
+pub fn derive_public_inputs_readable(input: TokenStream) -> TokenStream {
+    let input_ast = parse_macro_input!(input as DeriveInput);
     impl_derive_public_inputs_readable(input_ast).into()
 }
 
 #[proc_macro_derive(CircuitTarget, attributes(target, serde))]
-pub fn derive_circuit_target(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input_ast: DeriveInput = parse_macro_input!(input as DeriveInput);
+pub fn derive_circuit_target(input: TokenStream) -> TokenStream {
+    let input_ast = parse_macro_input!(input as DeriveInput);
     impl_derive_circuit_target(input_ast).into()
 }
 
 #[proc_macro_derive(SetWitness, attributes(serde))]
-pub fn derive_set_witness(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input_ast: DeriveInput = parse_macro_input!(input as DeriveInput);
+pub fn derive_set_witness(input: TokenStream) -> TokenStream {
+    let input_ast = parse_macro_input!(input as DeriveInput);
     impl_derive_set_witness(input_ast).into()
 }
