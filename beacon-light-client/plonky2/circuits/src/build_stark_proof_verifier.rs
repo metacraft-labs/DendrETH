ed9f1439f55e18dde83a4734ef551386ed259ad9
Dimo99 <dimodimov1999@gmail.com>
2024-05-18 16:22:26 +0300

    feat: Introduce final exponentiate

---
+++ beacon-light-client/plonky2/circuits/src/build_stark_proof_verifier.rs
@@ -0,0 +1,165 @@
+use plonky2::{
+    field::extension::Extendable,
+    hash::hash_types::RichField,
+    iop::target::Target,
+    plonk::{
+        circuit_builder::CircuitBuilder,
+        circuit_data::{CircuitConfig, CircuitData},
+        config::{AlgebraicHasher, GenericConfig},
+    },
+    util::serialization::{Buffer, IoResult, Read, Write},
+};
+use starky::{
+    config::StarkConfig,
+    proof::{StarkOpeningSetTarget, StarkProofTarget, StarkProofWithPublicInputsTarget},
+    stark::Stark,
+};
+use starky_bls12_381::aggregate_proof::define_recursive_proof;
+
+use crate::targets_serialization::{ReadTargets, WriteTargets};
+
+const D: usize = 2;
+
+// Maybe handled by Marti refactoring
+impl ReadTargets for StarkProofWithPublicInputsTarget<D> {
+    fn read_targets(data: &mut Buffer) -> IoResult<Self>
+    where
+        Self: Sized,
+    {
+        let proof = StarkProofWithPublicInputsTarget {
+            proof: StarkProofTarget {
+                trace_cap: data.read_target_merkle_cap()?,
+                auxiliary_polys_cap: if data.read_bool()? {
+                    Some(data.read_target_merkle_cap()?)
+                } else {
+                    None
+                },
+                quotient_polys_cap: data.read_target_merkle_cap()?,
+                openings: StarkOpeningSetTarget {
+                    local_values: data.read_target_ext_vec()?,
+                    next_values: data.read_target_ext_vec()?,
+                    auxiliary_polys: if data.read_bool()? {
+                        Some(data.read_target_ext_vec()?)
+                    } else {
+                        None
+                    },
+                    auxiliary_polys_next: if data.read_bool()? {
+                        Some(data.read_target_ext_vec()?)
+                    } else {
+                        None
+                    },
+                    ctl_zs_first: if data.read_bool()? {
+                        Some(data.read_target_vec()?)
+                    } else {
+                        None
+                    },
+                    quotient_polys: data.read_target_ext_vec()?,
+                },
+                opening_proof: data.read_target_fri_proof()?,
+            },
+            public_inputs: data.read_target_vec()?,
+        };
+
+        Ok(proof)
+    }
+}
+
+impl WriteTargets for StarkProofWithPublicInputsTarget<D> {
+    fn write_targets(&self) -> IoResult<Vec<u8>> {
+        let mut target_bytes: Vec<u8> = Vec::new();
+
+        target_bytes.write_target_merkle_cap(&self.proof.trace_cap)?;
+
+        target_bytes.write_bool(self.proof.auxiliary_polys_cap.is_some())?;
+
+        if let Some(auxiliary_polys_cap) = &self.proof.auxiliary_polys_cap {
+            target_bytes.write_target_merkle_cap(auxiliary_polys_cap)?;
+        }
+
+        target_bytes.write_target_merkle_cap(&self.proof.quotient_polys_cap)?;
+        target_bytes.write_target_ext_vec(&self.proof.openings.local_values)?;
+        target_bytes.write_target_ext_vec(&self.proof.openings.next_values)?;
+
+        target_bytes.write_bool(self.proof.openings.auxiliary_polys.is_some())?;
+
+        if let Some(auxiliary_polys) = &self.proof.openings.auxiliary_polys {
+            target_bytes.write_target_ext_vec(auxiliary_polys)?;
+        }
+
+        target_bytes.write_bool(self.proof.openings.auxiliary_polys_next.is_some())?;
+
+        if let Some(auxiliary_polys_next) = &self.proof.openings.auxiliary_polys_next {
+            target_bytes.write_target_ext_vec(auxiliary_polys_next)?;
+        }
+
+        target_bytes.write_bool(self.proof.openings.ctl_zs_first.is_some())?;
+
+        if let Some(ctl_zs_first) = &self.proof.openings.ctl_zs_first {
+            target_bytes.write_target_vec(ctl_zs_first)?;
+        }
+
+        target_bytes.write_target_ext_vec(&self.proof.openings.quotient_polys)?;
+
+        target_bytes.write_target_fri_proof(&self.proof.opening_proof)?;
+
+        target_bytes.write_target_vec(&self.public_inputs)?;
+
+        Ok(target_bytes)
+    }
+}
+
+pub struct RecursiveStarkTargets {
+    pub proof: StarkProofWithPublicInputsTarget<D>,
+    pub zero: Target,
+}
+
+impl ReadTargets for RecursiveStarkTargets {
+    fn read_targets(data: &mut Buffer) -> IoResult<Self>
+    where
+        Self: Sized,
+    {
+        let proof = StarkProofWithPublicInputsTarget::read_targets(data)?;
+        let zero = data.read_target()?;
+
+        Ok(RecursiveStarkTargets { proof, zero })
+    }
+}
+
+impl WriteTargets for RecursiveStarkTargets {
+    fn write_targets(&self) -> IoResult<Vec<u8>> {
+        let mut target_bytes = self.proof.write_targets()?;
+        target_bytes.write_target(self.zero)?;
+
+        Ok(target_bytes)
+    }
+}
+
+pub fn build_stark_proof_verifier<
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+    S: Stark<F, D> + Copy,
+>(
+    stark: S,
+    inner_proof: &starky::proof::StarkProofWithPublicInputs<F, C, D>,
+    inner_config: &StarkConfig,
+) -> (RecursiveStarkTargets, CircuitData<F, C, D>)
+where
+    C::Hasher: AlgebraicHasher<F>,
+{
+    let circuit_config = CircuitConfig::standard_recursion_config();
+    let mut builder = CircuitBuilder::<F, D>::new(circuit_config);
+
+    let proof = define_recursive_proof::<F, C, S, C, D>(
+        stark,
+        inner_proof,
+        &inner_config,
+        false,
+        &mut builder,
+    );
+
+    let zero = builder.zero();
+
+    let data = builder.build::<C>();
+
+    (RecursiveStarkTargets { proof, zero }, data)
+}
---
+++ beacon-light-client/plonky2/circuits_executables/bin/final_exponentiate_circuit_data_generation.rs
@@ -0,0 +1,109 @@
+use std::{fs, marker::PhantomData, time::Instant};
+
+use ark_std::UniformRand;
+use circuits::{
+    build_stark_proof_verifier::build_stark_proof_verifier, targets_serialization::WriteTargets,
+};
+use plonky2::{
+    field::goldilocks_field::GoldilocksField,
+    plonk::config::{GenericConfig, PoseidonGoldilocksConfig},
+};
+use plonky2_circuit_serializer::serializer::{CustomGateSerializer, CustomGeneratorSerializer};
+use starky::{config::StarkConfig, proof::StarkProofWithPublicInputs};
+use starky_bls12_381::{
+    aggregate_proof::final_exponentiate_main,
+    final_exponentiate::FinalExponentiateStark,
+    native::{Fp, Fp12},
+};
+
+const D: usize = 2;
+type C = PoseidonGoldilocksConfig;
+type F = <C as GenericConfig<D>>::F;
+type FeStark = FinalExponentiateStark<F, D>;
+
+fn main_thread() {
+    // let (validators_balance_verification_targets, first_level_data) = build_final_exponentiate();
+    let rng = &mut ark_std::rand::thread_rng();
+
+    let fq = ark_bls12_381::Fq12::rand(rng);
+
+    let fp12 = Fp12([
+        Fp::get_fp_from_biguint(fq.c0.c0.c0.0.into()),
+        Fp::get_fp_from_biguint(fq.c0.c0.c1.0.into()),
+        Fp::get_fp_from_biguint(fq.c0.c1.c0.0.into()),
+        Fp::get_fp_from_biguint(fq.c0.c1.c1.0.into()),
+        Fp::get_fp_from_biguint(fq.c0.c2.c0.0.into()),
+        Fp::get_fp_from_biguint(fq.c0.c2.c1.0.into()),
+        Fp::get_fp_from_biguint(fq.c1.c0.c0.0.into()),
+        Fp::get_fp_from_biguint(fq.c1.c0.c1.0.into()),
+        Fp::get_fp_from_biguint(fq.c1.c1.c0.0.into()),
+        Fp::get_fp_from_biguint(fq.c1.c1.c1.0.into()),
+        Fp::get_fp_from_biguint(fq.c1.c2.c0.0.into()),
+        Fp::get_fp_from_biguint(fq.c1.c2.c1.0.into()),
+    ]);
+
+    println!("Final exponetiate stark proof started");
+
+    let s = Instant::now();
+
+    let (stark_final_exp, proof_final_exp, config_final_exp): (
+        FeStark,
+        StarkProofWithPublicInputs<GoldilocksField, PoseidonGoldilocksConfig, 2>,
+        StarkConfig,
+    ) = final_exponentiate_main::<F, C, D>(fp12);
+
+    println!("Final exponetiate stark proof done in {:?}", s.elapsed());
+
+    let s = Instant::now();
+
+    let (recursive_stark_targets, data) =
+        build_stark_proof_verifier::<GoldilocksField, PoseidonGoldilocksConfig, FeStark>(
+            stark_final_exp,
+            &proof_final_exp,
+            &config_final_exp,
+        );
+
+    println!(
+        "time taken for building plonky2 recursive circuit data {:?}",
+        s.elapsed()
+    );
+
+    println!("Starting serialization");
+
+    let s = Instant::now();
+
+    let circuit_bytes = data
+        .to_bytes(
+            &CustomGateSerializer,
+            &CustomGeneratorSerializer {
+                _phantom: PhantomData::<PoseidonGoldilocksConfig>,
+            },
+        )
+        .unwrap();
+
+    fs::write(
+        "circuits/final_exponentiate_circuit.plonky2_circuit",
+        &circuit_bytes,
+    )
+    .unwrap();
+
+    let targets = recursive_stark_targets.write_targets().unwrap();
+
+    fs::write(
+        "circuits/final_exponentiate_circuit.plonky2_targets",
+        &targets,
+    )
+    .unwrap();
+
+    println!("time taken for serialization {:?}", s.elapsed());
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .spawn(|| {
+            main_thread();
+        })
+        .unwrap()
+        .join()
+        .unwrap();
+}
