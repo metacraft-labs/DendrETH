082e7002eb81b8b17007a06c643dd3c4aef7cfa5
Martin Ivanov <xeartybg@gmail.com>
2024-05-22 14:01:00 +0300

    delete an unused function

--- beacon-light-client/plonky2/crates/circuit_executables/src/provers.rs
+++ beacon-light-client/plonky2/crates/circuit_executables/src/provers.rs
@@ -11,45 +11,6 @@ use plonky2::{
 
 use anyhow::Result;
 
-pub fn handle_generic_inner_level_proof(
-    proof1_bytes: Vec<u8>,
-    proof2_bytes: Vec<u8>,
-    inner_circuit_data: &CircuitData<GoldilocksField, PoseidonGoldilocksConfig, 2>,
-    proof1_target: &ProofWithPublicInputsTarget<2>,
-    proof2_target: &ProofWithPublicInputsTarget<2>,
-    verifier_circuit_target: &VerifierCircuitTarget,
-    circuit_data: &CircuitData<GoldilocksField, PoseidonGoldilocksConfig, 2>,
-) -> Result<ProofWithPublicInputs<GoldilocksField, PoseidonGoldilocksConfig, 2>> {
-    let inner_proof1 =
-        ProofWithPublicInputs::<GoldilocksField, PoseidonGoldilocksConfig, 2>::from_bytes(
-            proof1_bytes,
-            &inner_circuit_data.common,
-        )?;
-
-    let inner_proof2 =
-        ProofWithPublicInputs::<GoldilocksField, PoseidonGoldilocksConfig, 2>::from_bytes(
-            proof2_bytes,
-            &inner_circuit_data.common,
-        )?;
-
-    let mut pw = PartialWitness::new();
-
-    pw.set_proof_with_pis_target(proof1_target, &inner_proof1);
-    pw.set_proof_with_pis_target(proof2_target, &inner_proof2);
-
-    pw.set_cap_target(
-        &verifier_circuit_target.constants_sigmas_cap,
-        &inner_circuit_data.verifier_only.constants_sigmas_cap,
-    );
-
-    pw.set_hash_target(
-        verifier_circuit_target.circuit_digest,
-        inner_circuit_data.verifier_only.circuit_digest,
-    );
-
-    Ok(circuit_data.prove(pw)?)
-}
-
 pub fn prove_inner_level(
     proof1_bytes: Vec<u8>,
     proof2_bytes: Vec<u8>,
