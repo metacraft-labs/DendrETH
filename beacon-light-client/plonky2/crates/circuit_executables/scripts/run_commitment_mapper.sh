ff1ebe9c7145f576b3d8d32591f46420eccd700f
Martin Ivanov <xeartybg@gmail.com>
2024-05-25 07:47:33 +0300

    chore(balance_verification_scripts): update scripts to accept protocol, redis socket address and proof storage directory

--- beacon-light-client/plonky2/crates/circuit_executables/scripts/run_commitment_mapper.sh
+++ beacon-light-client/plonky2/crates/circuit_executables/scripts/run_commitment_mapper.sh
@@ -1,12 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -euo pipefail
 
-(
+source $(dirname ${BASH_SOURCE[0]})/parse_common_cmdline_opts.sh
+
 pushd $GIT_ROOT/beacon-light-client/plonky2/crates/circuit_executables
-
-echo $(pwd)
-
-cargo run --bin commitment_mapper --release -- --proof-storage-type file --folder-name proofs
- popd
-)
+cargo run --bin commitment_mapper --release -- --proof-storage-type file --folder-name $PROOF_STORAGE_DIR --redis $REDIS_ADDRESS
+popd
--- beacon-light-client/plonky2/crates/circuit_executables/scripts/run_final_layer.sh
+++ beacon-light-client/plonky2/crates/circuit_executables/scripts/run_final_layer.sh
@@ -1,12 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -euo pipefail
 
-(
+source $(dirname ${BASH_SOURCE[0]})/parse_common_cmdline_opts.sh
+
 pushd $GIT_ROOT/beacon-light-client/plonky2/crates/circuit_executables
-
-echo $(pwd)
-
-cargo run --bin final_layer --release -- --proof-storage-type file  --folder-name /mnt/solunka-server-dendreth/sepolia --protocol $1 --redis redis://solunska-server:6379/
+cargo run --bin final_layer --release -- --proof-storage-type file  --folder-name $PROOF_STORAGE_DIR --protocol $PROTOCOL --redis $REDIS_ADDRESS
 popd
-)
