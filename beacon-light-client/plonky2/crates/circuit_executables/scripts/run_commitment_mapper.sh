e7b561612f1524e696cbe0cff159d73d4ef4d3d5
Martin Ivanov <xeartybg@gmail.com>
2024-05-26 07:07:10 +0300

    feat(balance_verification_scripts): Make popd silent

--- beacon-light-client/plonky2/crates/circuit_executables/scripts/run_commitment_mapper.sh
+++ beacon-light-client/plonky2/crates/circuit_executables/scripts/run_commitment_mapper.sh
@@ -6,4 +6,4 @@ source $(dirname ${BASH_SOURCE[0]})/parse_common_cmdline_opts.sh
 
 pushd $GIT_ROOT/beacon-light-client/plonky2/crates/circuit_executables
 cargo run --bin commitment_mapper --release -- --proof-storage-type file --folder-name $PROOF_STORAGE_DIR --redis $REDIS_ADDRESS
-popd
+command popd "$@" > /dev/null
--- beacon-light-client/plonky2/crates/circuit_executables/scripts/run_final_layer.sh
+++ beacon-light-client/plonky2/crates/circuit_executables/scripts/run_final_layer.sh
@@ -6,4 +6,4 @@ source $(dirname ${BASH_SOURCE[0]})/parse_common_cmdline_opts.sh
 
 pushd $GIT_ROOT/beacon-light-client/plonky2/crates/circuit_executables
 cargo run --bin final_layer --release -- --proof-storage-type file  --folder-name $PROOF_STORAGE_DIR --protocol $PROTOCOL --redis $REDIS_ADDRESS
-popd
+command popd "$@" > /dev/null
