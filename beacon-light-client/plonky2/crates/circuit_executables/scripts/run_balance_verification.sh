04c9fd8e1a7b3949ad1eebd3b9d509b8849cb29e
Martin Ivanov <xeartybg@gmail.com>
2024-05-26 08:41:42 +0300

    chore(balance_verification_scripts): Make pushd silent

--- beacon-light-client/plonky2/crates/circuit_executables/scripts/run_balance_verification.sh
+++ beacon-light-client/plonky2/crates/circuit_executables/scripts/run_balance_verification.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 source $(dirname ${BASH_SOURCE[0]})/parse_common_cmdline_opts.sh
 
-pushd $GIT_ROOT/beacon-light-client/plonky2/crates/circuit_executables
+command pushd $GIT_ROOT/beacon-light-client/plonky2/crates/circuit_executables "$@" > /dev/null
 
 for i in $(seq 0 37); do
     cargo run --bin balance_verification --release -- --preserve-intermediary-proofs --level $i --stop-after 0 --protocol $PROTOCOL --proof-storage-type file --folder-name $PROOF_STORAGE_DIR --redis $REDIS_ADDRESS
--- beacon-light-client/plonky2/crates/circuit_executables/scripts/run_commitment_mapper.sh
+++ beacon-light-client/plonky2/crates/circuit_executables/scripts/run_commitment_mapper.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 source $(dirname ${BASH_SOURCE[0]})/parse_common_cmdline_opts.sh
 
-pushd $GIT_ROOT/beacon-light-client/plonky2/crates/circuit_executables
+command pushd $GIT_ROOT/beacon-light-client/plonky2/crates/circuit_executables "$@" > /dev/null
 cargo run --bin commitment_mapper --release -- --proof-storage-type file --folder-name $PROOF_STORAGE_DIR --redis $REDIS_ADDRESS
 command popd "$@" > /dev/null
