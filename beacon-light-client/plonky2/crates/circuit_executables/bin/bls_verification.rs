c838d3e5f6c6db57336e24c1d65acb794a49d55f
Stefan Nikolov <consultsen.stefan@gmail.com>
2024-05-27 18:16:00 +0300

    feat(bls_verification): Implement prove_bls(BLSData) fully

--- beacon-light-client/plonky2/crates/circuit_executables/bin/bls_verification.rs
+++ beacon-light-client/plonky2/crates/circuit_executables/bin/bls_verification.rs
@@ -1,8 +1,9 @@
 #![allow(incomplete_features)]
 #![feature(generic_const_exprs)]
 
-use ark_bls12_381::{G1Affine, G1Projective, G2Affine};
+use ark_bls12_381::{Fq, Fq2, G1Affine, G1Projective, G2Affine};
 use ark_ec::Group;
+use ark_ff::PrimeField;
 use ark_serialize::CanonicalDeserialize;
 use aws_sdk_s3::primitives::event_stream::Message;
 use circuit::{
@@ -10,7 +11,7 @@ use circuit::{
     SerdeCircuitTarget,
 };
 use circuit_executables::{
-    constants::{SERIALIZED_CIRCUITS_DIR, VALIDATOR_REGISTRY_LIMIT},
+    constants::{BLS_DST, SERIALIZED_CIRCUITS_DIR, VALIDATOR_REGISTRY_LIMIT},
     crud::{
         common::{
             delete_balance_verification_proof_dependencies, fetch_proofs_balances,
@@ -25,11 +26,11 @@ use circuit_executables::{
         generate_pairing_precomp_proof, prove_inner_level,
     },
     utils::{
-        parse_balance_verification_command_line_options, parse_config_file,
-        CommandLineOptionsBuilder,
+        parse_balance_verification_command_line_options, parse_bls_verification_command_line_options, parse_config_file, CommandLineOptionsBuilder
     },
 };
 use circuits::{
+    bls_verification::bls12_381_circuit::BlsCircuitTargets,
     common_targets::BasicRecursiveInnerCircuitTarget,
     types::BalanceProof,
     withdrawal_credentials_balance_aggregator::{
@@ -42,6 +43,7 @@ use num::BigUint;
 use snowbridge_amcl::bls381::{big::Big, bls381::utils::hash_to_curve_g2, ecp2::ECP2};
 use starky_bls12_381::native::{miller_loop, Fp, Fp12, Fp2};
 use std::{
+    ops::Neg,
     println,
     str::FromStr,
     thread,
@@ -52,15 +54,14 @@ use anyhow::Result;
 
 use futures_lite::future;
 use plonky2::{
-    field::goldilocks_field::GoldilocksField,
-    iop::witness::PartialWitness,
+    field::{goldilocks_field::GoldilocksField, types::Field},
+    iop::witness::{PartialWitness, WitnessWrite},
     plonk::{
         circuit_data::CircuitData, config::PoseidonGoldilocksConfig, proof::ProofWithPublicInputs,
     },
     util::serialization::Buffer,
 };
 
-use redis::aio::Connection;
 use redis_work_queue::{Item, KeyPrefix, WorkQueue};
 
 use jemallocator::Jemalloc;
@@ -90,7 +91,7 @@ async fn async_main() -> Result<()> {
         .with_proof_storage_options()
         .get_matches();
 
-    let config = parse_balance_verification_command_line_options(&matches);
+    let config = parse_bls_verification_command_line_options(&matches);
 
     println!("{}", "Connecting to Redis...".yellow());
     let client = redis::Client::open(config.redis_connection)?;
@@ -124,12 +125,8 @@ async fn async_main() -> Result<()> {
         &mut con,
         proof_storage.as_mut(),
         &queue,
-        &circuit_data,
-        start,
-        config.time_to_run,
         config.stop_after,
         config.lease_for,
-        &protocol,
     )
     .await
 }
@@ -138,14 +135,10 @@ async fn process_queue(
     con: &mut redis::aio::Connection,
     proof_storage: &mut dyn ProofStorage,
     queue: &WorkQueue,
-    circuit_data: &CircuitData<GoldilocksField, PoseidonGoldilocksConfig, 2>,
-    start: Instant,
-    time_to_run: Option<Duration>,
     stop_after: u64,
     lease_for: u64,
-    protocol: &str,
 ) -> Result<()> {
-    while time_to_run.is_none() || start.elapsed() < time_to_run.unwrap() {
+    loop {
         let queue_item = match queue
             .lease(
                 con,
@@ -170,17 +163,43 @@ async fn process_queue(
         }
 
         let data = prepare_bls_prover(queue_item).await;
-        prove_bls(data);
+
+        println!(
+            "{}",
+            format!(
+                "Processing task for index {}...",
+                data.pubkey.to_string().magenta()
+            )
+            .blue()
+            .bold()
+        );
+
+        let proof = prove_bls(data).await;
+
+        proof_storage
+            .set_proof("bls12_381_proof".to_string(), &proof.unwrap().to_bytes())
+            .await?;
     }
-
-    Ok(())
 }
 
-async fn prepare_bls_prover(item: Item) -> BLSData {
-    todo!()
+async fn prepare_bls_prover(queue_item: Item) -> BLSData {
+    BLSData {
+        dst: BLS_DST.to_string(),
+        pubkey: std::str::from_utf8(&queue_item.data[0..8])
+            .unwrap()
+            .to_string(),
+        signature: std::str::from_utf8(&queue_item.data[8..16])
+            .unwrap()
+            .to_string(),
+        msg: std::str::from_utf8(&queue_item.data[16..24])
+            .unwrap()
+            .to_string(),
+    }
 }
 
-async fn prove_bls(data: BLSData) -> Result<(), anyhow::Error> {
+async fn prove_bls(
+    data: BLSData,
+) -> Result<ProofWithPublicInputs<GoldilocksField, PoseidonGoldilocksConfig, 2>, anyhow::Error> {
     let dst = data.dst.as_str();
     let pubkey = data.pubkey.as_str();
     let signature = data.signature.as_str();
@@ -247,7 +266,55 @@ async fn prove_bls(data: BLSData) -> Result<(), anyhow::Error> {
         SERIALIZED_CIRCUITS_DIR, "bls12_381"
     ))?;
 
-    Ok(())
+    let mut target_buffer = Buffer::new(&target_bytes);
+
+    let targets = BlsCircuitTargets::deserialize(&mut target_buffer).unwrap();
+    let mut pw = PartialWitness::<GoldilocksField>::new();
+
+    pw.set_target_arr(
+        &targets.pubkey,
+        &hex::decode(pubkey)
+            .unwrap()
+            .iter()
+            .map(|x| GoldilocksField::from_canonical_usize(*x as usize))
+            .collect::<Vec<GoldilocksField>>(),
+    );
+    pw.set_target_arr(
+        &targets.sig,
+        &hex::decode(signature)
+            .unwrap()
+            .iter()
+            .map(|x| GoldilocksField::from_canonical_usize(*x as usize))
+            .collect::<Vec<GoldilocksField>>(),
+    );
+    pw.set_target_arr(
+        &targets.msg,
+        &hex::decode(msg)
+            .unwrap()
+            .iter()
+            .map(|x| GoldilocksField::from_canonical_usize(*x as usize))
+            .collect::<Vec<GoldilocksField>>(),
+    );
+
+    pw.set_proof_with_pis_target(&targets.pt_pp1, &pp1);
+    pw.set_proof_with_pis_target(&targets.pt_pp2, &pp2);
+    pw.set_proof_with_pis_target(&targets.pt_ml1, &ml1);
+    pw.set_proof_with_pis_target(&targets.pt_ml2, &ml2);
+    pw.set_proof_with_pis_target(&targets.pt_fp12m, &fp12_mul_proof);
+    pw.set_proof_with_pis_target(&targets.pt_fe, &final_exp_proof);
+
+    println!("Starting proof generation");
+
+    let proof = circuit_data.prove(pw).unwrap();
+
+    println!("Proof generated");
+
+    println!(
+        "Is valid signature {}",
+        proof.public_inputs[proof.public_inputs.len() - 1]
+    );
+
+    Ok(proof)
 }
 
 async fn handle_final_exponentiation(
--- beacon-light-client/plonky2/crates/circuit_executables/src/constants.rs
+++ beacon-light-client/plonky2/crates/circuit_executables/src/constants.rs
@@ -1,2 +1,3 @@
 pub const SERIALIZED_CIRCUITS_DIR: &str = "serialized_circuits";
 pub const VALIDATOR_REGISTRY_LIMIT: usize = 1099511627776;
+pub const BLS_DST: &str = "BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_";
