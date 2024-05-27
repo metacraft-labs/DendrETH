b0fd293c569a751344cafbea565b041109e5380b
Martin Ivanov <xeartybg@gmail.com>
2024-05-27 16:01:16 +0300

    refactor(): Delete dead code

--- beacon-light-client/plonky2/crates/circuits/src/utils/mod.rs
+++ beacon-light-client/plonky2/crates/circuits/src/utils/mod.rs
@@ -1,29 +1,6 @@
-use itertools::Itertools;
-use num_bigint::BigUint;
-use plonky2::hash::hash_types::RichField;
 use sha2::{Digest, Sha256};
 
 pub mod circuit;
-
-pub fn hex_string_from_field_element_bits<F: RichField>(bits: &[F]) -> String {
-    assert!(bits.len() % 4 == 0);
-    let bits = bits
-        .iter()
-        .map(|element| element.to_canonical_u64() != 0)
-        .collect_vec();
-
-    hex::encode(bits_to_bytes(&bits))
-}
-
-pub fn biguint_from_field_elements<F: RichField>(limbs: &[F]) -> BigUint {
-    BigUint::from_slice(
-        limbs
-            .iter()
-            .map(|element| element.to_canonical_u64() as u32)
-            .collect_vec()
-            .as_slice(),
-    )
-}
 
 pub fn u64_to_ssz_leaf(value: u64) -> [u8; 32] {
     let mut ret = vec![0u8; 32];
