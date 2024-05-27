fcb008d5995af63540b385121e2706998197b51b
Martin Ivanov <xeartybg@gmail.com>
2024-05-27 16:01:58 +0300

    refactor(circuit_utils): Rename poseidon hash function and refactor validator hash tree root

--- beacon-light-client/plonky2/crates/circuits/src/utils/circuit/hashing/merkle/poseidon.rs
+++ beacon-light-client/plonky2/crates/circuits/src/utils/circuit/hashing/merkle/poseidon.rs
@@ -1,15 +1,16 @@
+use circuit::ToTargets;
 use itertools::Itertools;
 use plonky2::{
     field::extension::Extendable,
-    hash::{
-        hash_types::{HashOutTarget, RichField},
-        poseidon::PoseidonHash,
-    },
+    hash::hash_types::{HashOutTarget, RichField},
     iop::target::BoolTarget,
     plonk::circuit_builder::CircuitBuilder,
 };
 
-use crate::{common_targets::ValidatorTarget, utils::circuit::hashing::poseidon::poseidon_pair};
+use crate::{
+    common_targets::ValidatorTarget,
+    utils::circuit::hashing::poseidon::{poseidon, poseidon_pair},
+};
 
 pub fn hash_tree_root_poseidon<F: RichField + Extendable<D>, const D: usize>(
     builder: &mut CircuitBuilder<F, D>,
@@ -47,54 +48,16 @@ pub fn hash_validator_poseidon<F: RichField + Extendable<D>, const D: usize>(
     builder: &mut CircuitBuilder<F, D>,
     validator: &ValidatorTarget,
 ) -> HashOutTarget {
-    let leaves = vec![
-        builder.hash_n_to_hash_no_pad::<PoseidonHash>(
-            validator.pubkey.iter().map(|x| x.target).collect(),
-        ),
-        builder.hash_n_to_hash_no_pad::<PoseidonHash>(
-            validator
-                .withdrawal_credentials
-                .iter()
-                .map(|x| x.target)
-                .collect(),
-        ),
-        builder.hash_n_to_hash_no_pad::<PoseidonHash>(
-            validator
-                .effective_balance
-                .limbs
-                .iter()
-                .map(|x| x.0)
-                .collect(),
-        ),
-        builder.hash_n_to_hash_no_pad::<PoseidonHash>(vec![validator.slashed.target]),
-        builder.hash_n_to_hash_no_pad::<PoseidonHash>(
-            validator
-                .activation_eligibility_epoch
-                .limbs
-                .iter()
-                .map(|x| x.0)
-                .collect(),
-        ),
-        builder.hash_n_to_hash_no_pad::<PoseidonHash>(
-            validator
-                .activation_epoch
-                .limbs
-                .iter()
-                .map(|x| x.0)
-                .collect(),
-        ),
-        builder.hash_n_to_hash_no_pad::<PoseidonHash>(
-            validator.exit_epoch.limbs.iter().map(|x| x.0).collect(),
-        ),
-        builder.hash_n_to_hash_no_pad::<PoseidonHash>(
-            validator
-                .withdrawable_epoch
-                .limbs
-                .iter()
-                .map(|x| x.0)
-                .collect(),
-        ),
+    let leaves = &[
+        poseidon(builder, validator.pubkey.to_targets()),
+        poseidon(builder, validator.withdrawal_credentials.to_targets()),
+        poseidon(builder, validator.effective_balance.to_targets()),
+        poseidon(builder, validator.slashed.to_targets()),
+        poseidon(builder, validator.activation_eligibility_epoch.to_targets()),
+        poseidon(builder, validator.activation_epoch.to_targets()),
+        poseidon(builder, validator.exit_epoch.to_targets()),
+        poseidon(builder, validator.withdrawable_epoch.to_targets()),
     ];
 
-    hash_tree_root_poseidon(builder, &leaves)
+    hash_tree_root_poseidon(builder, leaves)
 }
