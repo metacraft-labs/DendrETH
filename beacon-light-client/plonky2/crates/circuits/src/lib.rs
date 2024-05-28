ad848ab7b8ff4a31b8f94caad1e8b90572f841aa
Martin Ivanov <xeartybg@gmail.com>
2024-05-28 23:42:28 +0300

    refactor(deposits_accumulator_balances): Refactor final circuit

--- beacon-light-client/plonky2/crates/circuits/src/lib.rs
+++ beacon-light-client/plonky2/crates/circuits/src/lib.rs
@@ -4,11 +4,10 @@
 pub mod bls_verification;
 pub mod common_targets;
 pub mod deposits_accumulator_balance_aggregator;
-// pub mod prev_rebase_commitment_mapper;
+pub mod deposits_accumulator_commitment_mapper;
 pub mod final_layer;
 pub mod redis_storage_types;
 pub mod serializers;
 pub mod utils;
 pub mod validators_commitment_mapper;
 pub mod withdrawal_credentials_balance_aggregator;
-pub mod deposits_accumulator_commitment_mapper;
