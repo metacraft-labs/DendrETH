0655b357e6a688b3fd63b408daacee757a7b1525
Stefan Nikolov <consultsen.stefan@gmail.com>
2024-05-28 16:12:23 +0300

    feat(bls_verification): Initialization script creates serialize directory

--- beacon-light-client/plonky2/crates/circuit_executables/scripts/build_bls_circuit_data.sh
+++ beacon-light-client/plonky2/crates/circuit_executables/scripts/build_bls_circuit_data.sh
@@ -11,6 +11,14 @@ log() {
     echo -e "\n$(date +'%Y-%m-%d %H:%M:%S') - $msg\n"
 }
 
+create_serialized_circuits_folder() {
+    local folder_name="serialized_circuits"
+    if [ ! -d "$folder_name" ]; then
+        mkdir -p "$folder_name"
+        log "Created folder: ${GREEN}$folder_name${NC}"
+    fi
+}
+
 run_rust_command() {
     local bin_name="$1"
     local RUST_MIN_STACK="${2:-16777216}"  # Default value for RUST_MIN_STACK if not provided
@@ -18,6 +26,8 @@ run_rust_command() {
     RUST_MIN_STACK=16777216 cargo run --bin "$bin_name" --release
     log "Finished ${GREEN}${bin_name}${NC}"
 }
+
+create_serialized_circuits_folder
 
 run_rust_command "calc_pairing_precomp_circuit_data_generation"
 run_rust_command "final_exponentiate_circuit_data_generation"
