46ebc879b5923a0a242fa8fe46f4353e5491bad5
Martin Ivanov <xeartybg@gmail.com>
2024-06-04 12:41:43 +0300

    feat(circuits): Implement cached circuit builds

--- beacon-light-client/plonky2/crates/circuit_executables/src/constants.rs
+++ beacon-light-client/plonky2/crates/circuit_executables/src/constants.rs
@@ -1,2 +1,4 @@
-pub const SERIALIZED_CIRCUITS_DIR: &str = "serialized_circuits";
 pub const VALIDATOR_REGISTRY_LIMIT: usize = 1099511627776;
+
+// TODO: Delete this constant as it's defined in cached_circuit_build.rs
+pub const SERIALIZED_CIRCUITS_DIR: &str = "serialized_circuits";
