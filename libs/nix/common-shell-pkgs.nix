e25bd7e046b1509c009e8e77c16904586e9bc5f5
Dimo99 <dimodimov1999@gmail.com>
2024-06-05 14:43:02 +0300

    ci(gh-actions): Switch to self-hosted runners

--- libs/nix/common-shell-pkgs.nix
+++ libs/nix/common-shell-pkgs.nix
@@ -78,6 +78,10 @@ in
     # Developer tool to help you get up and running quickly with a new Rust
     # project by leveraging a pre-existing git repository as a template.
     cargo-generate
+
+    docker-client
+    curl
+    python3
   ]
   ++ lib.optionals (stdenv.isx86_64) [
     metacraft-labs.rapidsnark
