815560026e16074c9c0866f493914980b2031785
Nikolay Kostadinov <nikolaykostadinov21@gmail.com>
2024-06-11 19:29:07 +0300

    Refactor VerifyIsNotAtInfinityCircuit but test isn't successful

--- beacon-light-client/plonky2/crates/circuits/src/bls_verification/verify_is_not_at_infinity_circuit.rs
+++ beacon-light-client/plonky2/crates/circuits/src/bls_verification/verify_is_not_at_infinity_circuit.rs
@@ -1,7 +1,4 @@
-use circuit_derive::{
-    AddVirtualTarget, CircuitTarget, PublicInputsReadable, SerdeCircuitTarget, SetWitness,
-    TargetPrimitive,
-};
+use circuit_derive::{CircuitTarget, SerdeCircuitTarget};
 use plonky2::{
     iop::target::{BoolTarget, Target},
     plonk::{
@@ -10,27 +7,17 @@ use plonky2::{
         config::{GenericConfig, PoseidonGoldilocksConfig},
     },
 };
-use plonky2_crypto::{
-    biguint::{BigUintTarget, CircuitBuilderBiguint},
-    u32::arithmetic_u32::U32Target,
-};
+use plonky2_crypto::biguint::{BigUintTarget, CircuitBuilderBiguint};
 use starky_bls12_381::{
-    calc_pairing_precomp,
     fp2_plonky2::is_zero,
     fp_plonky2::{is_equal, FpTarget},
-    g1_plonky2::PointG1Target,
-    g2_plonky2::PointG2Target,
+    g1_plonky2::{pk_point_check, PointG1Target},
+    g2_plonky2::{signature_point_check, PointG2Target},
 };
 
 use circuit::{circuit_builder_extensions::CircuitBuilderExtensions, Circuit};
 
-use crate::{
-    common_targets::{PubkeyTarget, SignatureTarget},
-    serializers::serde_bool_array_to_hex_string,
-    utils::circuit::bits_to_bytes_target,
-};
-
-use super::bls12_381_circuit::{get_neg_generator, N};
+use super::bls12_381_circuit::get_neg_generator;
 
 type F = <C as GenericConfig<2>>::F;
 type C = PoseidonGoldilocksConfig;
@@ -41,24 +28,18 @@ pub struct VerifyIsNotAtInfinityCircuit;
 #[derive(CircuitTarget, SerdeCircuitTarget)]
 pub struct VerifyIsNotAtInfinityCircuitTargets {
     // Pub inputs
-    #[target(in)]
-    pub validator_credentials: ValidatorCredentials,
-}
+    #[target(in, out)]
+    pub pubkey_bytes: [Target; 48],
 
-#[derive(
-    Clone,
-    Debug,
-    TargetPrimitive,
-    SetWitness,
-    PublicInputsReadable,
-    AddVirtualTarget,
-    SerdeCircuitTarget,
-)]
-pub struct ValidatorCredentials {
-    #[serde(with = "serde_bool_array_to_hex_string")]
-    pub pubkey: PubkeyTarget,
-    #[serde(with = "serde_bool_array_to_hex_string")]
-    pub signature: SignatureTarget,
+    #[target(in, out)]
+    pub sig_bytes: [Target; 96],
+
+    pub pubkey_g1_x: BigUintTarget,
+    pub pubkey_g1_y: BigUintTarget,
+    pub sig_g2_x_c0: BigUintTarget,
+    pub sig_g2_x_c1: BigUintTarget,
+    pub sig_g2_y_c0: BigUintTarget,
+    pub sig_g2_y_c1: BigUintTarget,
 }
 
 impl Circuit for VerifyIsNotAtInfinityCircuit {
@@ -74,37 +55,52 @@ impl Circuit for VerifyIsNotAtInfinityCircuit {
         _: &Self::Params,
     ) -> Self::Target {
         let input = Self::read_circuit_input_target(builder);
-        let pubkey = input.validator_credentials.pubkey;
-        let signature = input.validator_credentials.signature;
 
-        let pubkey = bits_to_bytes_target(builder, &pubkey);
-        let pubkey = pubkey.into_iter().collect::<Vec<_>>().try_into().unwrap();
-        let pubkey_g1 = get_g1_point_from_public_inputs(&pubkey);
-        assert_pk_ne_not_generator(builder, pubkey_g1.to_owned());
-        let signature = bits_to_bytes_target(builder, &signature);
-        let signature = signature
-            .into_iter()
-            .collect::<Vec<_>>()
-            .try_into()
-            .unwrap();
-        let signature_g2 = get_g2_point_from_public_inputs(builder, &signature);
+        let pubkey_g1_x = builder.add_virtual_biguint_target(12);
+        let pubkey_g1_y = builder.add_virtual_biguint_target(12);
+
+        let sig_g2_x_c0 = builder.add_virtual_biguint_target(12);
+        let sig_g2_x_c1 = builder.add_virtual_biguint_target(12);
+        let sig_g2_y_c0 = builder.add_virtual_biguint_target(12);
+        let sig_g2_y_c1 = builder.add_virtual_biguint_target(12);
+        let pubkey_bytes = input.pubkey_bytes;
+
+        let pubkey_g1 = [pubkey_g1_x.to_owned(), pubkey_g1_y.to_owned()];
+        pk_point_check(builder, &pubkey_g1, &pubkey_bytes);
+        assert_pk_ne_not_generator(builder, &pubkey_g1);
         let is_g1_point_is_at_infinity = is_g1_point_is_at_infinity(builder, &pubkey_g1);
-        let is_g2_point_is_at_infinity = is_g2_point_is_at_infinity(builder, &signature_g2);
+
+        let sig_bytes = input.sig_bytes;
+        let sig_g2 = [
+            [sig_g2_x_c0.to_owned(), sig_g2_x_c1.to_owned()],
+            [sig_g2_y_c0.to_owned(), sig_g2_y_c1.to_owned()],
+        ];
+        signature_point_check(builder, &sig_g2, &sig_bytes);
+        let is_g2_point_is_at_infinity = is_g2_point_is_at_infinity(builder, &sig_g2);
+
         assert_g1_or_g2_point_arent_at_infinity(
             builder,
             is_g1_point_is_at_infinity,
             is_g2_point_is_at_infinity,
         );
 
-        let pubkey = input.validator_credentials.pubkey;
-        let signature = input.validator_credentials.signature;
         Self::Target {
-            validator_credentials: ValidatorCredentials { pubkey, signature },
+            pubkey_bytes,
+            sig_bytes,
+            pubkey_g1_x,
+            pubkey_g1_y,
+            sig_g2_x_c0,
+            sig_g2_x_c1,
+            sig_g2_y_c0,
+            sig_g2_y_c1,
         }
     }
 }
 
-fn assert_pk_ne_not_generator(builder: &mut CircuitBuilder<F, D>, public_key_point: PointG1Target) {
+fn assert_pk_ne_not_generator(
+    builder: &mut CircuitBuilder<F, D>,
+    public_key_point: &PointG1Target,
+) {
     let get_neg_generator = get_neg_generator(builder);
 
     let is_pk_point_x_eq_not_generator_x =
@@ -150,80 +146,14 @@ fn is_fp_zero(builder: &mut CircuitBuilder<F, D>, input: &FpTarget) -> BoolTarge
     builder.cmp_biguint(input, &zero)
 }
 
-fn get_g1_point_from_public_inputs(public_inputs: &[Target; 48]) -> PointG1Target {
-    let g1_x = BigUintTarget {
-        limbs: (0..N)
-            .into_iter()
-            .map(|i| U32Target(public_inputs[calc_pairing_precomp::X0_PUBLIC_INPUTS_OFFSET + i]))
-            .collect(),
-    };
-
-    let g1_y = BigUintTarget {
-        limbs: (0..N)
-            .into_iter()
-            .map(|i| U32Target(public_inputs[calc_pairing_precomp::X1_PUBLIC_INPUTS_OFFSET + i]))
-            .collect(),
-    };
-
-    [g1_x, g1_y]
-}
-
-fn get_g2_point_from_public_inputs(
-    builder: &mut CircuitBuilder<F, D>,
-    public_inputs: &[Target; 96],
-) -> PointG2Target {
-    let sig_point_x0 = BigUintTarget {
-        limbs: (0..N)
-            .into_iter()
-            .map(|i| U32Target(public_inputs[calc_pairing_precomp::X0_PUBLIC_INPUTS_OFFSET + i]))
-            .collect(),
-    };
-    let sig_point_x1 = BigUintTarget {
-        limbs: (0..N)
-            .into_iter()
-            .map(|i| U32Target(public_inputs[calc_pairing_precomp::X1_PUBLIC_INPUTS_OFFSET + i]))
-            .collect(),
-    };
-    let sig_point_y0 = BigUintTarget {
-        limbs: (0..N)
-            .into_iter()
-            .map(|i| U32Target(public_inputs[calc_pairing_precomp::Y0_PUBLIC_INPUTS_OFFSET + i]))
-            .collect(),
-    };
-    let sig_point_y1 = BigUintTarget {
-        limbs: (0..N)
-            .into_iter()
-            .map(|i| U32Target(public_inputs[calc_pairing_precomp::Y1_PUBLIC_INPUTS_OFFSET + i]))
-            .collect(),
-    };
-
-    let zero = builder.zero();
-    let one = builder.one();
-    for i in 0..N {
-        if i == 0 {
-            builder.connect(
-                public_inputs[calc_pairing_precomp::Z0_PUBLIC_INPUTS_OFFSET + i],
-                one,
-            );
-        } else {
-            builder.connect(
-                public_inputs[calc_pairing_precomp::Z0_PUBLIC_INPUTS_OFFSET + i],
-                zero,
-            );
-        }
-        builder.connect(
-            public_inputs[calc_pairing_precomp::Z1_PUBLIC_INPUTS_OFFSET + i],
-            zero,
-        );
-    }
-
-    [[sig_point_x0, sig_point_x1], [sig_point_y0, sig_point_y1]]
-}
-
 #[cfg(test)]
 pub mod tests {
+    use ark_bls12_381::{G1Affine, G2Affine};
+    use ark_serialize::CanonicalDeserialize;
     use circuit::{Circuit, CircuitInput, SetWitness};
+    use num::BigUint;
     use plonky2::iop::witness::PartialWitness;
+    use plonky2_crypto::biguint::WitnessBigUint;
 
     use super::VerifyIsNotAtInfinityCircuit;
 
@@ -231,36 +161,67 @@ pub mod tests {
     fn test_g1_or_g2_are_at_infinity() {
         let (targets, circuit) = VerifyIsNotAtInfinityCircuit::build(&());
 
-        let input =
-        serde_json::from_str::<CircuitInput<VerifyIsNotAtInfinityCircuit>>(r#"{
-            "validator_credentials": {
-                "pubkey": "b301803f8b5ac4a1133581fc676dfedc60d891dd5fa99028805e5ea5b08d3491af75d0707adab3b70c6a6a580217bf81",
-                "signature": "b23c46be3a001c63ca711f87a005c200cc550b9429d5f4eb38d74322144f1b63926da3388979e5321012fb1a0526bcd100b5ef5fe72628ce4cd5e904aeaa3279527843fae5ca9ca675f4f51ed8f83bbf7155da9ecc9663100a885d5dc6df96d9"
-            }
-          }"#).unwrap();
+        let pubkey = "b781956110d24e4510a8b5500b71529f8635aa419a009d314898e8c572a4f923ba643ae94bdfdf9224509177aa8e6b73";
+        println!("pubkey_as_bytes are: {:?}", pubkey.as_bytes());
+        let signature = "b735d0d0b03f51fcf3e5bc510b5a2cb266075322f5761a6954778714f5ab8831bc99454380d330f5c19d93436f0c4339041bfeecd2161a122c1ce8428033db8dda142768a48e582f5f9bde7d40768ac5a3b6a80492b73719f1523c5da35de275";
+        println!("signature_as_bytes are: {:?}", signature.as_bytes());
+
+        let input = serde_json::from_str::<CircuitInput<VerifyIsNotAtInfinityCircuit>>(
+            r#"{
+            "pubkey_bytes": [
+                98, 55, 56, 49, 57, 53, 54, 49, 49, 48, 100, 50, 52, 101, 52, 53, 49, 48, 97, 56, 98,
+                53, 53, 48, 48, 98, 55, 49, 53, 50, 57, 102, 56, 54, 51, 53, 97, 97, 52, 49, 57, 97,
+                48, 48, 57, 100, 51, 49, 52, 56, 57, 56, 101, 56, 99, 53, 55, 50, 97, 52, 102, 57, 50,
+                51, 98, 97, 54, 52, 51, 97, 101, 57, 52, 98, 100, 102, 100, 102, 57, 50, 50, 52, 53,
+                48, 57, 49, 55, 55, 97, 97, 56, 101, 54, 98, 55, 51
+            ],
+            "sig_bytes": [
+                98, 55, 51, 53, 100, 48, 100, 48, 98, 48, 51, 102, 53, 49, 102, 99, 102, 51, 101, 53,
+                98, 99, 53, 49, 48, 98, 53, 97, 50, 99, 98, 50, 54, 54, 48, 55, 53, 51, 50, 50, 102,
+                53, 55, 54, 49, 97, 54, 57, 53, 52, 55, 55, 56, 55, 49, 52, 102, 53, 97, 98, 56, 56,
+                51, 49, 98, 99, 57, 57, 52, 53, 52, 51, 56, 48, 100, 51, 51, 48, 102, 53, 99, 49, 57,
+                100, 57, 51, 52, 51, 54, 102, 48, 99, 52, 51, 51, 57, 48, 52, 49, 98, 102, 101, 101,
+                99, 100, 50, 49, 54, 49, 97, 49, 50, 50, 99, 49, 99, 101, 56, 52, 50, 56, 48, 51, 51,
+                100, 98, 56, 100, 100, 97, 49, 52, 50, 55, 54, 56, 97, 52, 56, 101, 53, 56, 50, 102,
+                53, 102, 57, 98, 100, 101, 55, 100, 52, 48, 55, 54, 56, 97, 99, 53, 97, 51, 98, 54, 97,
+                56, 48, 52, 57, 50, 98, 55, 51, 55, 49, 57, 102, 49, 53, 50, 51, 99, 53, 100, 97, 51,
+                53, 100, 101, 50, 55, 53
+            ]
+          }"#,
+        )
+        .unwrap();
+
+        let pubkey_g1: G1Affine =
+            G1Affine::deserialize_compressed_unchecked(&*hex::decode(pubkey).unwrap()).unwrap();
+        let signature_g2 =
+            G2Affine::deserialize_compressed_unchecked(&*hex::decode(signature).unwrap()).unwrap();
 
         let mut pw = PartialWitness::new();
         targets.set_witness(&mut pw, &input);
-
-        let proof = circuit.prove(pw).unwrap();
-        let _ = circuit.verify(proof);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_one_privkey() {
-        let (targets, circuit) = VerifyIsNotAtInfinityCircuit::build(&());
-
-        let input =
-        serde_json::from_str::<CircuitInput<VerifyIsNotAtInfinityCircuit>>(r#"{
-            "validator_credentials": {
-                "pubkey": "97f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb",
-                "signature": "a42ae16f1c2a5fa69c04cb5998d2add790764ce8dd45bf25b29b4700829232052b52352dcff1cf255b3a7810ad7269601810f03b2bc8b68cf289cf295b206770605a190b6842583e47c3d1c0f73c54907bfb2a602157d46a4353a20283018763"
-            }
-          }"#).unwrap();
-
-        let mut pw = PartialWitness::new();
-        targets.set_witness(&mut pw, &input);
+        pw.set_biguint_target(
+            &targets.pubkey_g1_x,
+            &BigUint::try_from(pubkey_g1.x).unwrap(),
+        );
+        pw.set_biguint_target(
+            &targets.pubkey_g1_y,
+            &BigUint::try_from(pubkey_g1.y).unwrap(),
+        );
+        pw.set_biguint_target(
+            &targets.sig_g2_x_c0,
+            &BigUint::try_from(signature_g2.x.c0).unwrap(),
+        );
+        pw.set_biguint_target(
+            &targets.sig_g2_x_c1,
+            &BigUint::try_from(signature_g2.x.c1).unwrap(),
+        );
+        pw.set_biguint_target(
+            &targets.sig_g2_y_c0,
+            &BigUint::try_from(signature_g2.y.c0).unwrap(),
+        );
+        pw.set_biguint_target(
+            &targets.sig_g2_y_c1,
+            &BigUint::try_from(signature_g2.y.c1).unwrap(),
+        );
 
         let proof = circuit.prove(pw).unwrap();
         let _ = circuit.verify(proof);
