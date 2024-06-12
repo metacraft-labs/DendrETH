5141bff5283216687c861dc4dade391cb3405365
Dimo99 <dimodimov1999@gmail.com>
2024-06-12 17:45:42 +0300

    fix(contracts) Verifier digest mutability

--- beacon-light-client/solidity/contracts/balance_verifier/BalanceVerifierDiva.sol
+++ beacon-light-client/solidity/contracts/balance_verifier/BalanceVerifierDiva.sol
@@ -43,7 +43,7 @@ contract BalanceVerifierDiva is BalanceVerifier, IBalanceVerifierDiva {
     uint64 _numberOfSlashedValidators
   ) external override {
     uint256[] memory publicInputs = new uint256[](2);
-    publicInputs[0] = VERIFIER_DIGEST;
+    publicInputs[0] = verifierDigest;
     publicInputs[1] = (uint256(
       sha256(
         abi.encodePacked(
