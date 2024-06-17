cfdbec388b3b5cdbe180faed4f7f7e5e0f43d3de
Aneta Tsvetkova <anetastsvetkova@gmail.com>
2024-06-17 16:08:57 +0300

    fix(contracts): Remove binary search redundant logic

--- beacon-light-client/solidity/test/ValidatorAccumulator.test.ts
+++ beacon-light-client/solidity/test/ValidatorAccumulator.test.ts
@@ -10,6 +10,9 @@ import {
 import { expect } from 'chai';
 import depositItems from './utils/depositData.json';
 
+const zeroHash =
+  '0x985e929f70af28d0bdd1a90a808f977f597c7c778c489e98d3bd8910d31ac0f7';
+
 describe('ValidatorsAccumulator tests', async function () {
   let validatorAccumulator: Contract;
   let pubkeys: Uint8Array[] = [];
@@ -34,16 +37,19 @@ describe('ValidatorsAccumulator tests', async function () {
     ]);
   });
 
+  async function depositTransaction(depositItem: any) {
+    return validatorAccumulator.deposit(
+      depositItem.pubkey,
+      depositItem.withdrawalCredentials,
+      depositItem.signature,
+      depositItem.depositDataRoot,
+      { value: ethers.utils.parseEther('32').toString() },
+    );
+  }
+
   async function deposit(depositItem: any) {
-    await (
-      await validatorAccumulator.deposit(
-        depositItem.pubkey,
-        depositItem.withdrawalCredentials,
-        depositItem.signature,
-        depositItem.depositDataRoot,
-        { value: ethers.utils.parseEther('32').toString() },
-      )
-    ).wait();
+    const tx = await depositTransaction(depositItem);
+    await tx.wait();
 
     const pubkey = hexToBytes(depositItem.pubkey);
 
@@ -72,7 +78,7 @@ describe('ValidatorsAccumulator tests', async function () {
     }
   });
 
-  it('Should find correct accumulator in consecutive blocks and prune all before it', async function () {
+  it('Should find correct accumulator in consecutive blocks', async function () {
     const startBlock =
       Number(await network.provider.send('eth_blockNumber')) + 1;
     for (const depositItem of depositItems) {
@@ -81,14 +87,14 @@ describe('ValidatorsAccumulator tests', async function () {
 
     const index = Math.floor(depositItems.length / 2);
 
-    const [count, accumulator] =
-      await validatorAccumulator.findAccumulatorByBlock(startBlock + index);
+    const accumulator = await validatorAccumulator.findAccumulatorByBlock(
+      startBlock + index,
+    );
 
     expect(accumulator).to.equal(accumulators[index]);
-    expect(count.toNumber()).to.equal(index + 1);
   });
 
-  it('Should find correct accumulator in non-consecutive blocks and prune all before it', async function () {
+  it('Should find correct accumulator in non-consecutive blocks', async function () {
     await deposit(depositItems[0]);
     await network.provider.send('hardhat_mine', [ethers.utils.hexValue(2)]);
     const startBlock0 = Number(await network.provider.send('eth_blockNumber'));
@@ -112,8 +118,7 @@ describe('ValidatorsAccumulator tests', async function () {
     ]);
 
     for (let i = 0; i < res.length; i++) {
-      expect(res[i][0].toNumber()).to.equal(i + 1);
-      expect(res[i][1]).to.equal(accumulators[i]);
+      expect(res[i]).to.equal(accumulators[i]);
     }
   });
 
@@ -121,24 +126,105 @@ describe('ValidatorsAccumulator tests', async function () {
     await deposit(depositItems[0]);
     const startBlock = Number(await network.provider.send('eth_blockNumber'));
 
-    const [count, accumulator] =
-      await validatorAccumulator.findAccumulatorByBlock(startBlock - 1);
-
-    expect(accumulator).to.equal(
-      '0x985e929f70af28d0bdd1a90a808f977f597c7c778c489e98d3bd8910d31ac0f7',
+    const accumulator = await validatorAccumulator.findAccumulatorByBlock(
+      startBlock - 1,
     );
-    expect(count.toNumber()).to.equal(0);
+
+    expect(accumulator).to.equal(zeroHash);
   });
 
   it('Should return zero hash if no validators have deposited', async function () {
     const startBlock = Number(await network.provider.send('eth_blockNumber'));
 
-    const [count, accumulator] =
-      await validatorAccumulator.findAccumulatorByBlock(startBlock);
+    const accumulator = await validatorAccumulator.findAccumulatorByBlock(
+      startBlock,
+    );
+
+    expect(accumulator).to.equal(zeroHash);
+  });
+
+  it('Should return latest value for block number in the middle', async function () {
+    await deposit(depositItems[4]);
+
+    const startBlock = Number(await network.provider.send('eth_blockNumber'));
+    await network.provider.send('evm_setAutomine', [false]);
+
+    await depositTransaction(depositItems[0]);
+    await depositTransaction(depositItems[1]);
+    await depositTransaction(depositItems[2]);
+
+    await network.provider.send('evm_mine');
+    await network.provider.send('evm_setAutomine', [true]);
+
+    await deposit(depositItems[6]);
+
+    const accumulator = await validatorAccumulator.findAccumulatorByBlock(
+      startBlock + 1,
+    );
 
     expect(accumulator).to.equal(
-      '0x985e929f70af28d0bdd1a90a808f977f597c7c778c489e98d3bd8910d31ac0f7',
+      '0xd03564681d03f73e2fa4e5092dcb54fe0d0adb996613fff319183da3b01d3b2d',
     );
-    expect(count.toNumber()).to.equal(0);
+  });
+
+  it('Should return latest value for block number in the beginning', async function () {
+    const startBlock = Number(await network.provider.send('eth_blockNumber'));
+    await network.provider.send('evm_setAutomine', [false]);
+
+    await depositTransaction(depositItems[0]);
+    await depositTransaction(depositItems[1]);
+    await depositTransaction(depositItems[2]);
+
+    await network.provider.send('evm_mine');
+    await network.provider.send('evm_setAutomine', [true]);
+
+    for (const depositItem of depositItems.slice(7)) {
+      await deposit(depositItem);
+    }
+
+    const accumulator = await validatorAccumulator.findAccumulatorByBlock(
+      startBlock + 1,
+    );
+
+    expect(accumulator).to.equal(
+      '0x1e1f85c01394547df1c4fc661839a8fbaf63a9bb0c1d819785f549d9f81eeca0',
+    );
+  });
+
+  it('Should return latest value for block number at the end', async function () {
+    for (const depositItem of depositItems.slice(7)) {
+      await deposit(depositItem);
+    }
+
+    const startBlock = Number(await network.provider.send('eth_blockNumber'));
+    await network.provider.send('evm_setAutomine', [false]);
+
+    await depositTransaction(depositItems[0]);
+    await depositTransaction(depositItems[2]);
+    await depositTransaction(depositItems[3]);
+
+    await network.provider.send('hardhat_mine', [ethers.utils.hexValue(1)]);
+    await network.provider.send('evm_setAutomine', [true]);
+
+    const accumulator = await validatorAccumulator.findAccumulatorByBlock(
+      startBlock + 1,
+    );
+
+    expect(accumulator).to.equal(
+      '0x88d29f354f12c62a466e92352203fbb6cd5b468d46701d490037a16e5125ea26',
+    );
+  });
+
+  it('Should return latest accumulator if blockNumber is after last deposit', async function () {
+    for (const depositItem of depositItems) {
+      await deposit(depositItem);
+    }
+    const startBlock = Number(await network.provider.send('eth_blockNumber'));
+
+    const accumulator = await validatorAccumulator.findAccumulatorByBlock(
+      startBlock + 1,
+    );
+
+    expect(accumulator).to.equal(accumulators[accumulators.length - 1]);
   });
 });
