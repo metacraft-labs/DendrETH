68874e704afe44a011817dcf33204711e5753b2c
Petar Kirov <petar.p.kirov@gmail.com>
2024-06-19 12:30:34 +0300

    build(nix/circuits_executables): Update package

--- libs/nix/circuits_executables/default.nix
+++ libs/nix/circuits_executables/default.nix
@@ -6,15 +6,14 @@
   openssl,
   ...
 }: let
-  src-path = ../../../beacon-light-client/plonky2;
+  src-path = ../../../beacon-light-client/plonky2/crates;
 
   sharedAttrs = {
     version = "0.1.0";
     nativeBuildInputs = [pkg-config];
+    pname = "circuit-executables";
     buildInputs = [openssl];
     src = src-path;
-    cargoLock = "${src-path}/circuit_executables/Cargo.lock";
-    cargoToml = "${src-path}/circuit_executables/Cargo.toml";
     postUnpack = ''
       cd $sourceRoot/circuit_executables
       sourceRoot="."
