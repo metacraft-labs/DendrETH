101817220441b5664e41d0a699ed476b974c3a6f
Petar Kirov <petar.p.kirov@gmail.com>
2024-06-19 13:34:12 +0300

    build(nix/circuits_executables): Copy the the eth2-test files in the preCheck

--- libs/nix/circuits_executables/default.nix
+++ libs/nix/circuits_executables/default.nix
@@ -6,7 +6,14 @@
   openssl,
   ...
 }: let
-  src-path = ../../../beacon-light-client/plonky2/crates;
+  eth2-tests = fetchFromGitHub {
+    owner = "ethereum";
+    repo = "eth2.0-tests";
+    rev = "b01e476e90898e0092a848455797cee4e1609eff";
+    hash = "sha256-m+4w2wEXn4YZPs9YwrvdW5Lmd6N24ol5+zyJuBXLPKs=";
+  };
+
+  src-path = ../../../beacon-light-client/plonky2;
 
   sharedAttrs = {
     version = "0.1.0";
@@ -14,9 +21,17 @@
     pname = "circuit-executables";
     buildInputs = [openssl];
     src = src-path;
+    cargoLock = "${src-path}/crates/Cargo.lock";
+    cargoToml = "${src-path}/crates/Cargo.toml";
     postUnpack = ''
-      cd $sourceRoot/circuit_executables
+      cd $sourceRoot/crates
       sourceRoot="."
+    '';
+    preCheck = ''
+      pwd
+      ls -la
+      cp -r ${eth2-tests} $sourceRoot/
+      sed -i 's|../../../../vendor/eth2.0-tests|eth2.0-tests|' "beacon-light-client/plonky2/crates/circuits/src/utils/circuit/mod.rs"
     '';
   };
 
