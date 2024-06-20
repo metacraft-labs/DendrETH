d46f1135613957ad6e0e0ceaadbcdc500f75e75b
Kristin Kirkov <krischanss99@gmail.com>
2024-06-20 16:02:48 +0300

    refactor(relay): Move bash functions to libs

--- libs/bash/common-utils/common.sh
+++ libs/bash/common-utils/common.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+ZKEY_B3SUM_SUM='7bd1baf6e4aa1bb97933df06f68b26f8aa034e6743ff52c4dd7f6097d6e7d104'
+DAT_B3SUM_SUM='c94eb86af7c0451a4277a7bdfc90232a9db75c192d6852ad18baa9a46e1e52e5'
+
 look_for_ptau_file() {
   local phase1_file="$1"
 
@@ -17,5 +20,70 @@ create_build_folder() {
   if [ ! -d "${build_dir}" ]; then
     echo "No build directory found. Creating build directory..."
     mkdir -p "${build_dir}"
+  fi
+}
+
+calculate_checksum() {
+  local FILE_PATH=$1
+  b3sum "${FILE_PATH}" | cut -d ' ' -f 1
+}
+
+download_zkey_file() {
+  echo "Downloading zkey file from http://dendreth.metacraft-labs.com/deneb_284.zkey ..."
+
+  curl http://dendreth.metacraft-labs.com/deneb_284.zkey >"${GIT_ROOT}/data/light_client.zkey"
+
+  CALCULATED_ZKEY_SUM=$(calculate_checksum "${GIT_ROOT}/data/light_client.zkey")
+
+  if [ "${ZKEY_B3SUM_SUM}" = "${CALCULATED_ZKEY_SUM}" ]; then
+    echo "Zkey file downloaded successfully to ${GIT_ROOT}/data/light_client.zkey"
+  else
+    echo "Failed to download zkey file from http://dendreth.metacraft-labs.com/deneb_284.zkey"
+    exit 1
+  fi
+}
+
+download_dat_file() {
+  echo "Downloading .dat file from https://media.githubusercontent.com/media/metacraft-labs/DendrETH-build-artifacts/master/light_client_cpp/light_client.dat ..."
+
+  curl -k https://media.githubusercontent.com/media/metacraft-labs/DendrETH-build-artifacts/master/light_client_cpp/light_client.dat >"data/light_client.dat"
+
+  CALCULATED_DAT_SUM=$(calculate_checksum "${GIT_ROOT}/data/light_client.dat")
+
+  if [ "${DAT_B3SUM_SUM}" = "${CALCULATED_DAT_SUM}" ]; then
+    echo ".dat file downloaded successfully to ${GIT_ROOT}/data/light_client.dat"
+  else
+    echo "Failed to download .dat file from https://media.githubusercontent.com/media/metacraft-labs/DendrETH-build-artifacts/master/light_client_cpp/light_client.dat"
+    exit 1
+  fi
+}
+
+look_for_light_client_zkey_file() {
+  if [ ! -f "${GIT_ROOT}/data/light_client.zkey" ]; then
+    download_zkey_file
+  else
+    CALCULATED_ZKEY_SUM=$(calculate_checksum "${GIT_ROOT}/data/light_client.zkey")
+    echo "${CALCULATED_ZKEY_SUM}"
+    if [ "${ZKEY_B3SUM_SUM}" = "${CALCULATED_ZKEY_SUM}" ]; then
+      echo "Using cached zkey file at data/light_client.zkey"
+    else
+      echo "Wrong version of light_client.zkey cached downloading again..."
+      download_zkey_file
+    fi
+  fi
+}
+
+look_for_light_client_dat_file() {
+  if [ ! -f "${GIT_ROOT}/data/light_client.dat" ]; then
+    download_dat_file
+  else
+    CALCULATED_DAT_SUM=$(calculate_checksum "${GIT_ROOT}/data/light_client.dat")
+    echo "${CALCULATED_DAT_SUM}"
+    if [ "${DAT_B3SUM_SUM}" = "${CALCULATED_DAT_SUM}" ]; then
+      echo "Using cached .dat file at data/light_client.dat"
+    else
+      echo "Wrong version of light_client.dat cached downloading again..."
+      download_dat_file
+    fi
   fi
 }
--- relay/run-relay.sh
+++ relay/run-relay.sh
@@ -1,75 +1,9 @@
 #!/usr/bin/env bash
 
-ZKEY_B3SUM_SUM='7bd1baf6e4aa1bb97933df06f68b26f8aa034e6743ff52c4dd7f6097d6e7d104'
-DAT_B3SUM_SUM='c94eb86af7c0451a4277a7bdfc90232a9db75c192d6852ad18baa9a46e1e52e5'
-source .env
+source "${GIT_ROOT}/.env"
+source "${GIT_ROOT}/libs/bash/common-utils/common.sh"
 
 (cd "${GIT_ROOT} beacon-light-client/solidity/" && yarn hardhat compile)
-
-calculate_checksum() {
-  local FILE_PATH=$1
-  b3sum "${FILE_PATH}" | cut -d ' ' -f 1
-}
-
-download_zkey_file() {
-  echo "Downloading zkey file from http://dendreth.metacraft-labs.com/deneb_284.zkey ..."
-
-  curl http://dendreth.metacraft-labs.com/deneb_284.zkey >"data/light_client.zkey"
-
-  CALCULATED_ZKEY_SUM=$(calculate_checksum data/light_client.zkey)
-
-  if [ "${ZKEY_B3SUM_SUM}" = "${CALCULATED_ZKEY_SUM}" ]; then
-    echo "Zkey file downloaded successfully to data/light_client.zkey"
-  else
-    echo "Failed to download zkey file from http://dendreth.metacraft-labs.com/deneb_284.zkey"
-    exit 1
-  fi
-}
-
-download_dat_file() {
-  echo "Downloading .dat file from https://media.githubusercontent.com/media/metacraft-labs/DendrETH-build-artifacts/master/light_client_cpp/light_client.dat ..."
-
-  curl -k https://media.githubusercontent.com/media/metacraft-labs/DendrETH-build-artifacts/master/light_client_cpp/light_client.dat >"data/light_client.dat"
-
-  CALCULATED_DAT_SUM=$(calculate_checksum data/light_client.dat)
-
-  if [ "${DAT_B3SUM_SUM}" = "${CALCULATED_DAT_SUM}" ]; then
-    echo ".dat file downloaded successfully to data/light_client.dat"
-  else
-    echo "Failed to download .dat file from https://media.githubusercontent.com/media/metacraft-labs/DendrETH-build-artifacts/master/light_client_cpp/light_client.dat"
-    exit 1
-  fi
-}
-
-look_for_light_client_zkey_file() {
-  if [ ! -f "${GIT_ROOT}/data/light_client.zkey" ]; then
-    download_zkey_file
-  else
-    CALCULATED_ZKEY_SUM=$(calculate_checksum "${GIT_ROOT}/data/light_client.zkey")
-    echo "${CALCULATED_ZKEY_SUM}"
-    if [ "${ZKEY_B3SUM_SUM}" = "${CALCULATED_ZKEY_SUM}" ]; then
-      echo "Using cached zkey file at data/light_client.zkey"
-    else
-      echo "Wrong version of light_client.zkey cached downloading again..."
-      download_zkey_file
-    fi
-  fi
-}
-
-look_for_light_client_dat_file() {
-  if [ ! -f "${GIT_ROOT}/data/light_client.dat" ]; then
-    download_dat_file
-  else
-    CALCULATED_DAT_SUM=$(calculate_checksum "${GIT_ROOT}/data/light_client.dat")
-    echo "${CALCULATED_DAT_SUM}"
-    if [ "${DAT_B3SUM_SUM}" = "${CALCULATED_DAT_SUM}" ]; then
-      echo "Using cached .dat file at data/light_client.dat"
-    else
-      echo "Wrong version of light_client.dat cached downloading again..."
-      download_dat_file
-    fi
-  fi
-}
 
 look_for_light_client_zkey_file
 look_for_light_client_dat_file
