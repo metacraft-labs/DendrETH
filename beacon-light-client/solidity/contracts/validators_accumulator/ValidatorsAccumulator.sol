b8ed65e27c6d7a5655b360bcb37f4aa8c8d4ff34
Martin Ivanov <xeartybg@gmail.com>
2024-06-24 12:13:53 +0300

    refactor(): Delete unused program constructs

--- beacon-light-client/solidity/contracts/validators_accumulator/ValidatorsAccumulator.sol
+++ beacon-light-client/solidity/contracts/validators_accumulator/ValidatorsAccumulator.sol
@@ -152,18 +152,4 @@ contract ValidatorsAccumulator is IValidatorsAccumulator {
 
     return blockNumbers[lower];
   }
-
-  function toLe64(uint64 value) internal pure returns (bytes memory ret) {
-    ret = new bytes(8);
-    bytes8 bytesValue = bytes8(value);
-    // Byteswapping during copying to bytes.
-    ret[0] = bytesValue[7];
-    ret[1] = bytesValue[6];
-    ret[2] = bytesValue[5];
-    ret[3] = bytesValue[4];
-    ret[4] = bytesValue[3];
-    ret[5] = bytesValue[2];
-    ret[6] = bytesValue[1];
-    ret[7] = bytesValue[0];
-  }
 }
