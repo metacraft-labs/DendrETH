d493b8ca174fae30ac3ac8de37ecd83908f39d75
Martin Nikov <mnikov12@gmail.com>
2024-06-29 22:20:48 +0300

    fix(nix): circuits_executables

--- libs/nix/circuits_executables/default.nix
+++ libs/nix/circuits_executables/default.nix
@@ -6,16 +6,19 @@
   openssl,
   ...
 }: let
-  src-path = ../../../beacon-light-client/plonky2/crates;
+  src-path = ../../..;
 
   sharedAttrs = {
     version = "0.1.0";
     nativeBuildInputs = [pkg-config];
     pname = "circuit-executables";
+    cargoLock = "${src-path}/beacon-light-client/plonky2/crates/Cargo.lock";
+    cargoToml = "${src-path}/beacon-light-client/plonky2/crates/Cargo.toml";
     buildInputs = [openssl];
     src = src-path;
+    doChecks = false;
     postUnpack = ''
-      cd $sourceRoot/circuit_executables
+      cd $sourceRoot/beacon-light-client/plonky2/crates
       sourceRoot="."
     '';
   };
