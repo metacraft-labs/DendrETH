e36ae3fa4ac55b97af8e94bd3cc402765dbb5489
Simeon Armenchev <monyarm@gmail.com>
2024-06-30 04:03:56 +0300

    fix(circuits_executables): change src, to reduce unnecesary rebuilds

--- libs/nix/circuits_executables/default.nix
+++ libs/nix/circuits_executables/default.nix
@@ -6,21 +6,15 @@
   openssl,
   ...
 }: let
-  src-path = ../../..;
-
-  sharedAttrs = {
+  kv_db_constants = ../../../beacon-light-client/plonky2/kv_db_constants.json;
+  sharedAttrs = rec {
     version = "0.1.0";
     nativeBuildInputs = [pkg-config];
     pname = "circuit-executables";
-    cargoLock = "${src-path}/beacon-light-client/plonky2/crates/Cargo.lock";
-    cargoToml = "${src-path}/beacon-light-client/plonky2/crates/Cargo.toml";
+    cargoLock = "${src}/Cargo.lock";
+    cargoToml = "${src}/Cargo.toml";
     buildInputs = [openssl];
-    src = src-path;
-    doChecks = false;
-    postUnpack = ''
-      cd $sourceRoot/beacon-light-client/plonky2/crates
-      sourceRoot="."
-    '';
+    src = ../../../beacon-light-client/plonky2/crates;
   };
 
   brokenTests = lib.concatStringsSep " --skip " ["test_ssz_num_from_bits"];
@@ -33,5 +27,8 @@ in
     // {
       inherit cargoArtifacts;
       cargoTestExtraArgs = "-- --skip ${brokenTests} --skip ${slowTests}";
+      postUnpack = ''
+        sed -i 's|../../../kv_db_constants.json|${kv_db_constants}|g' crates/circuit_executables/src/db_constants.rs
+      '';
     }
   )
