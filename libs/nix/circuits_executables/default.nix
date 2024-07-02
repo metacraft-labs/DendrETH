104e6e84b9919af311d11b2a5079366237b340c8
Petar Kirov <petar.p.kirov@gmail.com>
2024-07-02 19:02:32 +0300

    style(nix/circuits_executables): Minor formatting

--- libs/nix/circuits_executables/default.nix
+++ libs/nix/circuits_executables/default.nix
@@ -1,24 +1,32 @@
 {
   lib,
   craneLib,
-  fetchFromGitHub,
   pkg-config,
   openssl,
   ...
 }: let
   kv_db_constants = ../../../beacon-light-client/plonky2/kv_db_constants.json;
+
   sharedAttrs = rec {
+    src = ../../../beacon-light-client/plonky2/crates;
     version = "0.1.0";
     nativeBuildInputs = [pkg-config];
     pname = "circuit-executables";
     cargoLock = "${src}/Cargo.lock";
     cargoToml = "${src}/Cargo.toml";
     buildInputs = [openssl];
-    src = ../../../beacon-light-client/plonky2/crates;
   };
 
   brokenTests = lib.concatStringsSep " --skip " ["test_ssz_num_from_bits"];
-  slowTests = lib.concatStringsSep " --skip " ["test_deposit_accumulator_diva_leaf_circuit_different_pubkeys" "test_deposit_accumulator_diva_leaf_circuit_is_dummy" "test_deposit_accumulator_diva_leaf_circuit_valid" "test_deposit_accumulator_diva_leaf_circuit_wrong_balances_root" "test_deposit_accumulator_diva_leaf_circuit_wrong_commitment_mapper_branch" "test_deposit_accumulator_diva_leaf_circuit_wrong_validator" "test_deposit_accumulator_diva_leaf_circuit_wrong_validator_gindex"];
+  slowTests = lib.concatStringsSep " --skip " [
+    "test_deposit_accumulator_diva_leaf_circuit_different_pubkeys"
+    "test_deposit_accumulator_diva_leaf_circuit_is_dummy"
+    "test_deposit_accumulator_diva_leaf_circuit_valid"
+    "test_deposit_accumulator_diva_leaf_circuit_wrong_balances_root"
+    "test_deposit_accumulator_diva_leaf_circuit_wrong_commitment_mapper_branch"
+    "test_deposit_accumulator_diva_leaf_circuit_wrong_validator"
+    "test_deposit_accumulator_diva_leaf_circuit_wrong_validator_gindex"
+  ];
 
   cargoArtifacts = craneLib.buildDepsOnly sharedAttrs;
 in
