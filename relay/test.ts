d93dba0f2c7bab3735200308bb3550babe1371c5
Kristin Kirkov <kristin.kirkov@metacraft-labs.com>
2024-07-04 17:46:31 +0300

    WIP finish getSlotUpdate and add getRotateUpdate

--- relay/test.ts
+++ relay/test.ts
@@ -8,6 +8,7 @@ import { hexToBits } from '@dendreth/utils/ts-utils/hex-utils';
 import { BitVectorType } from '@chainsafe/ssz';
 import { sha256 } from 'ethers/lib/utils';
 import { DenebClient } from 'telepathyx/src/operatorx/deneb';
+const fs = require('fs');
 
 (async () => {
   const { DenebClient } = await import('telepathyx/src/operatorx/deneb');
@@ -21,37 +22,38 @@ import { DenebClient } from 'telepathyx/src/operatorx/deneb';
   );
 
   await getStepUpdate(denebClient, ssz, 5239744);
+  await getRotateUpdate(denebClient, ssz, 5239744);
 
-  let rotate = await denebClient.getRotateUpdate(5239744);
-  let finalizedBlock = await denebClient.getBlock(5239744);
+  // let rotate = await denebClient.getRotateUpdate(5239744);
+  // let finalizedBlock = await denebClient.getBlock(5239744);
 
-  let pubkeysBytes = rotate.nextSyncCommittee.pubkeys;
-  let aggregatePubkeyBytesX = rotate.nextSyncCommittee.aggregatePubkey;
-  let pubkeysBigIntX = rotate.nextSyncCommittee.pubkeys
-    .map(x => PointG1.fromHex(x))
-    .map(x => bigint_to_array(55, 7, x.toAffine()[0].value));
-  let pubkeysBigIntY = rotate.nextSyncCommittee.pubkeys
-    .map(x => PointG1.fromHex(x))
-    .map(x => bigint_to_array(55, 7, x.toAffine()[1].value));
-  let syncCommitteeSSZ =
-    ssz.deneb.BeaconState.fields.nextSyncCommittee.hashTreeRoot(
-      rotate.nextSyncCommittee,
-    );
+  // let pubkeysBytes = rotate.nextSyncCommittee.pubkeys;
+  // let aggregatePubkeyBytesX = rotate.nextSyncCommittee.aggregatePubkey;
+  // let pubkeysBigIntX = rotate.nextSyncCommittee.pubkeys
+  //   .map(x => PointG1.fromHex(x))
+  //   .map(x => bigint_to_array(55, 7, x.toAffine()[0].value));
+  // let pubkeysBigIntY = rotate.nextSyncCommittee.pubkeys
+  //   .map(x => PointG1.fromHex(x))
+  //   .map(x => bigint_to_array(55, 7, x.toAffine()[1].value));
+  // let syncCommitteeSSZ =
+  //   ssz.deneb.BeaconState.fields.nextSyncCommittee.hashTreeRoot(
+  //     rotate.nextSyncCommittee,
+  //   );
 
-  let syncCommitteeBranch = rotate.nextSyncCommitteeBranch;
+  // let syncCommitteeBranch = rotate.nextSyncCommitteeBranch;
 
-  let syncCommitteePoseidon = ''; // TODO: find some way
+  // let syncCommitteePoseidon = ''; // TODO: find some way
 
-  let finalizedHeaderRoot = ssz.deneb.BeaconBlock.hashTreeRoot(finalizedBlock);
-  let finalizedSlot = ssz.deneb.BeaconBlock.fields.slot.hashTreeRoot(
-    finalizedBlock.slot,
-  );
-  let finalizedProposerIndex = finalizedBlock.proposerIndex;
-  let finalizedParentRoot = finalizedBlock.parentRoot;
-  let finalizedStateRoot = finalizedBlock.stateRoot;
-  let finalizedBodyRoot = ssz.deneb.BeaconBlockBody.hashTreeRoot(
-    finalizedBlock.body as any,
-  );
+  // let finalizedHeaderRoot = ssz.deneb.BeaconBlock.hashTreeRoot(finalizedBlock);
+  // let finalizedSlot = ssz.deneb.BeaconBlock.fields.slot.hashTreeRoot(
+  //   finalizedBlock.slot,
+  // );
+  // let finalizedProposerIndex = finalizedBlock.proposerIndex;
+  // let finalizedParentRoot = finalizedBlock.parentRoot;
+  // let finalizedStateRoot = finalizedBlock.stateRoot;
+  // let finalizedBodyRoot = ssz.deneb.BeaconBlockBody.hashTreeRoot(
+  //   finalizedBlock.body as any,
+  // );
 })();
 async function getStepUpdate(
   denebClient: DenebClient,
@@ -104,6 +106,7 @@ async function getStepUpdate(
 
   let syncCommitteePoseidon = ''; // TODO: read from contract
 
+  // Compute the necessary values in the correct format
   let finalityBranch = step.finalityBranch;
   let executionStateRoot = step.executionStateRoot;
   let executionStateBranch = step.executionStateBranch;
@@ -125,4 +128,113 @@ async function getStepUpdate(
   let finalizedBodyRoot = ssz.deneb.BeaconBlockBody.hashTreeRoot(
     step.finalizedBlock.body as any,
   );
+
+  // Create the JSON object in the specified order
+  const jsonOutput = {
+    /* Attested Header */
+    attestedHeaderRoot,
+    attestedSlot,
+    attestedProposerIndex,
+    attestedParentRoot,
+    attestedStateRoot,
+    attestedBodyRoot,
+
+    /* Finalized Header */
+    finalizedHeaderRoot,
+    finalizedSlot,
+    finalizedProposerIndex,
+    finalizedParentRoot,
+    finalizedStateRoot,
+    finalizedBodyRoot,
+
+    /* Sync Committee Protocol */
+    pubkeysX,
+    pubkeysY,
+    aggregationBits,
+    signature,
+    domain,
+    signingRoot: signing_root,
+    participation,
+    syncCommitteePoseidon,
+
+    /* Finality Proof */
+    finalityBranch,
+
+    /* Execution State Proof */
+    executionStateRoot,
+    executionStateBranch,
+
+    /* Commitment to Public Inputs */
+    publicInputsRoot: '', // TODO
+  };
+  // Write the JSON output to a file
+  const outputFilePath = 'InputForStepUpdate.json';
+  await fs.writeFile(
+    outputFilePath,
+    JSON.stringify(jsonOutput, null, 2),
+    'utf-8',
+  );
+
+  console.log(`JSON file has been written to ${outputFilePath}`);
+}
+
+async function getRotateUpdate(
+  denebClient: DenebClient,
+  ssz: typeof import('@lodestar/types').ssz,
+  slot: number,
+) {
+  let rotate = await denebClient.getRotateUpdate(slot);
+  let finalizedBlock = await denebClient.getBlock(slot);
+
+  let pubkeysBytes = rotate.nextSyncCommittee.pubkeys;
+  let aggregatePubkeyBytesX = rotate.nextSyncCommittee.aggregatePubkey;
+  let pubkeysBigIntX = rotate.nextSyncCommittee.pubkeys
+    .map(x => PointG1.fromHex(x))
+    .map(x => bigint_to_array(55, 7, x.toAffine()[0].value));
+  let pubkeysBigIntY = rotate.nextSyncCommittee.pubkeys
+    .map(x => PointG1.fromHex(x))
+    .map(x => bigint_to_array(55, 7, x.toAffine()[1].value));
+  let syncCommitteeSSZ =
+    ssz.deneb.BeaconState.fields.nextSyncCommittee.hashTreeRoot(
+      rotate.nextSyncCommittee,
+    );
+
+  let syncCommitteeBranch = rotate.nextSyncCommitteeBranch;
+
+  let syncCommitteePoseidon = ''; // TODO: find some way
+
+  let finalizedHeaderRoot = ssz.deneb.BeaconBlock.hashTreeRoot(finalizedBlock);
+  let finalizedSlot = ssz.deneb.BeaconBlock.fields.slot.hashTreeRoot(
+    finalizedBlock.slot,
+  );
+  let finalizedProposerIndex = finalizedBlock.proposerIndex;
+  let finalizedParentRoot = finalizedBlock.parentRoot;
+  let finalizedStateRoot = finalizedBlock.stateRoot;
+  let finalizedBodyRoot = ssz.deneb.BeaconBlockBody.hashTreeRoot(
+    finalizedBlock.body as any,
+  );
+  const jsonOutput = {
+    pubkeysBytes,
+    aggregatePubkeyBytesX,
+    pubkeysBigIntX,
+    pubkeysBigIntY,
+    syncCommitteeSSZ,
+    syncCommitteeBranch,
+    syncCommitteePoseidon,
+    finalizedHeaderRoot,
+    finalizedSlot,
+    finalizedProposerIndex,
+    finalizedParentRoot,
+    finalizedStateRoot,
+    finalizedBodyRoot,
+  };
+  // Write the JSON output to a file
+  const outputFilePath = 'InputForRotateUpdate.json';
+  await fs.writeFile(
+    outputFilePath,
+    JSON.stringify(jsonOutput, null, 2),
+    'utf-8',
+  );
+
+  console.log(`JSON file has been written to ${outputFilePath}`);
 }
