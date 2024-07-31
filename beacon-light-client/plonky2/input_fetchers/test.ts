238c00e98b09414c42df749d8037836656c19127
Martin Ivanov <xeartybg@gmail.com>
2024-07-31 23:53:25 +0300

    Delete proof caching

--- beacon-light-client/plonky2/input_fetchers/test.ts
+++ beacon-light-client/plonky2/input_fetchers/test.ts
@@ -1,35 +1,33 @@
 import { getBeaconApi } from '@dendreth/relay/implementations/beacon-api';
 import common_config from '../common_config.json';
 import { Tree } from '@chainsafe/persistent-merkle-tree/lib/tree';
-import { bytesToHex, hexToBytes } from '@dendreth/utils/ts-utils/bls';
-import { sha256 } from 'ethers/lib/utils';
+import { bytesToHex } from '@dendreth/utils/ts-utils/bls';
+import { CommandLineOptionsBuilder } from './utils/cmdline';
 
-(async function () {
+(async function() {
+  const args = new CommandLineOptionsBuilder()
+    .option('slot', {
+      type: 'number',
+      demandOption: true,
+    })
+    .build();
+
   const { ssz } = await import('@lodestar/types');
   const api = await getBeaconApi(common_config['beacon-node']);
 
-  const { beaconState } = (await api.getBeaconState(9111936n))!;
-  console.log('pubkey', bytesToHex(beaconState.validators[0].pubkey));
+  const { beaconState } = (await api.getBeaconState(args['slot']))!;
+  const validators = beaconState.validators;
 
-  beaconState.validators = beaconState.validators.slice(0, 19);
+  const validatorsViewDU = ssz.deneb.BeaconState.fields.validators.toViewDU(validators);
 
-  const validatorsViewDU = ssz.deneb.BeaconState.fields.validators.toViewDU(
-    beaconState.validators,
-  );
-
-  const newValidatorsTree = new Tree(validatorsViewDU.node.left);
+  const tree = new Tree(validatorsViewDU.node);
   console.log(
     'validators root',
-    bytesToHex(newValidatorsTree.getNode(1n).root),
+    bytesToHex(tree.getRoot(1n)),
   );
-
-  const validator = beaconState.validators[0];
-  const hashTreeRoot = bytesToHex(ssz.phase0.Validator.hashTreeRoot(validator));
-  console.log('hash_tree_root', hashTreeRoot);
-
-  const hashTreeRoot2 = bytesToHex(
-    ssz.phase0.Validator.hashTreeRoot(validator),
+  console.log(
+    'proof root',
+    bytesToHex(tree.getRoot(2n)),
   );
-  const inner = sha256(hexToBytes(hashTreeRoot + hashTreeRoot2));
-  console.log('inner:', inner);
+  console.log('validators length', validators.length)
 })();
