36fce93817aefb9b27d1098de4dfe81bc2b03311
Petar Kirov <petar.p.kirov@gmail.com>
2024-09-04 12:16:55 +0300

    build(nix/flake/outputs): Expose `input-fetchers` pkg

--- nix/pkgs/default.nix
+++ nix/pkgs/default.nix
@@ -9,6 +9,7 @@
   in {
     packages = {
       light-client = callPackage ./light-client {};
+      input-fetchers = callPackage ./input-fetchers {};
       circuit-executables = callPackage ../pkgs/circuits_executables {inherit craneLib;};
       gnark-plonky2-verifier = callPackage ./gnark-plonky2-verifier {};
     };
