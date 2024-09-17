7e0b794754bd1517b0559966ce7c1df07061103f
Petar Kirov <petar.p.kirov@gmail.com>
2024-09-17 21:13:04 +0300

    wip

--- nix/pkgs/input-fetchers/default.nix
+++ nix/pkgs/input-fetchers/default.nix
@@ -6,29 +6,36 @@
   callPackage,
   ...
 }: let
-  overrideDep = old: {
-    buildInputs = old.buildInputs ++ [python3 sqlite];
-  };
+  root = ../../..;
+  yarnPlugins = root + /.yarn/plugins;
+  plonky2 = root + /beacon-light-client/plonky2;
 
-  project =
-    callPackage ./yarn-project.generated.nix {
-      inherit nodejs;
-    } {
-      src = lib.cleanSource ../../..;
-      overrideBcryptoAttrs = overrideDep;
-      overrideBufferutilAttrs = overrideDep;
-      overrideClassicLevelAttrs = overrideDep;
-      overrideMsgpackrExtractAttrs = overrideDep;
-      overrideUtf8ValidateAttrs = overrideDep;
-      overrideLeveldownAttrs = overrideDep;
-      overrideBcryptAttrs = overrideDep;
-      overrideGetBalancesInputAttrs = overrideDep;
+  yarnFilenames = [
+    "yarn.lock"
+    "package.json"
+    ".yarnrc.yml"
+    "tsconfig.json"
+  ];
+
+  npmDepsSrc = with lib.fileset;
+    toSource {
+      inherit root;
+      fileset = unions [
+        (fileFilter (file: (builtins.elem file.name yarnFilenames) || file.hasExt "ts") root)
+        yarnPlugins
+        (fileFilter (file: file.hasExt "json") plonky2)
+      ];
     };
+
+  yarnProject = callPackage ./yarn-project.generated.nix {inherit nodejs;} {
+    src = npmDepsSrc;
+  };
+  inherit (yarnProject) cacheDrv project;
 in
   project.overrideAttrs (oldAttrs: {
     name = "input-fetchers";
     buildInputs = oldAttrs.buildInputs ++ [python3 sqlite];
-    buildPhase = ''
-      yarn workspace @dendreth/balance-verification build:tsc
-    '';
+    # buildPhase = ''
+    #   yarn workspace @dendreth/balance-verification build:tsc
+    # '';
   })
