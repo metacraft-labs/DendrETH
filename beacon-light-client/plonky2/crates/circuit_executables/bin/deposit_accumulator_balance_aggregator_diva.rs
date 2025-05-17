d7c946e37d61dcf7a6e4ecc481dad37a8b97758a
Martin Ivanov <xeartybg@gmail.com>
2025-05-18 01:17:27 +0300

    wip rust

--- beacon-light-client/plonky2/crates/circuit_executables/bin/deposit_accumulator_balance_aggregator_diva.rs
+++ beacon-light-client/plonky2/crates/circuit_executables/bin/deposit_accumulator_balance_aggregator_diva.rs
@@ -1,15 +1,14 @@
 #![allow(incomplete_features)]
 #![feature(generic_const_exprs)]
 
-use circuit::{set_witness::SetWitness, Circuit, CircuitTargetType};
+use circuit::{set_witness::SetWitness, Circuit, CircuitDataType, CircuitTargetType};
 use circuit_executables::{
     cached_circuit_build::build_recursive_circuit_single_level_cached,
     constants::VALIDATOR_REGISTRY_LIMIT,
     crud::{
         common::{
             delete_balance_verification_diva_proof_dependencies, fetch_proofs_balances,
-            fetch_validator_balance_aggregator_input, load_circuit_data,
-            save_balance_aggregator_proof,
+            fetch_validator_balance_aggregator_input, save_balance_aggregator_proof,
         },
         proof_storage::{get_storage_key_from_matches, MetadataBlobStorage, ProofStorage},
     },
@@ -27,11 +26,11 @@ use circuits::{
 };
 use colored::Colorize;
 use std::{
-    println, thread,
+    thread,
     time::{Duration, Instant},
 };
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 
 use plonky2::{
     field::goldilocks_field::GoldilocksField,
@@ -55,6 +54,125 @@ enum Targets {
     InnerLevel(Option<BasicRecursiveInnerCircuitTarget>),
 }
 
+//#[allow(clippy::large_enum_variant)]
+//enum CircuitState {
+//    None,
+//    FirstLevel(
+//        CircuitTargetType<DepositAccumulatorBalanceAggregatorDivaFirstLevel>,
+//        CircuitDataType<DepositAccumulatorBalanceAggregatorDivaFirstLevel>,
+//    ),
+//    InnerLevel(
+//        CircuitTargetType<DepositAccumulatorBalanceAggregatorDivaInnerLevel>,
+//        CircuitDataType<DepositAccumulatorBalanceAggregatorDivaInnerLevel>,
+//    ),
+//}
+
+struct FirstLevelCircuitState {
+    target: CircuitTargetType<DepositAccumulatorBalanceAggregatorDivaFirstLevel>,
+    data: CircuitDataType<DepositAccumulatorBalanceAggregatorDivaFirstLevel>,
+}
+
+struct InnerLevelCircuitState {
+    target: CircuitTargetType<DepositAccumulatorBalanceAggregatorDivaInnerLevel>,
+    data: CircuitDataType<DepositAccumulatorBalanceAggregatorDivaInnerLevel>,
+    verifier_data: CircuitDataType<DepositAccumulatorBalanceAggregatorDivaFirstLevel>,
+}
+
+enum CircuitState {
+    FirstLevel(FirstLevelCircuitState),
+    InnerLevel(InnerLevelCircuitState),
+}
+
+impl CircuitState {
+    pub fn into_inner_level_state(self) -> InnerLevelCircuitState {
+        match self {
+            Self::FirstLevel(_) => unreachable!(),
+            Self::InnerLevel(state) => state,
+        }
+    }
+
+    pub fn get_data_owned(
+        self,
+    ) -> CircuitDataType<DepositAccumulatorBalanceAggregatorDivaFirstLevel> {
+        match self {
+            CircuitState::FirstLevel(FirstLevelCircuitState { data, .. }) => data,
+            CircuitState::InnerLevel(InnerLevelCircuitState { data, .. }) => data,
+        }
+    }
+}
+
+#[derive(Default)]
+struct State {
+    current_level: usize,
+    circuit: Option<Box<CircuitState>>,
+    is_outdated: bool,
+}
+
+impl State {
+    pub fn new() -> Self {
+        Self {
+            current_level: 0,
+            circuit: None,
+            is_outdated: true,
+        }
+    }
+
+    fn get_circuit(&self) -> &CircuitState {
+        self.circuit.as_ref().unwrap()
+    }
+
+    pub fn load_circuit_cached(&mut self, serialized_circuits_dir: &str) {
+        if !self.is_outdated {
+            return;
+        }
+
+        if self.current_level == 0 {
+            let (target, data) = build_recursive_circuit_single_level_cached(
+                serialized_circuits_dir,
+                CIRCUIT_NAME,
+                self.current_level,
+                &|| DepositAccumulatorBalanceAggregatorDivaFirstLevel::build(&()),
+            );
+
+            self.circuit = Some(Box::new(CircuitState::FirstLevel(FirstLevelCircuitState {
+                target,
+                data,
+            })));
+        } else {
+            let previous_circuit_box = self
+                .circuit
+                .take()
+                .expect("Circuit state should exist when current_level > 0");
+
+            let child_circuit_data = previous_circuit_box.get_data_owned();
+
+            let (target, data) = build_recursive_circuit_single_level_cached(
+                serialized_circuits_dir,
+                CIRCUIT_NAME,
+                self.current_level,
+                &|| DepositAccumulatorBalanceAggregatorDivaInnerLevel::build(&child_circuit_data),
+            );
+
+            self.circuit = Some(Box::new(CircuitState::InnerLevel(InnerLevelCircuitState {
+                target,
+                data,
+                verifier_data: child_circuit_data,
+            })));
+        }
+
+        self.is_outdated = false;
+    }
+
+    pub fn transition_to_next_level(&mut self) {
+        self.current_level += 1;
+        self.is_outdated = true;
+    }
+
+    pub fn reset(&mut self) {
+        *self = Self::new();
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let matches = CommandLineOptionsBuilder::new("balance_verification")
@@ -73,54 +191,7 @@ async fn main() -> Result<()> {
     let storage_key = get_storage_key_from_matches(&matches, None)?;
     let mut storage = MetadataBlobStorage::from_file(storage_config_filepath, storage_key).await?;
 
-    println!("{}", "Loading circuit data...".yellow());
-
-    let inner_circuit_data = if config.circuit_level == 0 {
-        None
-    } else {
-        Some(
-            load_circuit_data::<DepositAccumulatorBalanceAggregatorDivaFirstLevel>(
-                serialized_circuits_dir,
-                &format!("{}_{}", CIRCUIT_NAME, &config.circuit_level - 1),
-            )
-            .unwrap(),
-        )
-    };
-
-    let (targets, circuit_data) = match inner_circuit_data {
-        Some(ref inner_circuit_data) => {
-            let circuit = build_recursive_circuit_single_level_cached(
-                serialized_circuits_dir,
-                CIRCUIT_NAME,
-                config.circuit_level as usize,
-                &|| DepositAccumulatorBalanceAggregatorDivaInnerLevel::build(inner_circuit_data),
-            );
-
-            (Targets::InnerLevel(Some(circuit.0)), circuit.1)
-        }
-        None => {
-            let circuit = build_recursive_circuit_single_level_cached(
-                serialized_circuits_dir,
-                CIRCUIT_NAME,
-                config.circuit_level as usize,
-                &|| DepositAccumulatorBalanceAggregatorDivaFirstLevel::build(&()),
-            );
-
-            (Targets::FirstLevel(Some(circuit.0)), circuit.1)
-        }
-    };
-
-    println!(
-        "{}",
-        format!("Starting worker for level {}...", config.circuit_level).yellow()
-    );
-
     let protocol = matches.value_of("protocol").unwrap();
-
-    let queue = WorkQueue::new(KeyPrefix::new(format!(
-        "{}:{}:{}",
-        protocol, DB_CONSTANTS.deposit_balance_verification_queue, config.circuit_level
-    )));
 
     println!(
         "{}",
@@ -130,23 +201,172 @@ async fn main() -> Result<()> {
         )
     );
 
-    let start: Instant = Instant::now();
-    process_queue(
-        &mut storage,
-        &queue,
-        &circuit_data,
-        &inner_circuit_data,
-        &targets,
-        config.circuit_level,
-        start,
-        config.time_to_run,
-        config.stop_after,
-        config.lease_for,
-        config.preserve_intermediary_proofs,
+    execute_tasks_by_level_ascending(&mut storage, protocol, serialized_circuits_dir, false).await;
+    Ok(())
+
+    //let start: Instant = Instant::now();
+    //process_queue(
+    //    &mut storage,
+    //    &queue,
+    //    &circuit_data,
+    //    &inner_circuit_data,
+    //    &targets,
+    //    config.circuit_level,
+    //    start,
+    //    config.time_to_run,
+    //    config.stop_after,
+    //    config.lease_for,
+    //    config.preserve_intermediary_proofs,
+    //    protocol,
+    //)
+    //.await
+}
+
+async fn execute_tasks_by_level_ascending(
+    storage: &mut MetadataBlobStorage,
+    protocol: &str,
+    serialized_circuits_dir: &str,
+    preserve_intermediary_proofs: bool,
+) {
+    let mut state = State::new();
+
+    while state.current_level < 37 {
+        let first_level_prefix = format!(
+            "{}:{}:{}",
+            protocol, DB_CONSTANTS.deposit_balance_verification_queue, state.current_level,
+        );
+
+        let work_queue = WorkQueue::new(KeyPrefix::new(first_level_prefix));
+
+        let queue_len = work_queue.queue_len(&mut storage.metadata).await.unwrap();
+
+        if queue_len > 0 {
+            // Process tasks on level `current_level`
+            state.load_circuit_cached(serialized_circuits_dir);
+
+            let Some(item) = work_queue
+                .lease(&mut storage.metadata, None, Duration::from_secs(5))
+                .await
+                .unwrap()
+            else {
+                continue;
+            };
+
+            match state.get_circuit() {
+                CircuitState::FirstLevel(first_level_state) => {
+                    handle_first_level_task(
+                        storage,
+                        &work_queue,
+                        item,
+                        protocol,
+                        &first_level_state.data,
+                        &first_level_state.target,
+                    )
+                    .await
+                }
+                CircuitState::InnerLevel(inner_level_state) => {
+                    handle_inner_level_task(
+                        storage,
+                        &work_queue,
+                        item,
+                        &inner_level_state.data,
+                        &inner_level_state.target,
+                        &inner_level_state.verifier_data,
+                        state.current_level,
+                        preserve_intermediary_proofs,
+                        protocol,
+                    )
+                    .await
+                }
+            }
+        } else {
+            // Move to the next level
+            state.transition_to_next_level();
+        }
+    }
+}
+
+async fn handle_first_level_task(
+    storage: &mut MetadataBlobStorage,
+    queue: &WorkQueue,
+    queue_item: Item,
+    protocol: &str,
+    circuit_data: &CircuitDataType<DepositAccumulatorBalanceAggregatorDivaFirstLevel>,
+    circuit_target: &CircuitTargetType<DepositAccumulatorBalanceAggregatorDivaFirstLevel>,
+) {
+    let res = process_first_level_task(
+        &mut storage.metadata,
+        storage.blob.as_mut(),
+        queue,
+        queue_item,
+        circuit_data,
+        circuit_target,
         protocol,
     )
-    .await
+    .await;
+
+    if let Err(err) = res {
+        println!(
+            "{}",
+            format!("Error processing first level task {:?}", err)
+                .red()
+                .bold()
+        );
+    };
 }
+
+async fn handle_inner_level_task(
+    storage: &mut MetadataBlobStorage,
+    queue: &WorkQueue,
+    queue_item: Item,
+    circuit_data: &CircuitDataType<DepositAccumulatorBalanceAggregatorDivaInnerLevel>,
+    circuit_target: &CircuitTargetType<DepositAccumulatorBalanceAggregatorDivaInnerLevel>,
+    verifier_data: &CircuitDataType<DepositAccumulatorBalanceAggregatorDivaFirstLevel>,
+    level: usize,
+    preserve_intermediary_proofs: bool,
+    protocol: &str,
+) {
+    let res = process_inner_level_job(
+        &mut storage.metadata,
+        storage.blob.as_mut(),
+        queue,
+        queue_item,
+        circuit_data,
+        verifier_data,
+        circuit_target,
+        level as u64,
+        preserve_intermediary_proofs,
+        protocol,
+    )
+    .await;
+
+    if let Err(err) = res {
+        println!(
+            "{}",
+            format!("Error processing inner level task {:?}", err)
+                .red()
+                .bold()
+        );
+    };
+}
+
+//async fn handle_work_queue_item(ctx: &mut CommitmentMapperContext, item: &VCMWorkQueueItem) {
+//    match CommitmentMapperTask::deserialize(&item.item.data) {
+//        Ok(task) => {
+//            task.log();
+//
+//            match handle_task(ctx, task).await {
+//                Ok(_) => complete_task(ctx, item).await,
+//                Err(err) => log_error_and_wait(err),
+//            }
+//        }
+//        Err(err) => {
+//            complete_task(ctx, item).await;
+//            println!("{}", format!("Error parsing task: {err}").bold().red());
+//            println!("{}", format!("Got bytes: {:?}", item.item.data).red());
+//        }
+//    }
+//}
 
 #[allow(clippy::too_many_arguments)]
 async fn process_queue(
@@ -218,7 +438,7 @@ async fn process_queue(
                     queue_item,
                     circuit_data,
                     inner_circuit_data.as_ref().unwrap(),
-                    inner_circuit_targets,
+                    inner_circuit_targets.as_ref().unwrap(),
                     level,
                     preserve_intermediary_proofs,
                     protocol,
@@ -315,7 +535,7 @@ async fn process_inner_level_job(
     queue_item: Item,
     circuit_data: &CircuitData<GoldilocksField, PoseidonGoldilocksConfig, 2>,
     inner_circuit_data: &CircuitData<GoldilocksField, PoseidonGoldilocksConfig, 2>,
-    inner_circuit_target: &Option<BasicRecursiveInnerCircuitTarget>,
+    inner_circuit_target: &BasicRecursiveInnerCircuitTarget,
     level: u64,
     preserve_intermediary_proofs: bool,
     protocol: &str,
@@ -359,7 +579,7 @@ async fn process_inner_level_job(
                 proofs.0,
                 proofs.1,
                 inner_circuit_data,
-                inner_circuit_target.as_ref().unwrap(),
+                inner_circuit_target,
                 circuit_data,
             )?;
 
